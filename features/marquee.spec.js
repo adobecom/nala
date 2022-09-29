0f02f706a7dde075701e67d70f182730ac6adaad
Chris Millar <cmillar@adobe.com>
2022-09-29 13:13:30 -0600

    tags

---
+++ features/marquee.spec.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'name': 'Marquee',
+  'features': [
+    {
+      'name': 'Marquee Button',
+      'url': 'https://main--milo--adobecom.hlx.live/test/features/blocks/marquee',
+      'env': '@milo',
+      'tags': '@large-button @medium-button @inline-button',
+    },
+    {
+      'name': 'Marquee Button',
+      'url': 'https://main--bacom--adobecom.hlx.live/customer-success-stories/princess-cruises-case-study',
+      'env': '@consumer',
+      'tags': '@medium-button',
+    },
+  ],
+};
