f37af23c728b4bfc0775103607f6490c38063dd9
Aaron Mauchley <mauchley@adobe.com>
2022-10-05 11:04:26 -0600

    Fixing conflicts for syncing with main
    Fixing conflicts for syncing with main

--- selectors/columns.selectors.js
+++ selectors/columns.selectors.js
@@ -1,8 +1,4 @@
 module.exports = {
-  '@marquee-large': '.marquee.large',
-  '@medium-button': '.marquee .con-button',
-  '@large-button': '.marquee.large .con-button.button-XL',
-  '@inline-button': '.marquee.inline .con-button',
   '@columns-default': '.columns',
   '@columns-contained': '.columns.contained',
   '@columns-contained-middle': '.columns.contained.middle',
--- tests/marquee.test.js
+++ tests/marquee.test.js
@@ -1,6 +1,7 @@
 const { expect, test } = require('@playwright/test');
 const parse = require('../features/parse');
 const marquee = require('../features/marquee.spec');
+const selectors = require('../selectors/marquee.selectors');
 
 // Parse the feature file into something flat that can be tested separately
 const parsed = parse(marquee);
@@ -9,25 +10,15 @@ test.describe(`${parsed.name}`, () => {
   parsed.features.forEach((props) => {
     const title = `${props.name} ${props.env} ${props.tag} on ${props.url}`;
 
-    if (props.name === '@marquee') {
-      test(title, async ({ page }) => {
-        await page.goto(props.url);
-        const el = page.locator(props.selector).first();
+    test(title, async ({ page }) => {
+      await page.goto(props.url);
+      const el = page.locator(selectors[props.tag]).first();
+
+      if (props.tag === '@inline-button') {
+        await expect(el).not.toBeVisible();
+      } else {
         await expect(el).toBeVisible();
-      });
-    }
-
-    if (props.name === '@button') {
-      test(`${props.name} ${props.env} ${props.tag} on ${props.url}`, async ({ page }) => {
-        await page.goto(props.url);
-        const cta = page.locator(props.selector).first();
-
-        if (props.tag === '@inline-button') {
-          await expect(cta).not.toBeVisible();
-        } else {
-          await expect(cta).toBeVisible();
-        }
-      });
-    }
+      }
+    });
   });
 });
