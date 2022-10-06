de3f27454886c837bbb010306fd44f7eeaf11aab
Aaron Mauchley <mauchley@adobe.com>
2022-10-06 14:26:02 -0600

    Added Columns Test
    Added Columns Test, bacom blog and blog.adobe.com envs, added vscode workspace settings file to gitignore

--- tests/columns.test.js
+++ tests/columns.test.js
@@ -9,40 +9,37 @@ const parsed = parse(columns);
 test.describe('Columns', () => {
   parsed.features.forEach((props) => {
     const title = `${props.name} ${props.env} ${props.tag} on ${props.url}`;
-    // Test columns block is visible and expected amount of columns
 
-    test.beforeEach(async ({ page }) => {
-      await page.goto(props.url);
-    });
-
+    // Test columns block is visible and .col divs are present
     test(title, async ({ page }) => {
-      const columns = page.locator(selectors[props.tag]);
+      await page.goto(props.url);
+      const columns = page.locator(selectors[props.tag]).first();
+      await columns.scrollIntoViewIfNeeded();
       await expect(columns).toBeVisible();
-      const col = columns.locator(".col");
-      const count = await col.count();
-      expect(count).toEqual(props.columnCount);
+      const count = await columns.locator('.col').count();
+      expect(count).toBeGreaterThan(0);
     });
 
     //Test link
-    // if(props.tag != '@columns-contained-table') {
-      // test(title, async ({ page }) => {
-        // const el = page.locator(selectors[props.tag]);
-        // await el.locator('a').first().click();
-        // expect(page.url).not.toEqual(props.url);
-      // });
-    // }
+    if(props.tag != '@columns-contained-table') {
+      test(`${title} Links`, async ({ page }) => {
+        await page.goto(props.url);
+        await page.locator(`${selectors[props.tag]} >> a`).first().click();
+        expect(page.url).not.toEqual(props.url);
+      });
+    }
 
     //Test column content whether image or text
-    // test(title, async ({ page }) => {
-      // const el = page.locator(selectors[props.tag]);
-      // const row = el.locator('.row').first();
-      // if(props.tag != '@columns-image') {
-      //   await expect(row).not.toBeEmpty();
-      // } else {
-      //   const image = row.locator('img');
-      //   await expect(image).toBeVisible();
-      // }
-    // });
+    test(`${title} Contents`, async ({ page }) => {
+      await page.goto(props.url);
+      const row = page.locator(`${selectors[props.tag]} >> .row`).first();
+      if(props.env != '@blog') {
+        await expect(row.textContent).toBeTruthy();
+      } else {
+        const image = page.locator(`${selectors[props.tag]} >> .row >> img`).first();
+        await expect(image).toBeVisible();
+      }
+    });
   });
 
   test.afterEach (async ({ page }) => {
