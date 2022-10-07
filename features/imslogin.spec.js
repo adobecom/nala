597be9de08311aa13720c0fb4ea74c608bb3d183
Aaron Mauchley <mauchley@adobe.com>
2022-10-07 14:33:17 -0600

    Added spec and selector file
    Added spec and selector file

---
+++ features/imslogin.spec.js
@@ -0,0 +1,11 @@
+module.exports = {
+  'name': 'IMSLogin',
+  'features': [
+    {
+      'name': '@login',
+      'path': '/customer-success-stories/princess-cruises-case-study',
+      'envs': '@bacom',
+      'tags': '@gnav-signin @apple-signin @google-signin @facebook-signin',
+    },
+  ],
+};
---
+++ selectors/imslogin.selectors.js
@@ -0,0 +1,16 @@
+module.exports = {
+  '@gnav-signin': 'test=Sign In',
+  '@gnav-profile-button': '.gnav-profile-button',
+  '@gnav-signout': 'test=Sign Out',
+  '@gnav-viewaccount': '.gnav-profile-header',
+  '@email': '#EmailPage-EmailField',
+  '@password': '#PasswordPage-PasswordField',
+  '@continue-button': '.spectrum-Button',
+  '@apple-signin': 'data-id=EmailPage-AppleSignInButton',
+  '@google-signin': 'data-id=EmailPage-GoogleSignInButton',
+  '@facebook-signin':'data-id=EmailPage-FacebookSignInButton',
+  '@accept-continue-button': '.spectrum-Button .Button-big',
+  '@greeting': '.spectrum-Heading1 .greeting',
+  '@account-vw-profile-button': '.Profile-thumbnail',
+  '@stay-signed-in': '.spectrum-ToggleSwitch-input'
+};
