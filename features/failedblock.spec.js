7f9f5537ee9c624d7aeda094bf5548fe996f4973
Aaron Mauchley <mauchley@adobe.com>
2022-10-13 10:32:22 -0600

    added failed.block.spec.js as possible option
    added failed.block.spec.js as possible option for housing urls and environments.  Will require parse.js change to allow multiple paths.

---
+++ features/failedblock.spec.js
@@ -0,0 +1,29 @@
+module.exports = {
+  'name': 'Failed Block Check',
+  'features': [
+    {
+      'name': '@failedblockCheck',
+      'path': '/ /de/ /fr/ /jp/ /au/ /solutions/content-management /de/solutions/content-management /fr/solutions/content-management /jp/solutions/content-management /au/solutions/content-management /solutions/cookieless /de/solutions/cookieless /fr/solutions/cookieless /jp/solutions/cookieless /au/solutions/cookieless /resources/digital-economy-index /resources/ /de/resources/ /fr/resources/ /jp/resources/ /au/resources/ /customer-success-stories/ /products/real-time-customer-data-platform/RTCDP /de/products/real-time-customer-data-platform/RTCDP /fr/products/real-time-customer-data-platform/RTCDP /jp/products/real-time-customer-data-platform/RTCDP /au/products/real-time-customer-data-platform/RTCDP /resources/reports/gartner-mq-digital-experience-platform-2021 /customer-success-stories/ben-and-jerrys-case-study /request-consultation/experience-cloud /customer-success-stories/abb-case-study',
+      'envs': '@bacom',
+      'tags': '@failed-block',
+    },
+    {
+      'name': '@failedblockCheck',
+      'path': '/pages/artisthub/ /pages/artisthub/get-started /pages/artisthub/learn /pages/artisthub/get-inspired /pages/artisthub/get-inspired/creative-trends /pages/artisthub/advocates-program /pages/artisthub/community /pages/artisthub/learn/courses /pages/artisthub/learn/livestreams /pages/artisthub/learn/research-insights /pages/artisthub/learn/calls-for-content /pages/artisthub/get-inspired/creative-trends /pages/artisthub/get-inspired/curators-picks /pages/artisthub/get-inspired/seasonal /pages/artisthub/community/stock-mentors',
+      'envs': '@stock',
+      'tags': '@failed-block',
+    },
+    {
+      'name': '@failedblockCheck',
+      'path': '',
+      'envs': '@bacomblog',
+      'tags': '@failed-block',
+    },
+    {
+      'name': '@failedblockCheck',
+      'path': '',
+      'envs': '@blog',
+      'tags': '@failed-block',
+    },
+  ],
+};
