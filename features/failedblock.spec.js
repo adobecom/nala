a5ba0a8ccaca0257ca0e44c2c98fa87959bcc160
Aaron Mauchley <mauchley@adobe.com>
2022-10-14 12:23:07 -0600

    changed path variable to an array
    changed path variable to an array

--- features/failedblock.spec.js
+++ features/failedblock.spec.js
@@ -3,13 +3,15 @@ module.exports = {
   'features': [
     {
       'name': '@failedblockCheck',
-      'path': '/customer-success-stories/ /customer-success-stories/ben-and-jerrys-case-study /customer-success-stories/abb-case-study',
+      'path': [ '/customer-success-stories/', '/customer-success-stories/ben-and-jerrys-case-study',
+        '/customer-success-stories/abb-case-study' ],
       'envs': '@bacom',
       'tags': '@failed-block',
     },
     {
       'name': '@failedblockCheck',
-      'path': '/pages/artisthub/ /pages/artisthub/get-started /pages/artisthub/learn /pages/artisthub/get-inspired /pages/artisthub/get-inspired/creative-trends',
+      'path': [ '/pages/artisthub/', '/pages/artisthub/get-started', '/pages/artisthub/learn',
+        '/pages/artisthub/get-inspired', '/pages/artisthub/get-inspired/creative-trends' ],
       'envs': '@stock',
       'tags': '@failed-block',
     },
