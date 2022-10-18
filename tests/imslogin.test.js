f7d44fa13c4a200d5ced0027d20c36bc1840496a
Aaron Mauchley <mauchley@adobe.com>
2022-10-18 15:39:06 -0600

    updated error messaging for environment variables check
    updated error messaging for environment variables check

--- tests/imslogin.test.js
+++ tests/imslogin.test.js
@@ -7,71 +7,71 @@ import selectors from '../selectors/imslogin.selectors.js';
 const { name, features } = parse(imslogin);
 
 test.describe(`${name}`, () => {
-  if (process.env.IMS_TEST_USERNAME !== null || process.env.IMS_TEST_PSWD !== null) {
-    features.forEach((props) => {
-      if (props.tag === '@gnav-signin') {
-        test(props.title, async ({ page }) => {
-          await page.goto(props.url);
+  features.forEach((props) => {
+    if (props.tag === '@gnav-signin') {
+      test(props.title, async ({ page }) => {
+        expect(process.env.IMS_TEST_USERNAME, 'ERROR: No environment variable for username provided for IMS Test.').toBeTruthy();
+        expect(process.env.IMS_TEST_PSWD, 'ERROR: No environment variable for username provided for IMS Test.').toBeTruthy();
+        await page.goto(props.url);
 
-          // Sign-in
-          let signinBtn = page.locator(selectors[props.tag]);
-          await expect(signinBtn).toBeVisible();
-          await signinBtn.click();
-          if (props.url.includes('hlx.live')) {
-            await page.waitForURL('**/auth-stg1.services.adobe.com/en_US/index.html**/');
-            await expect(page).toHaveURL(/.*auth-stg1.services.adobe.com/);
-          } else {
-            await page.waitForURL('**/auth.services.adobe.com/en_US/index.html**/');
-            await expect(page).toHaveURL(/.*auth.services.adobe.com/);
-          }
-          await expect(page).toHaveTitle(/Adobe ID/);
-          let heading = await page.locator(selectors['@page-heading']).first().innerText();
-          expect(heading).toBe('Sign in');
+        // Sign-in
+        let signinBtn = page.locator(selectors[props.tag]);
+        await expect(signinBtn).toBeVisible();
+        await signinBtn.click();
+        if (props.url.includes('hlx.live')) {
+          await page.waitForURL('**/auth-stg1.services.adobe.com/en_US/index.html**/');
+          await expect(page).toHaveURL(/.*auth-stg1.services.adobe.com/);
+        } else {
+          await page.waitForURL('**/auth.services.adobe.com/en_US/index.html**/');
+          await expect(page).toHaveURL(/.*auth.services.adobe.com/);
+        }
+        await expect(page).toHaveTitle(/Adobe ID/);
+        let heading = await page.locator(selectors['@page-heading']).first().innerText();
+        expect(heading).toBe('Sign in');
 
-          // Fill out Sign-in Form
-          await page.locator(selectors['@email']).fill(process.env.IMS_TEST_USERNAME);
-          await page.locator(selectors['@email-continue-btn']).click();
-          await expect(page.locator(selectors['@password-rememberMe-btn'])).toBeVisible({ timeout: 15000 }); // Timeout accounting for how long IMS Login AEM page takes to switch form
-          heading = await page.locator(selectors['@page-heading'], { hasText: 'Enter your password' }).first().innerText();
-          expect(heading).toBe('Enter your password');
-          await page.locator(selectors['@password']).fill(process.env.IMS_TEST_PSWD);
-          await page.locator(selectors['@password-continue-btn']).click();
-          await page.waitForURL(`${props.url}#`);
-          await expect(page).toHaveTitle(/Princess Cruises entertains\.*.*/);
-          await expect(page).toHaveURL(`${props.url}#`);
+        // Fill out Sign-in Form
+        await page.locator(selectors['@email']).fill(process.env.IMS_TEST_USERNAME);
+        await page.locator(selectors['@email-continue-btn']).click();
+        await expect(page.locator(selectors['@password-rememberMe-btn'])).toBeVisible({ timeout: 15000 }); // Timeout accounting for how long IMS Login AEM page takes to switch form
+        heading = await page.locator(selectors['@page-heading'], { hasText: 'Enter your password' }).first().innerText();
+        expect(heading).toBe('Enter your password');
+        await page.locator(selectors['@password']).fill(process.env.IMS_TEST_PSWD);
+        await page.locator(selectors['@password-continue-btn']).click();
+        await page.waitForURL(`${props.url}#`);
+        await expect(page).toHaveTitle(/Princess Cruises entertains\.*.*/);
+        await expect(page).toHaveURL(`${props.url}#`);
 
-          // Sign-out Milo
-          await page.locator(selectors['@gnav-profile-button']).click();
-          const viewAccount = page.locator(selectors['@gnav-viewaccount']);
-          expect(viewAccount).toBeVisible();
-          const signoutBtn = page.locator(selectors['@gnav-signout']);
-          expect(signoutBtn).toBeVisible();
-          await signoutBtn.click();
-          await page.waitForURL(`${props.url}#`);
-          await expect(page).toHaveTitle(/Princess Cruises entertains\.*.*/);
-          expect(page).toHaveURL(`${props.url}#`);
-          signinBtn = page.locator(selectors[props.tag]);
-          await expect(signinBtn).toBeVisible();
-        });
-      }
+        // Sign-out Milo
+        await page.locator(selectors['@gnav-profile-button']).click();
+        const viewAccount = page.locator(selectors['@gnav-viewaccount']);
+        expect(viewAccount).toBeVisible();
+        const signoutBtn = page.locator(selectors['@gnav-signout']);
+        expect(signoutBtn).toBeVisible();
+        await signoutBtn.click();
+        await page.waitForURL(`${props.url}#`);
+        await expect(page).toHaveTitle(/Princess Cruises entertains\.*.*/);
+        expect(page).toHaveURL(`${props.url}#`);
+        signinBtn = page.locator(selectors[props.tag]);
+        await expect(signinBtn).toBeVisible();
+      });
+    }
 
-      // if (props.tag === '@apple-signin') {
-      //   test(props.title, async ({ page }) => {
-      //     await page.goto(props.url);
-      //   });
-      // }
+    // if (props.tag === '@apple-signin') {
+    //   test(props.title, async ({ page }) => {
+    //     await page.goto(props.url);
+    //   });
+    // }
 
-      // if (props.tag === '@google-signin') {
-      //   test(props.title, async ({ page }) => {
-      //     await page.goto(props.url);
-      //   });
-      // }
+    // if (props.tag === '@google-signin') {
+    //   test(props.title, async ({ page }) => {
+    //     await page.goto(props.url);
+    //   });
+    // }
 
-      // if (props.tag === '@facebook-signin') {
-      //   test(props.title, async ({ page }) => {
-      //     await page.goto(props.url);
-      //   });
-      // }
-    });
-  }
+    // if (props.tag === '@facebook-signin') {
+    //   test(props.title, async ({ page }) => {
+    //     await page.goto(props.url);
+    //   });
+    // }
+  });
 });
