1aef5b9a8d093d0696b283f2a3d8597183396294
Aaron Mauchley <mauchley@adobe.com>
2022-10-18 15:26:24 -0600

    removed credentials file to use github secrets
    removed credentials file to use github secrets

--- credentials/imslogin.creds.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  '@username': 'mauchley+US+Free+VISA+1@adobetest.com',
-  '@password': 'Bap@d0be',
-  '@username_management': 'mauchley+US+Free+VISA+1@adobetest.com',
-  '@password_management': 'Bap@d0be'
-};
