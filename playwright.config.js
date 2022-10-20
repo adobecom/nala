083f70c789288ed9558cdec8fa661f77056a3485
Aaron Mauchley <mauchley@adobe.com>
2022-10-20 11:59:04 -0600

    adding html uploading for debugging IMS test in GitHub
    adding html uploading for debugging IMS test in GitHub

--- playwright.config.js
+++ playwright.config.js
@@ -33,7 +33,6 @@ const config = {
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     [process.env.CI ? 'html' : 'list'],
-    ['html', { outputFolder: 'playwright-report' }],
     ['list'],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
