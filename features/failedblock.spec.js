06a4d378e2a67c8e43b50aaf7b65ec3540aeb070
Aaron Mauchley <mauchley@adobe.com>
2022-10-19 18:28:24 -0600

    fixed small typo (#31)
    fixed small typo

--- features/failedblock.spec.js
+++ features/failedblock.spec.js
@@ -5,7 +5,7 @@ module.exports = {
       name: '@failedblock',
       path: [
         '/customer-success-stories/',
-        'customer-success-stories/jaguar-land-rover-case-study',
+        '/customer-success-stories/jaguar-land-rover-case-study',
         '/customer-success-stories/abb-case-study',
         '/customer-success-stories/dentsu-isobar-case-study',
       ],
