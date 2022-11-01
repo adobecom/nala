e797be649eb65711c07bad53b1939ab1c524bb63
Aaron Mauchley <mauchley@adobe.com>
2022-11-01 12:26:37 -0600

    added waits for debugging
    added waits for debugging

--- playwright.config.js
+++ playwright.config.js
@@ -32,7 +32,7 @@ const config = {
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    [process.env.CI ? 'github' : 'list'],
+    [process.env.CI ? 'html' : 'list'],
     ['list'],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
