3d74c5a07716d42a8a488363982112aec5c98201
Aaron Mauchley <mauchley@adobe.com>
2022-11-01 13:27:14 -0600

    trying another way
    trying another way

--- run.sh
+++ run.sh
@@ -18,4 +18,4 @@ echo "npx playwright test ${TAGS}"
 cd $GITHUB_ACTION_PATH
 npm ci
 npx playwright install --with-deps
-npx playwright test ${TAGS} --headed
+xvfb-run --auto-servernum -- npm test ${TAGS}
