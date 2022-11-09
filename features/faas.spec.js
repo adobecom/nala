60671d924d76e25574f9ea80580e07b46c88b0c4
Aaron Mauchley <mauchley@adobe.com>
2022-11-08 21:36:58 -0700

    removed non form url
    removed non form url

--- features/faas.spec.js
+++ features/faas.spec.js
@@ -5,7 +5,6 @@ module.exports = {
       name: '@faas',
       path: [
         '/drafts/cpatton/guides/how-to-choose-a-future-proof-cdp',
-        '/drafts/cpatton/sdk/turn-engagement-into-action',
         '/drafts/cpatton/webinars/omnichannel-orchestration-with-adobe-marketo-engage-and-adobe-experience-cloud',
       ],
       envs: '@bacom',
