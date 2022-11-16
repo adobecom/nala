59d7fbe439a768011c4b336f70fe16dbfdb93833
Aaron Mauchley <mauchley@adobe.com>
2022-11-16 10:03:02 -0700

    Added initial files for testing
    Added initial files for testing

---
+++ features/georouting.spec.js
@@ -0,0 +1,23 @@
+module.exports = {
+  name: 'Georouting',
+  features: [
+    {
+      name: '@georouting',
+      path: '/test/features/blocks/columns',
+      envs: '@milo',
+      tags: '@columns-default @columns-table',
+    },
+    {
+      name: '@georouting',
+      path: '/blog/basics/agile-vs-waterfall',
+      envs: '@bacomblog',
+      tags: '@columns-table',
+    },
+    {
+      name: '@georouting',
+      path: '/en/publish/2022/07/28/announcing-2022-adobe-analytics-champions',
+      envs: '@blog',
+      tags: '@columns-default',
+    },
+  ],
+};
---
+++ tests/georouting.test.js
@@ -0,0 +1,16 @@
+import { expect, test } from '@playwright/test';
+import georouting from '../features/georouting.spec.js';
+import parse from '../features/parse.js';
+import selectors from '../selectors/georouting.selectors.js';
+
+// Parse the feature file into something flat that can be tested separately
+const { name, features } = parse(georouting);
+
+test.describe(`${name}`, () => {
+  features.forEach((props) => {
+    // Test georouting features
+    test(props.title, async ({ page }) => {
+      await page.goto(props.url);
+    });
+  });
+});
