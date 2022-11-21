7ee2b55a5e4156a49f24d3af5e393270c53d4edb
Dennis Li <denli@adobe.com>
2022-11-21 09:59:01 -0800

    Adding Marketo form tests

---
+++ selectors/marketo.selectors.js
@@ -0,0 +1,12 @@
+module.exports = {
+  '@marketo': '.marketo',
+  '@firstName': '#FirstName',
+  '@lastName': '#LastName',
+  '@email': '#Email',
+  '@company': '#Company',
+  '@title': '#Title',
+  '@country': '#Country',
+  '@phone': '#Phone',
+  '@submitButton': '.mktoButton',
+  '@errorAlert': '.marketo-error',
+};
