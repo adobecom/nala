2b7cb04122b7ec0834648556df0011aae2045766
Aaron Mauchley <mauchley@adobe.com>
2022-11-28 18:26:47 -0700

    removed files that were committed to early
    removed files that were committed to early

--- selectors/georouting.selectors.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  '@dialog-modal': '.dialog-modal .locale-modal',
-  '@dialog-close': '.dialog-close',
-  '@messages': '.text-wrapper',
-  '@message': '.locale-text',
-  '@links': '.link-wrapper',
-  '@link': '.locale-link',
-  '@modal-curtain': '.modal-curtain .is-open',
-};
