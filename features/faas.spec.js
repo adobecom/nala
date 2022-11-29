922ce5b8c850050194bce39b17fe4815044a4e1d
Aaron Mauchley <mauchley@adobe.com>
2022-11-28 17:19:22 -0700

    FaaS Testing (#32)
    * Preliminary Commit for FaaS Testing
    
    Preliminary Commit for FaaS Testing
    
    * added environments and paths
    
    * Added updates to spec and test
    
    Added updates to spec and test
    
    * removed non form url
    
    removed non form url
    
    * updated selectors and added form filling
    
    updated selectors and added form filling
    
    * adjusted form fills and test pages/environments
    
    adjusted form fills and test pages/environments
    
    * adjusted form fills and test pages/environments
    
    adjusted form fills and test pages/environments
    
    * added check for submission
    
    added check for submission
    
    * started adding negative tests
    
    started adding negative tests
    
    * Finished FaaS Test
    
    Added support methods and form injection testing.
    
    * renamed test methods to be unique
    
    renamed test methods to be unique
    
    * fixing runtime errors
    
    fixing runtime errors
    
    * updated test selectors, removed form fill and clear form
    
    updated test selectors, removed form fill and clear form
    
    * added subsequent clicks to help registering changes to form
    
    added subsequent clicks to help registering changes to form
    
    * Scaled back FaaS Test
    
    Scaled back FaaS Test after finding all the negative testing was unnecessary due to it being out of scope for Milo needs.
    
    * Scaled back FaaS Test
    
    Scaled back FaaS Test
    
    * Updated Playwright version to latest
    
    Updated Playwright version to latest in order to have page.getByLabel() method to work (added in v1.27).
    
    * removed unnecessary expect and await
    
    removed unnecessary expect and await where the await for the form input handles checking both.
    
    * Added conditional for webkit FaaS submissions
    
    Added conditional for webkit FaaS submissions.  FaaS times out during submission action.
    
    * added a timeout increase for overlay assertion
    
    added a timeout increase for overlay assertion
    
    * increased assertion timeout
    
    increased assertion timeout
    
    * removed condition of having run-nala
    
    * removed assertion timeout increase
    
    removed assertion timeout increase
    
    * added state/country for all form types
    
    added back filling in state/country for all form types
    
    * added timeout for local running
    
    added timeout for local running

---
+++ features/faas.spec.js
@@ -0,0 +1,23 @@
+module.exports = {
+  name: 'FaaS Blocks',
+  features: [
+    {
+      name: '@faas',
+      path: [
+        '/test/features/blocks/how-to-choose-a-future-proof-cdp',
+        '/test/features/blocks/omnichannel-orchestration-with-adobe-marketo-engage-and-adobe-experience-cloud#register-now',
+      ],
+      envs: '@bacom',
+      tags: '@faas-form',
+    },
+    {
+      name: '@faas',
+      path: [
+        '/test/features/blocks/faas-rfi',
+        '/test/features/blocks/faas-do',
+      ],
+      envs: '@milo',
+      tags: '@faas-form',
+    },
+  ],
+};
---
+++ tests/faas.test.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-await-in-loop */
+const { expect, test } = require('@playwright/test');
+const faas = require('../features/faas.spec.js');
+const parse = require('../features/parse.js');
+const selectors = require('../selectors/faas.selectors.js');
+
+// Parse the feature file into something flat that can be tested separately
+const { name, features } = parse(faas);
+test.describe(`${name}`, () => {
+  features.forEach((props) => {
+    test(props.title, async ({ page, browser }) => {
+      await page.goto(props.url);
+
+      // Fill out form
+      await page.getByLabel(selectors['@org-name']).fill('MiloTestOrg');
+      await page.getByLabel(selectors['@country']).selectOption({ label: 'United States' });
+      await page.getByLabel(selectors['@business-email']).fill('milo@adobetest.com');
+      await page.getByLabel(selectors['@first-name']).fill('MiloTest');
+      await page.getByLabel(selectors['@last-name']).fill('MiloTest');
+      await page.getByLabel(selectors['@business-phone']).fill('777-777-7777');
+      await page.getByLabel(selectors['@job-title-role']).selectOption({ label: 'Individual Contributor' });
+      await page.getByLabel(selectors['@area-department']).selectOption({ label: 'IT' });
+      await page.getByLabel(selectors['@contact-me']).check();
+
+      if (props.url.includes('faas-rfi')) {
+        await page.getByLabel(selectors['@area-interest']).selectOption({ label: 'Website optimization' });
+        await page.getByLabel(selectors['@questions']).fill('Hello World?');
+      }
+
+      // Generally waitForTimeout should only be used for debugging,
+      // but for this test country/state is populated when run outside of GitHub Actions.
+      // The test needs a small sleep to slow down the state field since it swaps field values
+      // according to the country field.
+      await page.waitForTimeout(1000);
+      await page.getByLabel(selectors['@state-province']).selectOption({ label: 'Utah' });
+      await page.getByLabel(selectors['@zipcode']).fill('77777');
+      await page.getByLabel(selectors['@website']).fill('milo.adobe.com');
+      await page.getByLabel(selectors['@industry']).selectOption({ label: 'Technology Software & Services' });
+
+      // Submit form
+      await page.locator(selectors['@submit']).first().click();
+      const formOverlay = page.locator(selectors['@form-overlay']);
+      await expect(formOverlay).toBeVisible();
+
+      // Only check thank you page on Milo domain, where BACOM hasn't been fully setup for FaaS
+      // Add conditional where FaaS times out with submission request with playwright webkit browser
+      // Remove once we know what causes timeout
+      if (browser.browserType().name() !== 'webkit') {
+        if (props.url.includes('faas-rfi') || props.url.includes('faas-do')) {
+          test.setTimeout(300000);
+          await page.waitForURL(/.*thank-you/);
+          await expect(page).toHaveURL(/.*thank-you/);
+          await expect(page).toHaveTitle(/Thank you\.*.*/);
+        }
+      }
+    });
+  });
+});
