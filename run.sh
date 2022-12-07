9d307c7b6762725068bf7fb5ed990803440bc12a
Aaron Mauchley <mauchley@adobe.com>
2022-12-06 19:57:02 -0700

    added reporter variable
    added reporter variable to debug.yml workflow as well as to the run.sh so that the execution can use the html reporter instead of the default github reporter

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 TAGS=""
+REPORTER=""
 
 # Convert github labels to tags that can be grepped
 for label in ${labels}
@@ -11,11 +12,16 @@ done
 # Remove the first pipe from tags if tags are not empty
 [[ ! -z "$TAGS" ]] && TAGS="${TAGS:1}" && TAGS="-g $TAGS"
 
+# Retrieve github reporter parameter if not empty
+# Otherwise use reporter settings in playwright.config.js
+REPORTER=$reporter
+[[ ! -z "$REPORTER" ]] && REPORTER="--reporter $REPORTER"
+
 echo "*** Running Nala on $branch ***"
 echo $TAGS
-echo "npx playwright test ${TAGS}"
+echo "npx playwright test ${TAGS} ${REPORTER}"
 
 cd $GITHUB_ACTION_PATH
 npm ci
 npx playwright install --with-deps
-npx playwright test ${TAGS}
+npx playwright test ${TAGS} ${REPORTER}
