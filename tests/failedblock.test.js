f306b6f9f26e495fd7b9c6afbaef3ad36619a5f9
Aaron Mauchley <mauchley@adobe.com>
2023-01-18 12:03:53 -0700

    added scroll method and soft expect
    added scroll method and removed hover method, changed expect to soft expect.

--- tests/failedblock.test.js
+++ tests/failedblock.test.js
@@ -8,20 +8,47 @@ const selectors = require('../selectors/failedblock.selectors.js');
 // Parse the feature file into something flat that can be tested separately
 const { name, features } = parse(failedBlock);
 
+/**
+ * Slow/fast scroll JS evaluation method.
+ * To be used in page.evaluate, i.e. page.evaluate(scroll, { direction: 'value', speed: 'value' });
+ * @param direction string direction you want to scroll
+ * @param speed string speed you would like to scroll. Options: slow, fast
+*/
+const scroll = async (args) => {
+  const { direction, speed } = args;
+  // eslint-disable-next-line no-promise-executor-return
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+  const scrollHeight = () => document.body.scrollHeight;
+  const start = direction === 'down' ? 0 : scrollHeight();
+  const shouldStop = (position) => (direction === 'down' ? position > scrollHeight() : position < 0);
+  const increment = direction === 'down' ? 100 : -100;
+  const delayTime = speed === 'slow' ? 30 : 5;
+  console.error(start, shouldStop(start), increment);
+  for (let i = start; !shouldStop(i); i += increment) {
+    window.scrollTo(0, i);
+    await delay(delayTime);
+  }
+};
+
 test.describe(`${name}`, () => {
   features.forEach((props) => {
-    test(props.title, async ({ page }) => {
+    test(props.title, async ({ page, browser }) => {
       await page.goto(props.url);
-      await page.locator('footer').hover(); // Added to give time for failed block JS to load. Without it, test becomes flaky.
+
+      // Added scrolling for failed block JS to load.
+      // Without it, test provides false count for validation checking.
+      await page.evaluate(scroll, { direction: 'down', speed: 'slow' });
+      await page.evaluate(scroll, { direction: 'up', speed: 'fast' });
+
       const locator = page.locator(selectors[props.tag]);
       const count = await locator.count();
-      expect(count).toEqual(0);
+      expect.soft(count).toEqual(0);
       if (count > 0) {
         const handles = await locator.elementHandles();
         for (const handlePromise of handles) {
           const handle = handlePromise;
           const reason = await handle.getAttribute('data-reason');
-          console.log(`${reason} on ${props.url}`);
+          console.log(`${browser.browserType().name()}: ${reason} on ${props.url}`);
         }
       }
     });
