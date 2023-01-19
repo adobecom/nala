e55b0f518b566f5483df7aa87a87e812df086d9c
John Fernandez <john@readyartwork.com>
2023-01-18 16:00:01 -0800

    First DC test

---
+++ selectors/dc_converter.selectors.js
@@ -0,0 +1,5 @@
+module.exports = {
+  '@pdf-converter': '.converter, .converterInline',
+  '@file-upload-input': '[data-test-id="file-upload-input"][accept=".pdf"]',
+  '@pdf-complete': '//div[contains(@class,"lifecycleComplete")]',
+};
