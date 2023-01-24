7009ec7faddb3550ee373ba89e74d47dba68d0ca
John Fernandez <john@readyartwork.com>
2023-01-24 14:55:22 -0800

    Update converter block selectors

--- selectors/dc_converter.selectors.js
+++ selectors/dc_converter.selectors.js
@@ -1,7 +1,7 @@
 module.exports = {
   '@pdf-converter': '.converter, .converterInline',
   '@file-upload-input': '[data-test-id="file-upload-input"][accept=".pdf"]',
-  '@pdf-complete': '//div[contains(@class,"lifecycleComplete")]',
-  '@file-preview': '//*[contains(@class,"PageView__PageView")]',
+  '@pdf-complete': 'section[class*=lifecycleComplete]',
+  '@file-preview': '[class*=PageView__PageView]',
   '@download': '[data-test-id="download"]',
 };
