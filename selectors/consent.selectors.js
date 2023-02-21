73fae30ce30aa28a3ef3a2d4ba61be2a344672a6
Dan Chivescu <chivescu@adobe.com>
2023-02-21 16:13:58 +0200

    [MWPW-126307] Added FEDS consent selectors (wip)

---
+++ selectors/consent.selectors.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  // OneTrust consent selectors:
+  '@OneTrustTitle': '#onetrust-policy-title',
+  '@OneTrustMessage': '#onetrust-policy-text',
+  '@OneTrustEnableButton': '#onetrust-accept-btn-handler',
+  '@OneTrustCookiesButton': '#onetrust-pc-btn-handler',
+  '@OneTrustDontEnableButton': '#onetrust-reject-all-handler',
+
+  // OneTrust container selectors:
+  '@OneTrustContainer': '#onetrust-banner-sdk',
+  '@OneTrustConsentFrame': '#onetrust-pc-sdk',
+  '@OneTrustConfirmChoices': 'button.save-and-close',
+  '@OneTrustModalDontEnable': 'button.disable-all-btn',
+  '@OneTrustModalEnableAll': 'button.enable-all-btn',
+  '@OneTrustModalClose': 'button.save-and-close'
+};
