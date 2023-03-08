3cdea6bab7f8aeca22fabf4e9718dd95bc527f4e
xiasun <xiasun@xiasuns-MacBook-Pro-2.local>
2023-03-08 12:57:21 -0800

    result envs

--- features/result.spec.js
+++ features/result.spec.js
@@ -2,27 +2,19 @@ module.exports = {
   name: 'Quiz Recommender Blocks',
   features: [
     {
-      name: '@result_prod_full',
+      name: '@result_full',
       path: [
         '/creativecloud/quiz-recommender/result.html#type=',
       ],
-      envs: '@adobe_prod',
+      envs: '@adobe_prod @adobe_stage',
       tags: '@result-single-template',
     },
     {
-      name: '@result_stage_full',
+      name: '@result_smoke',
       path: [
         '/creativecloud/quiz-recommender/result.html#type=',
       ],
-      envs: '@adobe_stage',
-      tags: '@result-single-template',
-    },
-    {
-      name: '@result_prod_smoke',
-      path: [
-        '/creativecloud/quiz-recommender/result.html#type=',
-      ],
-      envs: '@adobe_prod',
+      envs: '@adobe_prod @adobe_stage',
       tags: '@result-single-template',
     },
   ],
