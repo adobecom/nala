8bc96375069a846cb164584073e91be6aa9840cd
Dan Chivescu <chivescu@adobe.com>
2023-03-10 23:05:38 +0200

    [MWPW-126969] Added search component check

---
+++ tests/search.test.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-await-in-loop */
+const parse = require('../features/parse.js');
+const search = require('../features/search.spec.js');
+const { expect, test } = require('@playwright/test');
+import { FedsHeader } from '../pages/feds.header.page';
+
+const { name, features } = parse(search);
+test.describe(`${name}`, () => {
+  features.forEach((props) => {
+    test(props.title, async ({ page, browser }) => {
+      // Initialize FEDS page:
+      const Header = new FedsHeader(page);
+      // Load page with FEDS search component:
+      await page.goto(props.url);
+      // Wait for page to load & stabilize:
+      await page.waitForLoadState('networkidle');
+      // Check basic search functionality:
+      await Header.openSearchBar();
+      await Header.closeSearchBar();
+      // Check search functionality (real search):
+      await Header.openSearchBar();
+      await Header.SearchInput.fill('Adobe Photoshop');
+      await expect(Header.SearchResults).toBeVisible();
+      await Header.closeSearchBar();
+      // Check search functionality (bogus search):
+      await Header.openSearchBar();
+      await Header.SearchInput.fill('How much wood could a woodchuck chuck ...');
+      // Wait for search results to populate:
+      await page.waitForTimeout(1000);
+      await expect(Header.AdvancedSearchLink).toBeVisible();
+      await Header.closeSearchBar();
+    });
+  });
+});
