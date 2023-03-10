5331c210b94d53ce3cf54b1f62afa7fe5147ffb3
xiasun <xiasun@xiasuns-MacBook-Pro-2.local>
2023-03-09 20:56:20 -0800

    add dynamic tests

--- common/data-provider.js
+++ common/data-provider.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
+// eslint-disable-next-line import/no-extraneous-dependencies
 const yaml = require('js-yaml');
+const { request } = require('@playwright/test');
 
 /**
  * @param {string} filePath
@@ -8,4 +10,14 @@ async function loadTestData(dataFilePath) {
   return dataFilePath.includes('.yml') ? yaml.load(fs.readFileSync(dataFilePath, 'utf8')) : JSON.parse(fs.readFileSync(dataFilePath, 'utf8'));
 }
 
-module.exports = loadTestData;
+/**
+ * @param {string} path
+ * @param {string} url
+ */
+async function loadTestDataFromAPI(url, path) {
+  const context = await request.newContext({ baseURL: url });
+  const res = await context.fetch(path);
+  return res.json();
+}
+
+module.exports = { loadTestData, loadTestDataFromAPI };
