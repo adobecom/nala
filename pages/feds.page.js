1c5635f8e6b8fdc38b586cbe7ffafeb4a341bfde
Dan Chivescu <chivescu@adobe.com>
2023-03-28 20:23:25 +0300

    [MWPW-126969] Removed feds.page.js base page

--- pages/feds.page.js
+++
@@ -1,9 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-exports.FedsPage = class FedsPage {
-
-  constructor() {
-    // >> Generic FEDS page selectors declared here <<
-  }
-  // >> Generic FEDS page methods declared here <<
-};
