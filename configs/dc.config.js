dab5512aef863e6733f8ef906e593d8d5b63c661
Santoshkumar Nateekar <santoshkumar.sn@gmail.com>
2023-04-14 13:27:31 -0700

    MWPW-129400 update consumer config and baseURL structure (#101)
    * MWPW-129400 update consumer config and baseURL structure
    
    * fix end of file issue in run.sh
    
    * update run.sh error
    
    * fixed run-on prefix
    
    * fix label start prefix for tags
    
    * config file name fix
    
    * fixed elif and filename issue
    
    * test the tags
    
    * fixed json report path in configs
    
    * fixing reporter json  path
    
    * fixed no test found error
    
    * removing debuggin print statements
    
    * fix tag name
    
    * fixed config file name not found issue
    
    * update review comment
    
    * removed local json reporter
    
    * added project prod to dc config
    
    * updated acom  project config
    
    * updated run.sh to support few more commands
    
    * updated run.sh to support run-nala command
    
    * updated self.yml to consider labels containing run for test execution
    
    * update self.yml to updated the label check
    
    * reverting .yml changes
    
    ---------
    
    authored-by: nateekar <nateekar@adobe.com>

---
+++ configs/dc.config.js
@@ -0,0 +1,121 @@
+// @ts-check
+const { devices } = require('@playwright/test');
+
+const envs = require('../envs/envs.js');
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ * @type {import('@playwright/test').PlaywrightTestConfig}
+ */
+const config = {
+  testDir: '../tests',
+  outputDir: '../test-results',
+  /* Maximum time one test can run for. */
+  timeout: 30 * 1000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 5000,
+  },
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 2 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: process.env.CI
+    ? [['github'], ['../utils/reporters/json-reporter.js'], ['../utils/reporters/json-reporter.js']]
+    : [['html', { outputFolder: 'test-html-results' }]],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    actionTimeout: 60000,
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://localhost:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+    baseURL: process.env.BASE_URL || envs['@dc_preview'] || 'https://main--dc--adobecom.hlx.page',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'dc-preview-chrome',
+      use: {
+        ...devices['Desktop Chrome'],
+        baseURL: envs['@dc_preview'],
+      },
+    },
+
+    {
+      name: 'dc-preview-firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+        baseURL: envs['@dc_preview'],
+      },
+    },
+
+    {
+      name: 'dc-preview-webkit',
+      use: {
+        ...devices['Desktop Safari'],
+        baseURL: envs['@dc_preview'],
+      },
+    },
+
+    {
+      name: 'dc-live-chrome',
+      use: {
+        ...devices['Desktop Chrome'],
+        baseURL: envs['@dc_live'],
+      },
+    },
+
+    {
+      name: 'dc-live-firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+        baseURL: envs['@dc_live'],
+      },
+    },
+
+    {
+      name: 'dc-live-webkit',
+      use: {
+        ...devices['Desktop Safari'],
+        baseURL: envs['@dc_live'],
+      },
+    },
+
+    {
+      name: 'adobe-prod-chrome',
+      use: {
+        ...devices['Desktop Chrome'],
+        baseURL: envs['@adobe_prod'],
+      },
+    },
+
+    {
+      name: 'adobe-prod-firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+        baseURL: envs['@adobe_prod'],
+      },
+    },
+
+    {
+      name: 'adobe-prod-webkit',
+      use: {
+        ...devices['Desktop Safari'],
+        baseURL: envs['@adobe_prod'],
+      },
+    },
+  ],
+};
+module.exports = config;
