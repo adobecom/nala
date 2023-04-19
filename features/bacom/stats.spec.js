ad967cf5a8ec35f6d746beb71cab2ab5ef0b1417
Dennis Li <34138453+Dli3@users.noreply.github.com>
2023-04-19 11:24:38 -0700

    Adding stats block test. (#106)
    Co-authored-by: Dennis Li <denli@adobe.com>

---
+++ features/bacom/stats.spec.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: 'Stats',
+  features: [
+    {
+      tcid: '1',
+      name: '@Stats',
+      path: '/customer-success-stories/adobe-content-hub-case-study',
+      tags: '@stats @bacom @smoke @regression',
+    },
+  ],
+};
