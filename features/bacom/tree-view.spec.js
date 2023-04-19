2db0424ea89a4568f190b959b6e7d43aaa9873d6
Dennis Li <denli@adobe.com>
2023-04-19 16:03:58 -0700

    Adding treeview tests

---
+++ features/bacom/tree-view.spec.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: 'Tree-view block',
+  features: [
+    {
+      tcid: '1',
+      name: '@Tree-view',
+      path: '/test/features/blocks/tree-view',
+      tags: '@tree-view @smoke @regression @bacom',
+    },
+  ],
+};
