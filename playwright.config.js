0a3cb47e4ab7958725cf301311d05400f20f1ece
Santoshkumar Nateekar <santoshkumar.sn@gmail.com>
2023-05-04 12:23:53 -0700

    MWPW-130636 : Update test folder path in configs (#113)
    update test folder in configs
    
    Co-authored-by: nateekar <nateekar@adobe.com>

--- playwright.config.js
+++ playwright.config.js
@@ -12,7 +12,7 @@ const { devices } = require('@playwright/test');
  * @type {import('@playwright/test').PlaywrightTestConfig}
  */
 const config = {
-  testDir: './tests',
+  testDir: './tests/milo',
   outputDir: './test-results',
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
