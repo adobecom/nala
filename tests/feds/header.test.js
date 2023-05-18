b1dc3359ac2b6618ce36a13230b35076595b3cdd
Dan Chivescu <chivescu@adobe.com>
2023-05-18 16:29:54 +0300

    [MWPW-130773] Reorganized FEDS checks

--- tests/feds/header.test.js
+++ tests/feds/header.test.js
@@ -13,32 +13,31 @@ test.describe(`${name}`, () => {
   features.forEach((props) => {
     test(props.title, async ({ page }) => {
       const { title } = props;
-
       // Initialize FEDS header page:
       const Header = new FedsHeader(page);
-
       // Navigate to FEDS header page:
       await page.goto(props.url);
       // Wait for page to load & stabilize:
       await page.waitForLoadState('domcontentloaded');
-
       // Wait for FEDS GNAV to be visible:
       await Header.MainNavContainer.waitFor({ state: 'visible', timeout: 5000 });
       await expect(Header.SearchIcon).toBeVisible();
       await expect(Header.SignInLabel).toBeVisible();
-      if (!/adobe/.test(title)) await expect(Header.GnavLogo).toBeVisible();
       await expect(Header.MainNavLogo).toBeVisible();
+      if (!/adobe/.test(title)) await expect(Header.GnavLogo).toBeVisible();
 
-      // Check basic search functionality:
-      await Header.OpenSearchBar();
-      await Header.CloseSearchBar();
+      await test.step('Check HEADER search component', async () => {
+        await Header.OpenSearchBar();
+        await Header.CloseSearchBar();
+      });
 
-      // Check header mega menu:
-      await Header.MegaMenuToggle.waitFor({ state: 'visible', timeout: 5000 });
-      await Header.MegaMenuToggle.click();
-      await expect(Header.MegaMenuContainer).toBeVisible();
-      await Header.MegaMenuToggle.click();
-      await expect(Header.MegaMenuContainer).not.toBeVisible();
+      await test.step('Check HEADER mega menu component', async () => {
+        await Header.MegaMenuToggle.waitFor({ state: 'visible', timeout: 5000 });
+        await Header.MegaMenuToggle.click();
+        await expect(Header.MegaMenuContainer).toBeVisible();
+        await Header.MegaMenuToggle.click();
+        await expect(Header.MegaMenuContainer).not.toBeVisible();
+      });
     });
   });
 });
