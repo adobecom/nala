c599920783614bb4c5f985802cd1fcf1fc02451e
Aaron Mauchley <mauchley@adobe.com>
2023-05-22 20:30:52 -0600

    removed visual prep script
    removed visual prep script

--- runVisualPrep.sh
+++ runVisualPrep.sh
@@ -3,10 +3,6 @@
 # Navigate to workspace directory or bail
 cd "$GITHUB_ACTION_PATH" || exit
 
-echo "DEBUG STATEMENTS, PLEASE REMOVE..."
-pwd
-ls -ali
-
 # Copy base screenshots to nala repository in preparation for visual comparison testing
 echo "Copying base screenshots..."
 cp -fr nala-screenshots/screenshots/visual-compare/ nala/screenshots/visual-compare/
