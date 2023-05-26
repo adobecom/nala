1a0b9e14fb8242a7dbc8f8d15a90c77b3d8e6e87
nateekar <nateekar@adobe.com>
2023-05-25 17:37:34 -0700

    minor comments update

--- tests/milo/quote.block.test.js
+++ tests/milo/quote.block.test.js
@@ -18,7 +18,7 @@ test.describe('Milo Quote block test suite', () => {
   test(`${features[0].name},${features[0].tags}`, async ({ page, baseURL }) => {
     console.info(`${baseURL}${features[0].path}`);
     // test step-1
-    await test.step('Go to Quote feature test page', async () => {
+    await test.step('Go to Quote block test page', async () => {
       await page.goto(`${baseURL}${features[0].path}`);
       await page.waitForLoadState('domcontentloaded');
       await expect(page).toHaveURL(`${baseURL}${features[0].path}`);
@@ -26,7 +26,6 @@ test.describe('Milo Quote block test suite', () => {
 
     // test step-2
     await test.step('Verify Quote block content / specs ', async () => {
-      // verify quote and its content
       const { data } = features[0];
       expect(await quote.verifyQuote('quote', data)).toBeTruthy();
     });
@@ -36,15 +35,14 @@ test.describe('Milo Quote block test suite', () => {
   test(`${features[1].name},${features[1].tags}`, async ({ page, baseURL }) => {
     console.info(`${baseURL}${features[1].path}`);
     // test step-1
-    await test.step('Go to Quote feature test page', async () => {
+    await test.step('Go to Quote block test page', async () => {
       await page.goto(`${baseURL}${features[1].path}`);
       await page.waitForLoadState('domcontentloaded');
       await expect(page).toHaveURL(`${baseURL}${features[1].path}`);
     });
 
     // test step-2
-    await test.step('Verify Quote block content / specs ', async () => {
-      // verify quote and its content
+    await test.step('Verify Quote (contained) block content / specs ', async () => {
       const { data } = features[1];
       expect(await quote.verifyQuote('quote (contained)', data)).toBeTruthy();
     });
@@ -54,7 +52,7 @@ test.describe('Milo Quote block test suite', () => {
   test(`${features[2].name},${features[2].tags}`, async ({ page, baseURL }) => {
     console.info(`${baseURL}${features[2].path}`);
     // test step-1
-    await test.step('Go to Quote (inline)e block test page', async () => {
+    await test.step('Go to Quote (inline) block test page', async () => {
       await page.goto(`${baseURL}${features[2].path}`);
       await page.waitForLoadState('domcontentloaded');
       await expect(page).toHaveURL(`${baseURL}${features[2].path}`);
@@ -62,7 +60,6 @@ test.describe('Milo Quote block test suite', () => {
 
     // test step-2
     await test.step('Verify Quote (inline) block content / specs ', async () => {
-      // verify quote and its content
       const { data } = features[2];
       expect(await quote.verifyQuote('quote (inline)', data)).toBeTruthy();
     });
@@ -80,7 +77,6 @@ test.describe('Milo Quote block test suite', () => {
 
     // test step-2
     await test.step('Verify Quote (borders) block content / specs ', async () => {
-      // verify quote (borders) and its content
       const { data } = features[3];
       expect(await quote.verifyQuote('quote (borders)', data)).toBeTruthy();
     });
@@ -98,7 +94,6 @@ test.describe('Milo Quote block test suite', () => {
 
     // test step-2
     await test.step('Verify Quote (align-right) block content / specs ', async () => {
-      // verify quote (align-right) and its content
       const { data } = features[4];
       expect(await quote.verifyQuote('quote (align-right)', data)).toBeTruthy();
     });
@@ -116,7 +111,6 @@ test.describe('Milo Quote block test suite', () => {
 
     // test step-2
     await test.step('Verify Quote (xl-spaced) block content / specs ', async () => {
-      // verify quote ((xl-spaced)) and its content
       const { data } = features[5];
       expect(await quote.verifyQuote('quote (xl-spaced)', data)).toBeTruthy();
     });
