dd8a434da77c9bcee3684925040ab8bf443ce957
Dan Chivescu <106924778+chadsunvice@users.noreply.github.com>
2023-06-09 13:00:55 +0300

    [MWPW-131399] Added FEDS Footer default test (#128)
    * [MWPW-131399] Updated FOOTER checks
    
    * [MWPW-131399] Updated FEDS test-files (tackle warnings/consistency)
    
    * [MWPW-131399] Updated Footer page-object
    
    * [MWPW-131399] Added 2 new Footer checks
    
    ---------
    
    Co-authored-by: Dan Chivescu <chivescu@adobe.com>

--- tests/feds/userprofile.test.js
+++ tests/feds/userprofile.test.js
@@ -1,15 +1,18 @@
-/* eslint-disable no-restricted-syntax */
+/* eslint-disable import/named */
 /* eslint-disable no-await-in-loop */
-const { expect, test } = require('@playwright/test');
-const parse = require('../../libs/parse.js');
-const userProfile = require('../../features/feds/userprofile.spec.js');
+/* eslint-disable import/extensions */
+/* eslint-disable no-restricted-syntax */
+import { expect, test } from '@playwright/test';
 import { FedsLogin } from '../../selectors/feds/feds.login.page';
 import { FedsHeader } from '../../selectors/feds/feds.header.page';
+
+const parse = require('../../libs/parse.js');
+const userProfile = require('../../features/feds/userprofile.spec.js');
 
 const { name, features } = parse(userProfile);
 test.describe(`${name}`, () => {
   features.forEach((props) => {
-    test(props.title, async ({ page, browser }) => {
+    test(props.title, async ({ page }) => {
       // Initialize FEDS login page:
       const Login = new FedsLogin(page);
       const Header = new FedsHeader(page);
@@ -18,12 +21,12 @@ test.describe(`${name}`, () => {
       // Wait for page to load & stabilize:
       await page.waitForLoadState('domcontentloaded');
       // Wait for FEDS GNAV to be visible:
-      await Header.MainNavLogo.waitFor({state: 'visible', timeout: 5000});
-      await Header.MainNavContainer.waitFor({state: 'visible', timeout: 5000});
+      await Header.MainNavLogo.waitFor({ state: 'visible', timeout: 5000 });
+      await Header.MainNavContainer.waitFor({ state: 'visible', timeout: 5000 });
 
       await test.step('Login with a valid Adobe account', async () => {
         // Click 'Sign In' label:
-        await Header.SignInLabel.waitFor({state: 'visible', timeout: 5000});
+        await Header.SignInLabel.waitFor({ state: 'visible', timeout: 5000 });
         await Header.SignInLabel.click();
         await Login.loginOnAppForm(process.env.IMS_EMAIL, process.env.IMS_PASS);
       });
