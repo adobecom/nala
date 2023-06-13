393e92c3bea5e6692d2adefb02c06137df86824d
sigadamramesh <130433161+sigadamvenkata@users.noreply.github.com>
2023-06-13 10:46:45 +0530

    config, env, folder structure setup for CC (#126)
    * config, env, folder structure setup for CC
    
    config, env, folder structure setup for CC onboarding
    
    * PR 126 - Review comments changes
    
    PR 126 - Review comments changes
    
    * PR 126 asked changes
    
    PR 126 asked changes
    
    * Update febpub.sanity.js
    
    * Update fedpub.sanity.js
    
    * Update febpub.selectors.js
    
    * Update febpub.test.js
    
    ---------
    
    Co-authored-by: Santoshkumar Nateekar <santoshkumar.sn@gmail.com>

---
+++ selectors/cc/febpub.selectors.js
@@ -0,0 +1,4 @@
+   module.exports = {
+    '@gnavitem1': '[href="/creativecloud/photography.html#mini-plans-web-cta-photoshop-lightroom-card"]',
+    '@gnavtrybutton': '[class="con-button blue button-m"]',
+   };
