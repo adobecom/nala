4b5ba25884dbfb23db4ff96d1fc60b392dc4124a
Dan Chivescu <chivescu@adobe.com>
2023-06-15 15:24:12 +0300

    [MWPW_1322804] Removed test examples & duplicate fedpub spec-file

--- selectors/cc/febpub.selectors.js
+++
@@ -1,4 +0,0 @@
-   module.exports = {
-    '@gnavitem1': '[href="/creativecloud/photography.html#mini-plans-web-cta-photoshop-lightroom-card"]',
-    '@gnavtrybutton': '[class="con-button blue button-m"]',
-   };
