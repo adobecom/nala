ecc59eb6f2d3779be9f6b0468d24eb29b481e319
Dan Chivescu <106924778+chadsunvice@users.noreply.github.com>
2023-06-23 00:03:34 +0300

    [MWPW-132804] Fix CC checks & cleanup NALA small issues (#133)
    * [MWPW_1322804] Removed test examples & duplicate fedpub spec-file
    
    * [MWPW-132804] Fixed failing CC initial test
    
    * [MWPW-132804] Small improvements
    
    * [MWPW-132804] Added CC new files
    
    * [MWPW-132804] Removed no-restricted-exports & renamed test file (typo)
    
    * [MWPW-132804] Removed no-restricted-exports & renamed test file (typo)
    
    ---------
    
    Co-authored-by: Dan Chivescu <chivescu@adobe.com>
    Co-authored-by: Aaron Mauchley <mauchley@adobe.com>

---
+++ features/cc/fedpub.spec.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: 'FedPub Component',
+  features: [
+    {
+      tcid: '0',
+      name: '@FedPub-Sanity-Check',
+      path: '/creativecloud/photography/hub/guides/camera-settings-for-rain-photography',
+      envs: '@cc_live @adobe_stage',
+      tags: '@cc @cc-fedpub',
+    },
+  ],
+};
