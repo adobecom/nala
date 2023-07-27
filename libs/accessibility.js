7aec755724fc99030faad045ea808b3848de8471
Dan Chivescu <chadsunvice@gmail.com>
2023-07-27 15:56:13 +0300

    [MWPW-130049] Misc updates to support A11y analysis

--- libs/accessibility.js
+++ libs/accessibility.js
@@ -1,18 +1,17 @@
+/* eslint-disable max-len */
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-/* eslint-disable max-len */
 /* eslint-disable import/no-import-module-exports */
-/* eslint-disable-next-line import/no-import-module-exports */
-const fs = require('fs').promises;
-const path = require('path');
+/* eslint-disable import/no-extraneous-dependencies */
+import * as path from 'path';
+import { promises as fs } from 'fs';
 import { createHtmlReport } from 'axe-html-reporter';
 
-async function generateA11yReport(report, summary, project='feds', wcag='wcag21aa') {
-
+async function generateA11yReport(report, summary, project = 'feds', wcag = 'wcag21aa') {
   // Timestamp reports to track & avoid duplicates:
-  const time       = new Date();
-  const crtEnv     = 'prod';
-  const reportDate = time.toLocaleDateString().replace(/\//ig,'-');
+  const time = new Date();
+  const crtEnv = 'prod';
+  const reportDate = time.toLocaleDateString().replace(/\//ig, '-');
   const reportTime = time.toLocaleTimeString().split(' ')[0].replace(/:/g, '-');
 
   // Setup the 'axe-reports' folder:
@@ -35,18 +34,19 @@ async function generateA11yReport(report, summary, project='feds', wcag='wcag21a
   const htmlReport = createHtmlReport({
     results: report,
     options: {
-        projectKey: 'MWP Web (https://jira.corp.adobe.com/projects/MWPW)',
-        customSummary,
-        doNotCreateReportFile: true,
+      projectKey: 'MWP Web (https://jira.corp.adobe.com/projects/MWPW)',
+      customSummary,
+      doNotCreateReportFile: true,
     },
   });
 
   // Write a11y html report to file:
-  await fs.writeFile(path.resolve(folderPath, reportName), htmlReport, function(err) {
+  await fs.writeFile(path.resolve(folderPath, reportName), htmlReport, (err) => {
     if (err) {
       return console.error(`[FEDsError] Failed to write the '${reportName}' report! | Reason: '${err}'`);
     }
     console.info(`[FEDsInfo] Successfully wrote the '${reportName}' a11y report!`);
+    return 1;
   });
 }
 
