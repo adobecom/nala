0af3de62ec36392d70accc98f1b52a81cb074cc8
nateekar <nateekar@adobe.com>
2023-09-12 10:47:50 -0700

    add soft expect 2

--- tests/blog/urlredirect.test.js
+++ tests/blog/urlredirect.test.js
@@ -2,11 +2,14 @@ import { expect, test } from '@playwright/test';
 import { features } from '../../features/blog/urlredirect.spec.js';
 
 let redirectBlogUrl = 'https://blog.adobe.com';
+let stepCounter = 0;
 
 test.describe('Blog URL Redirects', () => {
     // Topic URL redirects
     test(`${features[0].name},${features[0].tags}`, async ({ page, baseURL }) => {
       const path = features[0].path;
+      stepCounter = 0;
+
       for (const [url, redirectUrl] of Object.entries(path)) {
         await test.step(`step-${++stepCounter}: verify url '${url}' redirect`, async () => {
           console.info(`[Test Page ${stepCounter}]: ${baseURL}${url}`);
@@ -20,6 +23,8 @@ test.describe('Blog URL Redirects', () => {
     // Index URL redirects
     test(`${features[1].name},${features[1].tags}`, async ({ page, baseURL }) => {
       const path = features[1].path;
+      stepCounter = 0;
+
       for (const [url, redirectUrl] of Object.entries(path)) {
         await test.step(`step-${++stepCounter}: verify url '${url}' redirect`, async () => {
           console.info(`[Test Page ${stepCounter}]: ${baseURL}${url}`);
@@ -33,6 +38,8 @@ test.describe('Blog URL Redirects', () => {
     // Author URL redirects
     test(`${features[2].name},${features[2].tags}`, async ({ page, baseURL }) => {
       const path = features[2].path;
+      stepCounter = 0;
+
       for (const [url, redirectUrl] of Object.entries(path)) {
         await test.step(`step-${++stepCounter}: verify url '${url}' redirect`, async () => {
           console.info(`[Test Page ${stepCounter}]: ${baseURL}${url}`);
@@ -46,6 +53,8 @@ test.describe('Blog URL Redirects', () => {
     // Publish URL redirects
     test(`${features[3].name},${features[3].tags}`, async ({ page, baseURL }) => {
       const path = features[3].path;
+      stepCounter = 0;
+
       for (const [url, redirectUrl] of Object.entries(path)) {
         await test.step(`step-${++stepCounter}: verify url '${url}' redirect`, async () => {
           console.info(`[Test Page ${stepCounter}]: ${baseURL}${url}`);
