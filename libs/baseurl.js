5efa11aa3e8f5216bca7cdf13a17e89da822e595
nateekar <nateekar@adobe.com>
2023-09-16 14:08:17 -0700

    update baseURL

--- libs/baseurl.js
+++ libs/baseurl.js
@@ -4,15 +4,15 @@ export async function isBranchURLValid(url) {
   try {
     const response = await head(url);
     if (response.status === 200) {
-      console.log(`URL (${url}) returned a 200 status code. It is valid.`);
+      console.info(`URL (${url}) returned a 200 status code. It is valid.`);
       return true;
     } else {
-      console.log(`URL (${url}) returned a non-200 status code (${response.status}). It is invalid.`);
+      console.info(`URL (${url}) returned a non-200 status code (${response.status}). It is invalid.`);
       return false;
     }
   } catch (error) {
     console.error(`Error checking URL (${url}):`, error);
-    return false; // Handle any network errors or other issues as invalid
+    return false; 
   }
 }
 
