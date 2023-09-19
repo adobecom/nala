56eb711b74c5a96e84e839a3ad332c0df75ce865
nateekar <nateekar@adobe.com>
2023-09-18 19:03:27 -0700

    update

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ const config = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
