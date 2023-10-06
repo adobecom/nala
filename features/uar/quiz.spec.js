9ab01ed3706f9038af78dabfdb0ae1b9725cdf3f
Jacky Sun <67350368+JackySun9@users.noreply.github.com>
2023-10-06 13:05:51 -0700

    Update UAR Tests (#182)
    * add test for quiz and result pages
    
    * add test for quiz and result pages
    
    * add test for quiz and result pages
    
    * add POC for analytics test
    
    * update analytics tests and envs
    
    * result envs
    
    * update analytics only on firefox
    
    * add dynamic tests
    
    * add dynamic test for quiz page
    
    * update quiz test
    
    * update test structure
    
    * add test for quiz and result pages
    
    * add POC for analytics test
    
    * update analytics tests and envs
    
    * result envs
    
    * update analytics only on firefox
    
    * add dynamic tests
    
    * add dynamic test for quiz page
    
    * update quiz test
    
    * update test structure
    
    * update uar tests to new format
    
    * update uar tests
    
    * move everything to uar folder
    
    * add dependency for js-yaml
    
    * update test according to latest comments
    
    * update test according to latest comments
    
    * update tests according to the latest comments
    
    * add uar config
    
    * add analytics for uar
    
    * update according to feedbacks
    
    * update tags
    
    * add @cc tag
    
    * update according to feedbacks
    
    * add dynamic tests for uar
    
    * update according to review comments
    
    * fix some typos
    
    * update according to review comments
    
    * merge console log info
    
    * add UI screenshots for UAR
    
    * move view point to test
    
    * update according to feedback
    
    * add analytics test for UAR
    
    * update validation and uar libs
    
    * update libs
    
    * update by feedback
    
    * update some config and tests
    
    * update some config and tests
    
    ---------
    
    Co-authored-by: xiasun <xiasun@xiasuns-MacBook-Pro-2.local>
    Co-authored-by: Aaron Mauchley <mauchley@adobe.com>

--- features/uar/quiz.spec.js
+++ features/uar/quiz.spec.js
@@ -2,6 +2,13 @@ module.exports = {
   name: 'Quiz Recommender Blocks',
   features: [
     {
+      tcid: '000',
+      name: '@quiz basic',
+      path: '/drafts/xiasun/quiz-2/',
+      tags: '@cc @uar-quiz-basic @uar-quiz-static',
+      data: 'data/uar/quiz/quiz-basic.yml',
+    },
+    {
       tcid: '0',
       name: '@quiz single template',
       path: '/drafts/xiasun/quiz-2/',
