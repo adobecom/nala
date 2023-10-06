e72fa1b61a4ee75165b4c2570e58a8b75b6c334f
xiasun <xiasun@xiasuns-MacBook-Pro-2.local>
2023-10-06 10:31:53 -0700

    update some config and tests

--- features/uar/quiz.screenshots.spec.js
+++ features/uar/quiz.screenshots.spec.js
@@ -3,9 +3,9 @@ module.exports = {
   features: [
     {
       tcid: '0',
-      name: '@quiz basic',
+      name: '@quiz screenshots',
       path: '/drafts/xiasun/quiz-2/',
-      tags: '@cc @uar-quiz-basic @uar-quiz-static',
+      tags: '@cc @uar-quiz-screenshots @uar-quiz-static',
       data: 'data/uar/quiz/quiz-basic.yml',
     },
   ],
