53320dcfb6a9ecff31c41e9065e0dc587c43e836
Ancy <sathyava@adobe.com>
2023-10-12 15:44:18 +0530

    commit beforeAfter changes

--- features/helpx/beforeAfter.spec.js
+++ features/helpx/beforeAfter.spec.js
@@ -1,12 +1,12 @@
 module.exports = {
-    name: 'BeforeAfter',
-    features: [
-      {
-        tcid: '0',
-        name: '@beforeAfterValidation',
-        path: '/automation/blocks/before-after-component',
-        envs: '@helpxInternal_live',
-        tags: '@beforeafter @regression @helpx',
-      },
-    ],
-  };
+  name: 'BeforeAfter',
+  features: [
+    {
+      tcid: '0',
+      name: '@beforeAfter',
+      path: '/automation/blocks/before-after-component',
+      envs: '@helpxInternal_live',
+      tags: '@beforeafter @regression @helpx',
+    },
+  ],
+};
--- tests/helpx/beforeafter.test.js
+++ tests/helpx/beforeafter.test.js
@@ -1,60 +1,35 @@
 import { expect, test } from '@playwright/test';
-import parse from '../../libs/parse.js';
-import beforeafter from '../../features/helpx/beforeafter.spec.js';
-import selectors from '../../selectors/helpx/beforeafter.selectors.js';
+import { features } from '../../features/helpx/procedure.spec.js';
+import { BeforeAfter } from '../../selectors/helpx/procedure.selectors.js';
 
-const { name, features } = parse(beforeafter);
-
+let beforeAftr;
 
 test.beforeAll(async({browser})=>{
-     const context = await browser.newContext();
-     //await context.authenticate({hlx_auth_token:''});
-    const page = await context.newPage();
-    await page.setExtraHTTPHeaders({
-        Authorization:'eyJhbGciOiJSUzI1NiIsImtpZCI6Ijdzb2k4N3pkb3NJRnc4b19fbVR5a082QlVRNEZBVGhjaHlyNGZqY1dSbWcifQ.eyJlbWFpbCI6ImhlbGl4QGFkb2JlLmNvbSIsIm5hbWUiOiJIZWxpeCBBZG1pbiIsImlhdCI6MTY4OTkyNjExMiwiaXNzIjoiaHR0cHM6Ly9hZG1pbi5obHgucGFnZS8iLCJhdWQiOiI4M2EzNjM1NS1hZDE3LTRlZDAtODcwMS1lOTlhMzAyMGY4NmEiLCJzdWIiOiJhZG9iZWNvbS9oZWxweC1pbnRlcm5hbCIsImV4cCI6MTcyMTQ2MjExMiwianRpIjoiSDlxMW9aSGlveUZHREp6K0hEVkIrUlRDSnhTdXBzdkZxM3NwTDNZcFFVMXQifQ.X9vg21fTwViIiEmwvIpuGL3WIVfh1GlOoV3g7-5PBU_0vnX0XFJpV1EoQv4eO4BGp0fcVkr53BD2c0LVUO6SwnbmlBFL1QHwgvh9XrZpeUYDM2J6KSCXZgpWDTWxNCUHglBmmBfkK7bDPW_cqPgkLciakipT0Kq44qMhJbXETRZg4NS4vzMKI-gCLm3x0DoRNg8ijjnPhn9M5g8Yh1S8qk6F1F8OQzxOKMXoCMjfTgdfIKI179KMBNwGMAqbxd5EO2JyAcJs4FhxGoIPJyE1Fo3IFhNqb2c20DcctKGj0nI24zzrRaUmGlcddFfXiZ3ZBRpdSt7iWXY-UZNUSLZIzA'
-    });
-    const url = "https://helpx-internal.stage.adobe.com/automation/blocks/before-after-component.html";
-
+  const authToken = process.env.HLX_TKN;
+  const context = await browser.newContext();
+     // Set the authorization token in the header
+    await context.setExtraHTTPHeaders({ 'authorization': `token ${authToken}` });
+   const page = await context.newPage();
+   beforeAftr = new BeforeAfter(page);
+    await page.goto(`${baseURL}${features[0].path}`);
+    await page.waitForLoadState('networkidle');
 })
 
+test.describe('BeforeAfter sanity test suite', () => {
 
-test.describe(`${name}`, () => {
-  features.forEach((props) => {
-    test(`${props.title} (Verify vertical movement )`, async ({}) => {
-      const page = await webContext.newPage();
-      await page.goto(props.url);
-      const beforeafterVertical = await page.locator(selectors['@beforeafter-vertical']);
-      await expect(beforeafterVertical).toBeVisible();
-      //beforeafterVertical.click();
-      
-      beforeafterVertical.scrollTop -= 40;
-      const beforeafterVerticalmoved = await page.locator(selectors['@beforeafter-vertical-moved']);
-      await expect(beforeafterVerticalmoved).toBeVisible();
+  // Procedure Sanity Checks:
+  test(`${features[0].name}, ${features[0].tags}`, async ({ page, baseURL }) => { 
+    console.info(`[BeforeAfter] Checking page: ${baseURL}${features[0].path}`);
+    await test.step('Navigate to BeforeAfter page', async () => {
+    await expect(page).toHaveURL(`${baseURL}${features[0].path}`);
     });
 
+    // Check beforeAfter
+    await expect(beforeAftr.beforeafterVertical).toBeVisible();
+     
+    // Check beforeAfter after vertical movement
+    beforeafterVertical.scrollTop -= 40;
+    await expect(beforeAftr.beforeafterVerticalmoved).toBeVisible();
+
+    });
   });
-});
-
-
-
-
-test(`${props.title} (Verify presence of procedure steps)`, async ({}) => {
-  const page = await context.newPage();
-  await page.goto(props.url);
-  const procedureStep = await page.locator(selectors['@procedure-step']).first();
-  await expect(procedureStep).toBeVisible();
-});
-
-test(`${props.title} (Verify presence of image)`, async ({}) => {
-  const page = await context.newPage();
-  await page.goto(props.url);
-  const procedureImage = await page.locator(selectors['@procedure-image']).first();
-  await expect(procedureImage).toBeVisible();
-});
-
-test(`${props.title} (Verify presence of bold text)`, async ({}) => {
-  const page = await context.newPage();
-  await page.goto(props.url);
-  const procedureTxtBold = await page.locator(selectors['@procedure-text-bold']).first();
-  await expect(procedureTxtBold).toBeVisible();
-});
