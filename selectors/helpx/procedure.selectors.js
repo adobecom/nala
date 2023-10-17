43e7986d5067dcb983e9284eb30617a0efa934a2
ancysr <127083004+ancysr@users.noreply.github.com>
2023-10-17 21:45:53 +0530

    commit helpx internal procedure component (#181)
    * commit helpx internal procedure component
    
    * Incorporating review comments
    
    * commit beforeAfter changes
    
    * handled scenario if auth token not set in env variable
    
    ---------
    
    Co-authored-by: Aaron Mauchley <mauchley@adobe.com>

---
+++ selectors/helpx/procedure.selectors.js
@@ -0,0 +1,12 @@
+exports.Procedure = class Procedure {
+    constructor(page) {
+      this.page = page;
+  
+      // Procedure Selectors:
+      this.procedure = page.locator('.procedure');
+      this.procedureStep = page.locator('li[class=step]');
+      this.procedureImage = page.locator('hli[class=step] img');
+      this.procedureTxtBold = page.locator('li[class=step] strong');
+    }
+  
+  };
