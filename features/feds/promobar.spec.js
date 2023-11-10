9978f66a8c393434c502b1ab44586b33d47c64d5
Dan Chivescu <chadsunvice@gmail.com>
2023-11-10 21:39:14 +0200

    [MWPW-13868] Removed unnecessary spec config

--- features/feds/promobar.spec.js
+++ features/feds/promobar.spec.js
@@ -6,7 +6,6 @@ module.exports = {
       path: [
         '/libs/feds/drafts/qa/promo/feds-promo-bar',
       ],
-      wcagTags: ['wcag2a', 'wcag2aa', 'wwcag21a', 'wcag21aa'],
       browserParams: '?georouting=off',
       envs: '@milo_live',
       tags: '@milo @feds @promobar @smoke @regression',
