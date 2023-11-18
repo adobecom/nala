2d5c8baf03988ec163676cd89f70a5ec3126b20d
Santoshkumar Nateekar <santoshkumar.sn@gmail.com>
2023-11-17 17:48:53 -0800

    Update Merch card test scripts as per the changes : MWPW-138882: Commerce v3 Merch Card (#212)
    * fix circle-ci url
    
    * update merch cards scripts
    
    ---------
    
    Co-authored-by: nateekar <nateekar@adobe.com>

--- selectors/milo/merchcard.block.page.js
+++ selectors/milo/merchcard.block.page.js
@@ -25,42 +25,44 @@ export default class Merchcard {
     this.strikethroughPriceRecurrence = this.inlinePrice2.locator('.price-recurrence');
 
     // merch-card segment locators
-    this.segmentRibbon = this.merchCard.locator('.consonant-SegmentBlade-ribbon');
-    this.segmentTitle = this.segment.locator('.consonant-SegmentBlade-title').nth(0);
-    this.segmentDescription1 = this.segment.locator('.consonant-SegmentBlade-description').nth(0);
-    this.segmentDescription2 = this.segment.locator('.consonant-SegmentBlade-description').nth(1);
+    this.segmentRibbon = this.merchCard.locator('.segment-ribbon');
+    this.segmentTitle = this.segment.locator('h3[slot="heading-xs"]').nth(0);
+    this.segmentDescription1 = this.segment.locator('div[slot="body-xs"] p').nth(0);
+    this.segmentDescription2 = this.segment.locator('div[slot="body-xs"] p').nth(1);
 
     this.linkText1 = this.segmentDescription2.locator('a').nth(0);
     this.linkText2 = this.segmentDescription2.locator('a').nth(1);
 
     // merch-card special offers
-    this.sepcialOffersImage = this.sepcialOffers.locator('.consonant-SpecialOffers-img');
-    this.sepcialOffersRibbon = this.merchCard.locator('.consonant-SpecialOffers-ribbon');
-    this.sepcialOffersTitleH4 = this.sepcialOffers.locator('h4.consonant-SpecialOffers-title');
-    this.sepcialOffersTitleH3 = this.sepcialOffers.locator('h3.consonant-SpecialOffers-title');
+    this.sepcialOffersImage = this.sepcialOffers.locator('div[slot="bg-image"] img');
+    this.sepcialOffersRibbon = this.merchCard.locator('.special-offers-ribbon');
+    this.sepcialOffersTitleH4 = this.sepcialOffers.locator('h5[slot="detail-m"]');
+    this.sepcialOffersTitleH5 = this.sepcialOffers.locator('h4[slot="body-xss"]');
+    this.sepcialOffersTitleH3 = this.sepcialOffers.locator('h3[slot="heading-xs"]');
 
-    this.sepcialOffersDescription1 = this.sepcialOffers.locator('.consonant-SpecialOffers-description').nth(0);
-    this.sepcialOffersDescription2 = this.sepcialOffers.locator('.consonant-SpecialOffers-description').nth(1);
+    this.sepcialOffersDescription1 = this.sepcialOffers.locator('div[slot="body-xs"] p').nth(1);
+    this.sepcialOffersDescription2 = this.sepcialOffers.locator('div[slot="body-xs"] p').nth(2);
 
-    this.sepcialOffersDescription3 = this.sepcialOffers.locator('.consonant-SpecialOffers-description').nth(2);
+    this.sepcialOffersDescription3 = this.sepcialOffers.locator('div[slot="body-xs"] p').nth(3);
     this.sepcialOffersLinkText3 = this.sepcialOffersDescription3.locator('a').nth(0);
 
     this.seeTermsTextLink = this.merchCard.locator('a:has-text("See terms")');
 
     // merch-card plans locators
-    this.productIcon = this.plans.locator('.consonant-MerchCard-ProductIcon');
-    this.plansRibbon = this.plans.locator('.consonant-PlansCard-ribbon');
-    this.plansCardTitleH3 = this.plans.locator('h3.consonant-PlansCard-title');
-    this.plansCardTitleH5 = this.plans.locator('h5.consonant-PlansCard-title');
-    this.plansCardDescription2 = this.plans.locator('.consonant-PlansCard-description').nth(1);
+    //this.productIcon = this.plans.locator('#shadow-root div.icons');
+    this.productIcon = this.plans.locator('img');
+    this.plansRibbon = this.plans.locator('.plans-ribbon');
+    this.plansCardTitleH3 = this.plans.locator('h3[slot="heading-xs"]');
+    this.plansCardTitleH5 = this.plans.locator('h5[slot="detail-m"]');
+    this.plansCardDescription2 = this.plans.locator('div[slot="body-xs"] p').nth(2);
     this.seePlansTextLink = this.merchCard.locator('a:has-text("See plan & pricing details")');
     
     // merch-card footer sections
-    this.footer = this.merchCard.locator('.consonant-CardFooter')    
-    this.footerCheckbox = this.merchCard.locator('.checkbox-container .checkmark')
-    this.footerCheckboxLabel = this.merchCard.locator('.checkbox-container .checkbox-label')
-    this.secureTransactionIcon = this.merchCard.locator('.secure-transaction-icon')
-    this.secureTransactionLabel = this.merchCard.locator('.secure-transaction-label')
+    this.footer = this.merchCard.locator('div[slot="footer"]');  
+    this.footerCheckbox = this.page.locator('#stock-checkbox input[type="checkbox"]');
+    this.footerCheckboxLabel = this.merchCard.locator('#stock-checkbox');
+    this.secureTransactionIcon = this.merchCard.locator('.secure-transaction-icon');
+    this.secureTransactionLabel = this.merchCard.locator('.secure-transaction-label');
     this.footerOutlineButton = this.merchCard.locator('a.con-button.outline'); 
     this.footerBlueButton = this.merchCard.locator('a.con-button.blue').nth(0);
     this.footerBlueButton2 = this.merchCard.locator('a.con-button.blue').nth(1);
@@ -68,13 +70,13 @@ export default class Merchcard {
     // merch-card attributes
     this.attributes = {
       'segmentRibbon': {
-        'style': 'background-color: rgb(237, 204, 45); color: rgb(0, 0, 0);',
+        'style': 'background-color: #EDCC2D; color: #000000; ',
       },
       'specialOfferRibbon': {
-        'style': 'background-color: rgb(246, 141, 46); color: rgb(0, 0, 0);',
+        'style': 'background-color: #F68D2E; color: #000000; ',
       },
       'plansRibbon': {
-        'style': 'background-color: rgb(246, 141, 46); color: rgb(0, 0, 0);',
+        'style': 'background-color: #EDCC2D; color: #000000; ',
       },
     };
   }
