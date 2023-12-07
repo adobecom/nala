f6fc0c3830a79b422eae306268172cb0683313a9
nateekar <nateekar@adobe.com>
2023-12-07 02:36:22 -0800

    update cc tests and add daily run

---
+++ dailyrun.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# get the input argument i.e. config.js file name
+PROJECT_NAME=$1 
+
+# Construct the full path to the config.js file
+CONFIG_FILE_PATH="./configs/${PROJECT_NAME}.config.js"
+
+# Check if the config.js file exists
+if [ -f "${CONFIG_FILE_PATH}" ]; then
+    echo "*** Running Playwright tests with config: ${CONFIG_FILE_PATH} ***"
+    npx playwright test --config="${CONFIG_FILE_PATH}" --project="${PROJECT_NAME}-live-chrome"
+    npx playwright test --config="${CONFIG_FILE_PATH}" --project="${PROJECT_NAME}-live-firefox"
+else
+    echo "Config file: ${CONFIG_FILE_PATH} not found, running default tests"
+    exit 1
+fi
+
+
--- tests/cc/ostprices.test.js
+++ tests/cc/ostprices.test.js
@@ -8,56 +8,73 @@ test.describe('test Offer selector tool price formats and checkout flows in US &
   ost = new Ostprices(page);
   });
 
+  // Test 0 : OST Currency Format price buy emailcta
   test(`${features[0].name},${features[0].tags}`, async ({ page, baseURL }) => {
     console.info(`[Test Page]: ${baseURL}${features[0].path}`);
-    await test.step('page marquee shows the CC price and CTA US locale', async () => {
+
+    await test.step('step-1: page marquee shows the CC price and CTA US locale', async () => {
       await page.goto(`${baseURL}${features[0].path}`);
       await page.waitForLoadState('domcontentloaded');
       await expect(page).toHaveURL(`${baseURL}${features[0].path}`);
-  });
+    });
     
-  await test.step('Test CC all apps price and UCv3 email checkout, buy button checks in marquee', async () => {
-   await page.waitForLoadState();
-   await expect(await ost.productname).toBeTruthy();
-   await expect(ost.uscountrycurrencysymbol).toBeTruthy();
-   await expect(ost.price).toBeVisible();
-   await expect(ost.buynowcta).toBeVisible();
-   await expect(ost.pricebeforedelimiter).toBeVisible();
-   await page.locator('//a[@daa-ll="Buy now-1|Make Create Amazing"]').click();
-   await expect(page).toHaveURL(new RegExp('^https://commerce.adobe.com/store/'));
-  });
-});
+    await test.step('step-2: Test CC all apps price and UCv3 email checkout, buy button checks in marquee', async () => {
+      await page.waitForLoadState();
+      
+      expect(await ost.productName).toBeTruthy();
+      expect(await ost.usCurrencySymbol).toBeTruthy();
 
+      await expect(await ost.price).toBeVisible();
+      await expect(await ost.emailBuynowCta).toBeVisible();
+      await expect(await ost.priceBeforeDelimiter).toBeVisible();
+  
+      // click 'Buy now' (ucv3 checkout) button and verify the checkout page.
+      await ost.emailBuynowCta.click()
+      await page.waitForTimeout(1000);  
+      await expect(page).toHaveURL(new RegExp('^https://commerce.adobe.com/store/'));
+    });
+  });
+
+  // Test 1 : OST CC all apps price buy segment cta
  test(`${features[1].name},${features[1].tags}`, async ({ page, baseURL }) => {
    console.info(`[Test Page]: ${baseURL}${features[1].path}`);
-   await test.step('page checks with month and year CC subscription, checkout', async () => {
+
+   await test.step('step-1: page checks with month and year CC subscription, checkout', async () => {
     await page.goto(`${baseURL}${features[1].path}`);
     await page.waitForLoadState('domcontentloaded');
     await expect(page).toHaveURL(`${baseURL}${features[1].path}`);
   });
   
-  await test.step('Verify the month and year price display , CTA for UCv3 segment checkout destination', async () => {
+  await test.step('step-2: Verify the month and year price display , CTA for UCv3 segment checkout destination', async () => {
    await page.waitForLoadState();
-   await expect(ost.productname_1).toBeTruthy();
-   await expect(ost.uscountrycurrencysymbol).toBeTruthy();
-   await expect(ost.cc_pricebeforedelimiter).toBeVisible();
-   await expect(ost.cc_priceafterdelimiter).toBeVisible();
-   await expect(ost.cc_licensecommitment).toBeVisible();
-   await expect(ost.cc_numberoflicenses).toBeVisible();
-   await expect(ost.productname_2).toBeTruthy();
-   await expect(ost.uscountrycurrencysymbol).toBeTruthy();
-   await expect(ost.ps_pricebeforedelimiter).toBeVisible();
-   await expect(ost.ps_priceafterdelimiter).toBeVisible();
-   await expect(ost.ps_licensecommitment).toBeVisible();
-   await expect(ost.ps_numberoflicenses).toBeVisible();
-   await page.locator('//a[@daa-ll="Buy now-1|Generative AI.This c"]').click();
+   expect(ost.productName1).toBeTruthy();
+   expect(ost.usCurrencySymbol).toBeTruthy();
+
+   await expect(ost.ccPriceBeforeDelimiter).toBeVisible();
+   await expect(ost.ccPriceAfterDelimiter).toBeVisible();
+   await expect(ost.ccLicenseCommitment).toBeVisible();
+   await expect(ost.ccNumberOfLicenses).toBeVisible();
+
+   await expect(ost.productName2).toBeTruthy();
+   await expect(ost.usCurrencySymbol).toBeTruthy();
+
+   await expect(ost.psPriceBeforeDelimiter).toBeVisible();
+   await expect(ost.psPriceAfterDelimiter).toBeVisible();
+   await expect(ost.psLicenseCommitment).toBeVisible();
+   await expect(ost.psNumberOfLicenses).toBeVisible();
+  
+   // click 'Buy now' (ucv3 checkout, checkout workflow = email ) button and verify the checkout page.   
+   await ost.segmentationBuynowCta.click() 
+   await page.waitForTimeout(1000);   
    await expect(page).toHaveURL(new RegExp('^https://commerce.adobe.com/store/segmentation'));
   });
 });
 
+  // Test 2 : OST student price buy cta
  test(`${features[2].name},${features[2].tags}`, async ({ page, baseURL }) => {
    console.info(`[Test Page]: ${baseURL}${features[2].path}`);
-   await test.step('Light room STE edition price display with email checkout cta destination', async () => {
+
+   await test.step('step-1: Light room STE edition price display with email checkout cta destination', async () => {
      await page.goto(`${baseURL}${features[2].path}`);
      await page.waitForLoadState('domcontentloaded');
      await expect(page).toHaveURL(`${baseURL}${features[2].path}`);
@@ -65,17 +82,21 @@ test.describe('test Offer selector tool price formats and checkout flows in US &
   
   await test.step('step-2: verify LR STU price and email CTA', async () => {
    await page.waitForLoadState();
-   await expect(ost.STEProductname).toBeTruthy();
-   await expect(ost.uscountrycurrencysymbol).toBeTruthy();
-   await expect(ost.STE_LR_price).toBeVisible();
-   await page.locator('//a[@daa-ll="Get started-1|Adobe Light room"]').click();
+   await expect(ost.SteProductName).toBeTruthy();
+   await expect(ost.usCurrencySymbol).toBeTruthy();
+   await expect(ost.SteLrPrice).toBeVisible();
+
+   await ost.emailBuynowCta.click();
+   await page.waitForTimeout(1000);
    await expect(page).toHaveURL(new RegExp('^https://commerce.adobe.com/store/email'));
   });
 });
 
+ // Test 3 : OST strike-through price buy cta
  test(`${features[3].name},${features[3].tags}`, async ({ page, baseURL }) => {
    console.info(`[Test Page]: ${baseURL}${features[3].path}`);
-   await test.step('page authored with CC product strike through price', async () => {
+
+   await test.step('step-1: page authored with CC product strike through price', async () => {
      await page.goto(`${baseURL}${features[3].path}`);
      await page.waitForLoadState('domcontentloaded');
      await expect(page).toHaveURL(`${baseURL}${features[3].path}`);
@@ -83,21 +104,26 @@ test.describe('test Offer selector tool price formats and checkout flows in US &
   
   await test.step('step-2: verify CC strike through prices are showup correctly with valid store bundle CTA destination', async () => {
    await page.waitForLoadState();
-   await expect(ost.producatname_3).toBeVisible();
-   await expect(ost.uscountrycurrencysymbol).toBeTruthy();
-   await expect(ost.pricelabel_actual).toBeVisible();
-   await expect(ost.strikethroughproperty).toBeTruthy();
-   await expect(ost.strikethrouthprice).toBeVisible();
-   await expect(ost.pricelabel_now).toBeVisible();
-   await expect(ost.purchaseCTAbandle).toBeVisible();
-   await page.locator('//a[@daa-ll="choose a plan-1|Actual price"]').click();
-   await expect(page).toHaveURL(new RegExp('^https://commerce.adobe.com/store/bundle'));
+   await expect(ost.productName3).toBeVisible();
+   await expect(ost.usCurrencySymbol).toBeTruthy();
+   await expect(ost.priceLabelActual).toBeVisible();
+   await expect(ost.strikeThroughProperty).toBeTruthy();
+   await expect(ost.strikeThroughPrice).toBeVisible();
+   await expect(ost.priceLabelNow).toBeVisible();
+
+   // click 'Choose a plan' (ucv3 checkout, checkout workflow = bundle ) button and verify the checkout page.   
+   await expect(ost.bundleBuynowCta).toBeVisible();
+   await ost.bundleBuynowCta.click();
+   await page.waitForTimeout(1000);
+   await expect(page).toHaveURL(new RegExp('^https://commerce.adobe.com/store/'));
  });
 });
 
+ // Test 4 : OST month-year-prices buy cta
  test(`${features[4].name},${features[4].tags}`, async ({ page, baseURL }) => {
    console.info(`[Test Page]: ${baseURL}${features[4].path}`);
-   await test.step('Product - photoshop month, year price with CTAs', async () => {
+
+   await test.step('step-1: Product - photoshop month, year price with CTAs', async () => {
      await page.goto(`${baseURL}${features[4].path}`);
      await page.waitForLoadState('domcontentloaded');
      await expect(page).toHaveURL(`${baseURL}${features[4].path}`);
@@ -105,59 +131,76 @@ test.describe('test Offer selector tool price formats and checkout flows in US &
   
   await test.step('step-2: Photoshop product month, year price with CTAs', async () => {
    await page.waitForLoadState();
-   await expect(ost.headingdiscription).toBeTruthy();
-   await expect(ost.headingdiscription2).toBeTruthy();
-   await expect(ost.firstcardheading).toBeVisible();
-   await expect(ost.individualPSpricemonth).toBeVisible();
-   await expect(ost.individualPSpriceyear).toBeVisible();
-   await page.locator('//a[@class="con-button placeholder-resolved"]').first().click();
+   await expect(ost.headingDiscription).toBeTruthy();
+   await expect(ost.headingDiscription2).toBeTruthy();
+   await expect(ost.firstCardHeading).toBeVisible();
+   await expect(ost.individualPsPriceMonth).toBeVisible();
+   await expect(ost.individualPsPriceYear).toBeVisible();
+
+   await ost.emailBuynowCta.click();
+   await page.waitForTimeout(1000);
    await expect(page).toHaveURL(new RegExp('^https://commerce.adobe.com/store/'));
   });
 });
 
+ // Test 5 : OST JP currency format-price buy email cta
  test(`${features[5].name},${features[5].tags}`, async ({ page, baseURL }) => {
    console.info(`[Test Page]: ${baseURL}${features[5].path}`);
-   await test.step('page marquee shows the CC price and CTA for JP', async () => {
+
+   await test.step('step-1: page marquee shows the CC price and CTA for JP', async () => {
      await page.goto(`${baseURL}${features[5].path}`);
      await page.waitForLoadState('domcontentloaded');
      await expect(page).toHaveURL(`${baseURL}${features[5].path}`);
   });
   
-  await test.step('Test CC all apps price and UCv3 email checkout, buy button checks in marquee for JP', async () => {
+  await test.step('step-2: Test CC all apps price and UCv3 email checkout, buy button checks in marquee for JP', async () => {
    await page.waitForLoadState();
-   await expect(ost.productname).toBeVisible();
-   await expect(ost.jpcountrycurrencysymbol).toBeVisible();
-   await expect(ost.jpproductprice).toBeVisible();
-   await page.locator('//a[@daa-ll="Buy now-1|Make Create Amazing"]').click();
+   await expect(ost.productName).toBeVisible();
+   await expect(ost.jpCurrencySymbol).toBeVisible();
+   await expect(ost.jpProductPrice).toBeVisible();
+
+   // click 'Buy now' (ucv3 checkout, checkout workflow = email ) button and verify the checkout page.   
+   await expect(ost.emailBuynowCta).toBeVisible();
+   await ost.emailBuynowCta.click();
+   await page.waitForTimeout(1000);
    await expect(page).toHaveURL(new RegExp('^https://commerce.adobe.com/store/'));
    await expect(page).toHaveURL(new RegExp('co=JP&lang=ja'));
   });
 });
 
+ // Test 6 : OST JP currency format-price buy email cta
  test(`${features[6].name},${features[6].tags}`, async ({ page, baseURL }) => {
    console.info(`[Test Page]: ${baseURL}${features[6].path}`);
-   await test.step('product with month, year subscriptions in JP', async () => {
+
+   await test.step('step-1: product with month, year subscriptions in JP', async () => {
      await page.goto(`${baseURL}${features[6].path}`);
      await page.waitForLoadState('domcontentloaded');
      await expect(page).toHaveURL(`${baseURL}${features[6].path}`);
   });
   
-  await test.step('JP locale monthly, year subscriptions', async () => {
+  await test.step('step-2: JP locale monthly, year subscriptions', async () => {
    await page.waitForLoadState();
-   await expect(ost.productname_1).toBeVisible();
-   await expect(ost.productname_2).toBeVisible();
+   await expect(ost.productName1).toBeVisible();
+   await expect(ost.productName2).toBeVisible();
    await expect(ost.cc_pricemonth_jp).toBeVisible();
    await expect(ost.ps_priceyear_jp).toBeVisible();
-   await expect(ost.jpcountrycurrencysymbol).toBeTruthy();
-   await page.locator('//a[@daa-ll="Buy now-1|Generative AI.This c"]').click();
+   await expect(ost.jpCurrencySymbol).toBeTruthy();
+
+
+   // click 'Buy now' (ucv3 checkout, checkout workflow = segmentation ) button and verify the checkout page.   
+   await expect(ost.segmentationBuynowCta).toBeVisible();
+   await ost.segmentationBuynowCta.click();
+   await page.waitForTimeout(1000);
    await expect(page).toHaveURL(new RegExp('^https://commerce.adobe.com/store/'));
    await expect(page).toHaveURL(new RegExp('co=JP&lang=ja'));
   });
 });
 
+ // Test 7 : OST JP CC all apps price buy segment cta
  test(`${features[7].name},${features[7].tags}`, async ({ page, baseURL }) => {
    console.info(`[Test Page]: ${baseURL}${features[7].path}`);
-   await test.step('page has JP STE prices for LR product', async () => {
+
+   await test.step('step-1: page has JP STE prices for LR product', async () => {
      await page.goto(`${baseURL}${features[7].path}`);
      await page.waitForLoadState('domcontentloaded');
      await expect(page).toHaveURL(`${baseURL}${features[7].path}`);
@@ -165,54 +208,68 @@ test.describe('test Offer selector tool price formats and checkout flows in US &
   
   await test.step('step-2: Verify STE price for Lighthouse in JP currency', async () => {
    await page.waitForLoadState();
-   await expect(ost.STEProductname).toBeTruthy();
-   await expect(ost.jpcountrycurrencysymbol).toBeTruthy();
-   await expect(ost.STE_LR_JP_price).toBeVisible();
-   await page.locator('//a[@daa-ll="Get started-1|Adobe Light room"]').click();
+   await expect(ost.SteProductName).toBeTruthy();
+   await expect(ost.jpCurrencySymbol).toBeTruthy();
+   await expect(ost.SteLrJpPrice).toBeVisible();
+
+  // click 'Get started' (ucv3 checkout, checkout workflow = email ) button and verify the checkout page.   
+   await expect(ost.emailBuynowCta).toBeVisible();
+   await ost.emailBuynowCta.click();
+   await page.waitForTimeout(1000);
    await expect(page).toHaveURL(new RegExp('^https://commerce.adobe.com/store/email'));
    await expect(page).toHaveURL(new RegExp('co=JP&lang=ja'));
   });
 });
 
+ // Test 8 : OST JP CC student-price buy cta
  test(`${features[8].name},${features[8].tags}`, async ({ page, baseURL }) => {
   console.info(`[Test Page]: ${baseURL}${features[8].path}`);
-  await test.step('product with month, year subscriptions in JP', async () => {
+
+  await test.step('step-1: product with month, year subscriptions in JP', async () => {
      await page.goto(`${baseURL}${features[8].path}`);
      await page.waitForLoadState('domcontentloaded');
      await expect(page).toHaveURL(`${baseURL}${features[8].path}`);
   });
   
-  await test.step('JP locale monthly, year subscriptions', async () => {
+  await test.step('step-2: JP locale monthly, year subscriptions', async () => {
    await page.waitForLoadState();
-   await expect(ost.producatname_3).toBeVisible();
-   await expect(ost.jpcountrycurrencysymbol).toBeTruthy();
-   await expect(ost.pricelabel_actual).toBeVisible();
-   await expect(ost.strikethroughproperty).toBeTruthy();
-   await expect(ost.strikethroughprice_jp_1).toBeVisible();
-   await expect(ost.pricelabel_now).toBeVisible();
-   await expect(ost.purchaseCTAbandle).toBeVisible();
-   await page.locator('//a[@daa-ll="choose a plan-1|Actual price"]').click();
+   await expect(ost.productName3).toBeVisible();
+   await expect(ost.jpCurrencySymbol).toBeTruthy();
+   await expect(ost.priceLabelActual).toBeVisible();
+   await expect(ost.strikeThroughProperty).toBeTruthy();
+   await expect(ost.strikeThroughPriceJp1).toBeVisible();
+   await expect(ost.priceLabelNow).toBeVisible();
+
+  // click 'Choose a plan' (ucv3 checkout, checkout workflow = bundle ) button and verify the checkout page.   
+   await expect(ost.bundleBuynowCta).toBeVisible();
+   await ost.bundleBuynowCta.click();
+   await page.waitForTimeout(1000);
    await expect(page).toHaveURL(new RegExp('^https://commerce.adobe.com/store/bundle'));
    await expect(page).toHaveURL(new RegExp('co=JP&lang=ja'));
   });
 });
 
+ // Test 9 : OST JP CC student-price buy cta
  test(`${features[9].name},${features[9].tags}`, async ({ page, baseURL }) => {
   console.info(`[Test Page]: ${baseURL}${features[9].path}`);
-  await test.step('product with month, year subscriptions in JP', async () => {
+
+  await test.step('step-1: product with month, year subscriptions in JP', async () => {
    await page.goto(`${baseURL}${features[9].path}`);
    await page.waitForLoadState('domcontentloaded');
    await expect(page).toHaveURL(`${baseURL}${features[9].path}`);
   });
   
-  await test.step('JP locale monthly, year subscriptions', async () => {
+  await test.step('step-2: JP locale monthly, year subscriptions', async () => {
    await page.waitForLoadState();
-   await expect(ost.headingdiscription).toBeTruthy();
-   await expect(ost.headingdiscription2).toBeTruthy();
-   await expect(ost.firstcardheading).toBeVisible();
-   await expect(ost.individualPSpricemonth_jp).toBeVisible();
-   await expect(ost.individualPSpriceyear_jp).toBeVisible();
-   await page.locator('//a[@class="con-button placeholder-resolved"]').first().click();
+   await expect(ost.headingDiscription).toBeTruthy();
+   await expect(ost.headingDiscription2).toBeTruthy();
+   await expect(ost.firstCardHeading).toBeVisible();
+   await expect(ost.individualPsPriceMonthJP).toBeVisible();
+   await expect(ost.individualPsPriceYearJP).toBeVisible();
+
+   await expect(ost.emailBuynowCta).toBeVisible();
+   await ost.emailBuynowCta.click();
+   await page.waitForTimeout(1000);
    await expect(page).toHaveURL(new RegExp('^https://commerce.adobe.com/store/'));
    await expect(page).toHaveURL(new RegExp('co=JP&lang=ja'));
   });
