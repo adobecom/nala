06efc03015364748d84643e5f5e8969e6f779e2b
Santoshkumar Nateekar <santoshkumar.sn@gmail.com>
2023-12-08 13:50:37 -0800

    [MWPW-140301] Introduce Daily runs for Nala Blog Automation scripts  (#227)
    * blog daily run
    
    * remove skips
    
    * update daily run time to 7-30 am pst
    
    ---------
    
    Co-authored-by: nateekar <nateekar@adobe.com>

--- configs/blog.config.js
+++ configs/blog.config.js
@@ -10,6 +10,7 @@ const envs = require('../envs/envs.js');
 const config = {
   testDir: '../tests/blog',
   outputDir: '../test-results',
+  globalSetup: '../global.setup.js',
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
@@ -30,7 +31,7 @@ const config = {
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI
     ? [['github'], ['../utils/reporters/json-reporter.js'], ['../utils/reporters/json-reporter.js']]
-    : [['html', { outputFolder: 'test-html-results' }]],
+    : [['html', { outputFolder: 'test-html-results' }],['list'],['../utils/reporters/base-reporter.js']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
