fbdba1a6a8bb0ce33c44b4095bea61b04c03ed14
cod23684 <cod23684@adobe.com>
2023-12-22 09:57:26 +0100

    remove comments

--- selectors/acom/home.page.js
+++ selectors/acom/home.page.js
@@ -9,17 +9,6 @@ export default class AcomHomePage {
       // Jarvis popup chat
       this.jarvisContainer = page.locator('iframe.adbmsgContentIframe');
       
-      // // Footer locators
-      // this.globalFooter = this.page.locator('.global-footer');
-      // this.changeLanguageText = this.globalFooter.locator('.feds-regionPicker-text');
-      // this.changeLanguageButton = this.globalFooter.locator('.feds-regionPicker');
-      // this.changeLanguageList = this.globalFooter.locator('.fragment p');
-      // this.copywright = this.globalFooter.locator('.feds-footer-privacySection');
-      // this.privacyLink = this.globalFooter.locator('a.feds-footer-privacyLink').nth(0);
-      // this.termsOfUseLink = this.globalFooter.locator('a.feds-footer-privacyLink').nth(1);
-      // this.cookiePreferencesLink = this.globalFooter.locator('a.feds-footer-privacyLink').nth(2);
-      // this.doNotSellInformationLink = this.globalFooter.locator('a.feds-footer-privacyLink').nth(3);
-      // this.adChoicesLink = this.globalFooter.locator('a.feds-footer-privacyLink').nth(4);
     }
   }
   
