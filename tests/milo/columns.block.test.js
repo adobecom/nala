1e257a72b24df28b7418b5984e24c2c7b701a364
Santoshkumar Nateekar <santoshkumar.sn@gmail.com>
2024-01-03 11:09:14 -0800

    [MWPW-140873] Refactor automation test scripts (#233)
    * update quote block
    
    * update
    
    * column blocks refactor
    
    * howto blocks refactor
    
    * rm space
    
    * zpattern block test
    
    * update html extn
    
    ---------
    
    Co-authored-by: nateekar <nateekar@adobe.com>

--- tests/milo/columns.block.test.js
+++ tests/milo/columns.block.test.js
@@ -1,17 +1,22 @@
 /* eslint-disable import/named */
 import { expect, test } from '@playwright/test';
+import { WebUtil } from '../../libs/webutil.js';
 import { features } from '../../features/milo/columns.block.spec.js';
 import ColumnsBlock from '../../selectors/milo/columns.block.page.js';
 
 let column;
+let webUtil;
 
 test.describe('Milo Columns Block test suite', () => {
   test.beforeEach(async ({ page }) => {
     column = new ColumnsBlock(page);
+    webUtil = new WebUtil(page);
   });
 
+  // Test 0 : Column default block
   test(`${features[0].name},${features[0].tags}`, async ({ page, baseURL }) => {
     console.info(`[Test Page]: ${baseURL}${features[0].path}`);
+    const { data } = features[0];
 
     await test.step('step-1: Go to Columns block test page', async () => {
       await page.goto(`${baseURL}${features[0].path}`);
@@ -20,13 +25,26 @@ test.describe('Milo Columns Block test suite', () => {
     });
 
     await test.step('step-2: Verify Columns block content/specs', async () => {
-      const { data } = features[0];
-      expect(await column.verifyColumns('columns', data)).toBeTruthy();
+      // verify colums visibility, rows count, columns count and text content
+      await expect(await column.column).toBeVisible();      
+
+      await expect(await column.rows).toHaveCount(data.rows);
+      await expect(await column.columns).toHaveCount(data.columns);
+
+      await expect(await column.columns.nth(0)).toContainText(data.col0);
+      await expect(await column.columns.nth(1)).toContainText(data.col1);
+      await expect(await column.columns.nth(2)).toContainText(data.col2);
+      
+      // verify the css and attributes
+      expect(await webUtil.verifyCSS_(await column.rows.nth(0), column.cssProperties['.columns > .row'])).toBeTruthy();
+      expect(await webUtil.verifyAttributes_(await column.column, column.attProperties.columns)).toBeTruthy();
     });
   });
 
+  // Test 1 : Columns (contained) block  
   test(`${features[1].name},${features[1].tags}`, async ({ page, baseURL }) => {
     console.info(`[Test Page]: ${baseURL}${features[1].path}`);
+    const { data } = features[1];
 
     await test.step('step-1: Go to Columns block test page', async () => {
       await page.goto(`${baseURL}${features[1].path}`);
@@ -35,13 +53,25 @@ test.describe('Milo Columns Block test suite', () => {
     });
 
     await test.step('step-2: Verify Columns(contained) block content/specs', async () => {
-      const { data } = features[1];
-      expect(await column.verifyColumns('columns(contained)', data)).toBeTruthy();
+      // verify colums visibility, rows count, columns count and text content
+      await expect(await column.column).toBeVisible();      
+
+      await expect(await column.rows).toHaveCount(data.rows);
+      await expect(await column.columns).toHaveCount(data.columns);
+
+      await expect(await column.columns.nth(0)).toContainText(data.col0);
+      await expect(await column.columns.nth(1)).toContainText(data.col1);
+
+      // verify the css and attributes
+      expect(await webUtil.verifyCSS_(await column.column, column.cssProperties['.columns.contained'])).toBeTruthy();
+      expect(await webUtil.verifyAttributes_(await column.column, column.attProperties['columns-contained'])).toBeTruthy();
     });
   });
 
+  // Test 2 : Columns (contained,middle) block    
   test(`${features[2].name},${features[2].tags}`, async ({ page, baseURL }) => {
     console.info(`[Test Page]: ${baseURL}${features[2].path}`);
+    const { data } = features[2];
 
     await test.step('step-1: Go to Columns block test page', async () => {
       await page.goto(`${baseURL}${features[2].path}`);
@@ -50,13 +80,25 @@ test.describe('Milo Columns Block test suite', () => {
     });
 
     await test.step('step-2: Verify Columns(contained,middle) block content/specs', async () => {
-      const { data } = features[2];
-      expect(await column.verifyColumns('columns(contained,middle)', data)).toBeTruthy();
+      // verify colums visibility, rows count, columns count and text content
+      await expect(await column.column).toBeVisible();      
+
+      await expect(await column.rows).toHaveCount(data.rows);
+      await expect(await column.columns).toHaveCount(data.columns);
+
+      await expect(await column.columns.nth(0)).toContainText(data.col0);
+      await expect(await column.columns.nth(1)).toContainText(data.col1); 
+      
+      // verify the css and attributes
+      expect(await webUtil.verifyCSS_(await column.column, column.cssProperties['.columns.contained'])).toBeTruthy();
+      expect(await webUtil.verifyAttributes_(await column.column, column.attProperties['columns-contained-middle'])).toBeTruthy();      
     });
   });
 
+  // Test 3 : Columns (table) block   
   test(`${features[3].name},${features[3].tags}`, async ({ page, baseURL }) => {
     console.info(`[Test Page]: ${baseURL}${features[3].path}`);
+    const { data } = features[3];
 
     await test.step('step-1: Go to Columns block test page', async () => {
       await page.goto(`${baseURL}${features[3].path}`);
@@ -65,13 +107,29 @@ test.describe('Milo Columns Block test suite', () => {
     });
 
     await test.step('step-2: Verify Columns(table) block content/specs', async () => {
-      const { data } = features[3];
-      expect(await column.verifyColumns('columns(table)', data)).toBeTruthy();
+      // verify colums visibility, rows count, columns count and text content
+      await expect(await column.column).toBeVisible();      
+
+      await expect(await column.rows).toHaveCount(data.rows);
+      await expect(await column.columns).toHaveCount(data.columns);
+
+      await expect(await column.columns.nth(0)).toContainText(data.col0);
+      await expect(await column.columns.nth(1)).toContainText(data.col1);
+      await expect(await column.columns.nth(2)).toContainText(data.col2);
+      await expect(await column.columns.nth(3)).toContainText(data.col3);
+      
+      // verify the css and attributes
+      expect(await webUtil.verifyCSS_(await column.rows.nth(0), column.cssProperties['.columns.table > .row:first-child'])).toBeTruthy();
+      expect(await webUtil.verifyCSS_(await column.rows.nth(1), column.cssProperties['.columns.table > .row'])).toBeTruthy();
+
+      expect(await webUtil.verifyAttributes_(await column.column, column.attProperties['columns-table'])).toBeTruthy();
     });
   });
 
+  // Test 4 : Columns (contained,table) block   
   test(`${features[4].name},${features[4].tags}`, async ({ page, baseURL }) => {
     console.info(`[Test Page]: ${baseURL}${features[4].path}`);
+    const { data } = features[4];
 
     await test.step('step-1: Go to Columns block test page', async () => {
       await page.goto(`${baseURL}${features[4].path}`);
@@ -80,8 +138,22 @@ test.describe('Milo Columns Block test suite', () => {
     });
 
     await test.step('step-2: Verify Columns(contained,table) block content/specs', async () => {
-      const { data } = features[4];
-      expect(await column.verifyColumns('columns(contained,table)', data)).toBeTruthy();
+      // verify colums visibility, rows count, columns count and text content
+      await expect(await column.column).toBeVisible();      
+
+      await expect(await column.rows).toHaveCount(data.rows);
+      await expect(await column.columns).toHaveCount(data.columns);
+
+      await expect(await column.columns.nth(0)).toContainText(data.col0);
+      await expect(await column.columns.nth(1)).toContainText(data.col1);
+      await expect(await column.columns.nth(2)).toContainText(data.col2);
+      await expect(await column.columns.nth(3)).toContainText(data.col3);
+      
+      // verify the css and attributes
+      expect(await webUtil.verifyCSS_(await column.rows.nth(0), column.cssProperties['.columns.table > .row:first-child'])).toBeTruthy();
+      expect(await webUtil.verifyCSS_(await column.rows.nth(1), column.cssProperties['.columns.table > .row'])).toBeTruthy();
+
+      expect(await webUtil.verifyAttributes_(await column.column, column.attProperties['columns-contained-table'])).toBeTruthy();
     });
   });
 });
