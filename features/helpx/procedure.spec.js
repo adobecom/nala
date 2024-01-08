79451391c53bee1b550a650df178799560625acd
kirupacommit <94790097+kirupacommit@users.noreply.github.com>
2024-01-08 11:23:08 +0530

    Helpx_Milo Components Script Fixes (#235)
    * Helpx_Milo Components Script Fixes
    
    * removed Base reporter from helpx
    
    * Added Base reporter to the helpx Config File
    
    * Added daily Run yml file and additional test cases on draft page
    
    * Draftpgae_MinorChanges on spec.js and test.js file
    
    * Helpx_Test cases Name changes and Added more test cases for Procedure component
    
    * Helpx Config variable changed to config
    
    * procedure_test case name changes
    
    ---------
    
    Co-authored-by: Kirupaagar R M <kirupaagarrm@adobe.com>

--- features/helpx/procedure.spec.js
+++ features/helpx/procedure.spec.js
@@ -7,6 +7,78 @@ module.exports = {
         path: '/automation/blocks/procedure',
         tags: '@procedure @regression @helpx',
       },
+      {
+        tcid: '1',
+        name: '@procedureComponentTitleValidation',
+        path: '/automation/blocks/procedure/procedure-step-component',
+        tags: '@procedure @regression @helpx',
+      },
+      {
+        tcid: '2',
+        name: '@procedureComponentCaseSensitive',
+        path: '/automation/blocks/procedure/procedure-step-component',
+        tags: '@procedure @regression @helpx',
+      },
+      {
+        tcid: '3',
+        name: '@procedureComponentWrongProcedureClassName',
+        path: '/automation/blocks/procedure/procedure-step-component',
+        tags: '@procedure @regression @helpx',
+      },
+      {
+        tcid: '4',
+        name: '@procedureComponentDraftFunctionality',
+        path: '/automation/blocks/procedure/procedure-step-component',
+        tags: '@procedure @regression @helpx',
+      },
+      {
+        tcid: '5',
+        name: '@procedureComponentTextProperties',
+        path: '/automation/blocks/procedure/procedure-step-component',
+        tags: '@procedure @regression @helpx',
+      },
+      {
+        tcid: '6',
+        name: '@procedureComponentVideo',
+        path: '/automation/blocks/procedure/procedure-step-component',
+        tags: '@procedure @regression @helpx',
+      },
+      {
+        tcid: '1',
+        name: '@procedureAndStepValidation',
+        path: '/automation/blocks/procedure/procedure-step-component',
+        tags: '@procedure @regression @helpx',
+      },
+      {
+        tcid: '7',
+        name: '@procedureComponentFileDownload',
+        path: '/automation/blocks/procedure/procedure-step-component',
+        tags: '@procedure @regression @helpx',
+      },
+      {
+        tcid: '8',
+        name: '@procedureComponentBeforeAfter',
+        path: '/automation/blocks/procedure/procedure-step-component',
+        tags: '@procedure @regression @helpx',
+      },
+      {
+        tcid: '9',
+        name: '@procedureComponentCodeBlock',
+        path: '/automation/blocks/procedure/procedure-step-component',
+        tags: '@procedure @regression @helpx',
+      },
+      {
+        tcid: '10',
+        name: '@procedureComponentAllComponents',
+        path: '/automation/blocks/procedure/procedure-step-component',
+        tags: '@procedure @regression @helpx',
+      },
+      {
+        tcid: '11',
+        name: '@procedureComponentImageComponent',
+        path: '/automation/blocks/procedure/procedure-step-component',
+        tags: '@procedure @regression @helpx',
+      }
     ],
   };
   
