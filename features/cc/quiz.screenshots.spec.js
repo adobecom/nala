ab54b072da0b0d5367268f5b631f9df6fd8aa70f
Jacky Sun <67350368+JackySun9@users.noreply.github.com>
2024-01-09 14:23:44 -0800

    Add screenshots for UAR in CC (#238)
    * add test for quiz and result pages
    
    * add test for quiz and result pages
    
    * add test for quiz and result pages
    
    * add POC for analytics test
    
    * update analytics tests and envs
    
    * result envs
    
    * update analytics only on firefox
    
    * add dynamic tests
    
    * add dynamic test for quiz page
    
    * update quiz test
    
    * update test structure
    
    * add test for quiz and result pages
    
    * add POC for analytics test
    
    * update analytics tests and envs
    
    * result envs
    
    * update analytics only on firefox
    
    * add dynamic tests
    
    * add dynamic test for quiz page
    
    * update quiz test
    
    * update test structure
    
    * update uar tests to new format
    
    * update uar tests
    
    * move everything to uar folder
    
    * add dependency for js-yaml
    
    * update test according to latest comments
    
    * update test according to latest comments
    
    * update tests according to the latest comments
    
    * add uar config
    
    * add analytics for uar
    
    * update according to feedbacks
    
    * update tags
    
    * add @cc tag
    
    * update according to feedbacks
    
    * add dynamic tests for uar
    
    * update according to review comments
    
    * fix some typos
    
    * update according to review comments
    
    * merge console log info
    
    * add UI screenshots for UAR
    
    * move view point to test
    
    * update according to feedback
    
    * add analytics test for UAR
    
    * update validation and uar libs
    
    * update libs
    
    * update by feedback
    
    * update some config and tests
    
    * update some config and tests
    
    * update test content path to common path
    
    * update analytics tests
    
    * fix test failures caused by test code
    
    * fix test failures
    
    * fix test failures
    
    * add visual test for CAAS with two pages
    
    * update according to feedbacks
    
    * update report in config file
    
    * add screenshot for milo main live vs uar-integration live
    
    * add timestamp js for time stampe recording
    
    * add screenshot diff for uar stable and beta
    
    * move uar screenshots into visual compare folder
    
    * add UI screenshots for DX Quiz
    
    * build a function to get screenshots
    
    * update locator
    
    * udate uar to only run basic tests
    
    * update according to feedback
    
    * add screenshot for UAR in CC which will go live
    
    ---------
    
    Co-authored-by: xiasun <xiasun@xiasuns-MacBook-Pro-2.local>
    Co-authored-by: Aaron Mauchley <mauchley@adobe.com>
    Co-authored-by: xiasun <xiasun@xiasuns-mbp-2.corp.adobe.com>

---
+++ features/cc/quiz.screenshots.spec.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: 'Quiz Recommender Blocks',
+  features: [
+    {
+      tcid: '0',
+      name: '@quiz screenshots',
+      path: '/cc-shared/uar/plan-recommender/',
+      tags: '@cc @uar-quiz-screenshots @uar-quiz-static',
+      data: 'data/cc/quiz/quiz-basic.yml',
+    },
+  ],
+};
---
+++ tests/cc/quiz.screenshots.test.js
@@ -0,0 +1,65 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-loop-func */
+/* eslint-disable no-restricted-syntax */
+import { expect, test } from '@playwright/test';
+import Quiz from '../../selectors/uar/quiz.page.js';
+import QuizOldPage from '../../selectors/uar/quiz.old.page.js';
+
+const QuizSpec = require('../../features/cc/quiz.screenshots.spec.js');
+
+const { features } = QuizSpec;
+const { WebUtil } = require('../../libs/webutil.js');
+
+test.describe('Quiz flow test suite', () => {
+  // reset timeout because we use this to run all test data
+  test.setTimeout(10 * 60 * 1000);
+  for (const feature of features) {
+    test(
+      `${feature.name}, ${feature.tags}`,
+      async ({ page, baseURL }) => {
+        const quiz = new Quiz(page);
+        const quizOldPage = new QuizOldPage(page);
+        const url = `${baseURL}${feature.path}`;
+        console.info(url);
+
+        // load test data from static files
+        const testdata = await WebUtil.loadTestData(`${feature.data}`);
+
+        let keyNumber = 0;
+
+        for (let key of Object.keys(testdata)) {
+          console.log(key);
+          let oldProduct = '';
+          let newProduct = '';
+          keyNumber += 1;
+          await test.step(`Old: Select each answer on test page according to ${key}`, async () => {
+            await quizOldPage.clickEachAnswer('https://www.adobe.com/creativecloud/quiz-recommender.html', key, keyNumber, true);
+          });
+
+          await test.step('Old: Check results on test page', async () => {
+            oldProduct = await quizOldPage.checkResultPage(testdata[key], key, keyNumber, true);
+          });
+
+          if (key.includes('PDFs > Edit quickly')) {
+            // eslint-disable-next-line no-continue
+            continue;
+          }
+
+          if (key.includes('PDFs > Take the time to control')) {
+            key = key.replace('PDFs > Take the time to control every detail', 'PDFs');
+          }
+
+          await test.step(`New: Select each answer on test page according to ${key}`, async () => {
+            await quiz.clickEachAnswer(url, key, keyNumber, 'new', true);
+          });
+
+          await test.step('New: Check results on test page', async () => {
+            newProduct = await quiz.checkResultPage(testdata[key], key, keyNumber, 'new', true);
+          });
+
+          // expect.soft(newProduct).toContain(oldProduct);
+        }
+      },
+    );
+  }
+});
