0ae64ab5c3bf05f4388b5bf75936ccfb6c6b906f
Dan Chivescu <chadsunvice@gmail.com>
2024-01-11 20:09:18 +0200

    [MWPW-140397] Updated FEDS checks to work on GHA (FEDS-run) (#234)
    * [MWPW-140397] Updated FEDS checks to work on GHA (FEDS-run)
    
    * [MWPW-140397] Force Milo production to show Jarvis without VPN
    
    * [MWPW-140397] Skip UserProfile checks until IMS creddentials issue is fixed

--- tests/feds/jarvis.desktop.test.js
+++ tests/feds/jarvis.desktop.test.js
@@ -4,26 +4,17 @@
 import { expect, test } from '@playwright/test';
 import { features } from '../../features/feds/jarvis.spec.js';
 import FedsJarvis from '../../selectors/feds/feds.jarvis.page.js';
-import FedsConsent from '../../selectors/feds/feds.consent.page.js';
 
 test.describe('Jarvis Component test suite', () => {
   // FEDS Jarvis Default Checks:
   test(`${features[0].name}, ${features[0].tags}`, async ({ page, baseURL }) => {
     const Jarvis = new FedsJarvis(page);
-    const Consent = new FedsConsent(page);
     console.info(`[FEDSInfo] Checking page: ${baseURL}${features[0].path}`);
 
     await test.step('Navigate to FEDS Jarvis Default page', async () => {
       await page.goto(`${baseURL}${features[0].path}${features[0].browserParams}`);
       await page.waitForLoadState('domcontentloaded');
       await expect(page).toHaveURL(`${baseURL}${features[0].path}${features[0].browserParams}`);
-    });
-
-    await test.step('Accept OneTrust consent bar', async () => {
-      // Wait for the OneTrust consent bar to appear:
-      await Consent.oneTrustContainer.waitFor({ state: 'visible', timeout: 20000 });
-      // Accept the OneTrust consent banner:
-      await Consent.acceptOneTrustConsentBar();
     });
 
     await test.step('Check Jarvis component functionality', async () => {
@@ -43,20 +34,12 @@ test.describe('Jarvis Component test suite', () => {
   // FEDS Jarvis Desktop Checks:
   test(`${features[1].name}, ${features[1].tags}`, async ({ page, baseURL }) => {
     const Jarvis = new FedsJarvis(page);
-    const Consent = new FedsConsent(page);
     console.info(`[FEDSInfo] Checking page: ${baseURL}${features[1].path}`);
 
     await test.step('Navigate to FEDS Jarvis Desktop page', async () => {
       await page.goto(`${baseURL}${features[1].path}${features[1].browserParams}`);
       await page.waitForLoadState('domcontentloaded');
       await expect(page).toHaveURL(`${baseURL}${features[1].path}${features[1].browserParams}`);
-    });
-
-    await test.step('Accept OneTrust consent bar', async () => {
-      // Wait for the OneTrust consent bar to appear:
-      await Consent.oneTrustContainer.waitFor({ state: 'visible', timeout: 20000 });
-      // Accept the OneTrust consent banner:
-      await Consent.acceptOneTrustConsentBar();
     });
 
     await test.step('Check Jarvis component functionality', async () => {
@@ -76,22 +59,12 @@ test.describe('Jarvis Component test suite', () => {
   // FEDS Jarvis Disabled Checks:
   test(`${features[2].name}, ${features[2].tags}`, async ({ page, baseURL }) => {
     const Jarvis = new FedsJarvis(page);
-    const Consent = new FedsConsent(page);
     console.info(`[FEDSInfo] Checking page: ${baseURL}${features[2].path}`);
 
     await test.step('Navigate to FEDS Jarvis Disabled page', async () => {
       await page.goto(`${baseURL}${features[2].path}${features[2].browserParams}`);
       await page.waitForLoadState('domcontentloaded');
       await expect(page).toHaveURL(`${baseURL}${features[2].path}${features[2].browserParams}`);
-    });
-
-    await test.step('Accept OneTrust consent bar', async () => {
-      // Wait for the OneTrust consent bar to appear:
-      await Consent.oneTrustContainer.waitFor({ state: 'visible', timeout: 20000 });
-      // Accept the OneTrust consent banner:
-      await Consent.acceptOneTrustConsentBar();
-      // Check consent persistence:
-      await Consent.assertOneTrustAcceptState();
     });
 
     await test.step('Check Jarvis component is not initialized', async () => {
