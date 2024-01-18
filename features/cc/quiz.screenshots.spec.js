9c04e8688b920202852443e7e1e828b94a6f2250
xiasun <xiasun@xiasuns-MacBook-Pro-2.local>
2024-01-18 13:10:24 -0800

    update cc uar screenshots to hlx.live vs stage

--- features/cc/quiz.screenshots.spec.js
+++ features/cc/quiz.screenshots.spec.js
@@ -5,6 +5,8 @@ module.exports = {
       tcid: '0',
       name: '@quiz screenshots',
       path: '/creativecloud/plan-recommender/quiz',
+      stable: '@adobe_stage',
+      beta: '@cc_live',
       tags: '@cc @uar-quiz-screenshots @uar-quiz-static',
       data: 'data/cc/quiz/quiz-basic.yml',
     },
