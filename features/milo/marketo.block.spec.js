2974e8c7b079bf16c7a3f0e75620cfa96b88ed7d
Dennis Li <denli@adobe.com>
2024-01-22 12:16:04 -0800

    Adding Marketo form tests.

---
+++ features/milo/marketo.block.spec.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: 'Marketo Forms block',
+  features: [
+    {
+      name: '@marketo production form',
+      path: '/drafts/nala/blocks/marketo/marketo-mcz-production-2277',
+      tags: '@marketo @marketoProductionForm @milo @smoke @regression',
+    },
+    {
+      name: '@marketo short form',
+      path: '/drafts/nala/blocks/marketo/marketo-mcz-short-form-2259',
+      tags: '@marketo @marketoShortForm @milo @smoke @regression',
+    },
+  ],
+};
