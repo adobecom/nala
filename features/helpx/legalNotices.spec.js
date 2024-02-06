38ae9dd19e74e80f237c77b69c035a8842b882ac
kirupacommit <94790097+kirupacommit@users.noreply.github.com>
2024-02-06 22:16:09 +0530

    Helpx Note Text Component (#258)
    * Helpx_Before After Component Additional Test cases
    
    * Helpx_Code Block Component Test Cases
    
    * Helpx_Note Text Component testcases
    
    * Helpx_note component Changes
    
    * Helpx_Legal Notices Test cases and Xpaths fix based on Analytics Change
    
    * renamed legalNotices File
    
    * Helpx_Changed from networkidle to domcontentloaded
    
    ---------
    
    Co-authored-by: Kirupaagar R M <kirupaagarrm@adobe.com>

---
+++ features/helpx/legalNotices.spec.js
@@ -0,0 +1,41 @@
+module.exports = {
+  name: 'LegalNotices',
+  features: [
+    {
+      tcid: '0',
+      name: '@legalNotices',
+      path: '/automation/blocks/legal-and-privacy-notice.html',
+      tags: '@draft @regression @helpx',
+    },
+    {
+      tcid: '1',
+      name: '@legalNoticesintable',
+      path: '/automation/blocks/legal-and-privacy-notice.html',
+      tags: '@draft @regression @helpx',
+    },
+    {
+      tcid: '2',
+      name: '@legalNoticesincaptialletter',
+      path: '/automation/blocks/legal-and-privacy-notice.html',
+      tags: '@draft @regression @helpx',
+    },
+    {
+      tcid: '3',
+      name: '@legalnoticesincapitalletterintable',
+      path: '/automation/blocks/legal-and-privacy-notice.html',
+      tags: '@draft @regression @helpx',
+    },
+    {
+      tcid: '4',
+      name: '@legalnoticesoriginalformat',
+      path: '/automation/blocks/legal-and-privacy-notice.html',
+      tags: '@draft @regression @helpx',
+    },
+    {
+      tcid: '5',
+      name: '@legalnoticesAddingfulltext',
+      path: '/automation/blocks/legal-and-privacy-notice.html',
+      tags: '@draft @regression @helpx',
+    }
+  ],
+};
