dcbbcb359575eb8294d4567a09defa2cf0c3adb3
Dennis Li <denli@adobe.com>
2024-02-13 15:23:41 -0800

    Updated selectors

--- selectors/milo/marketo.block.page.js
+++ selectors/milo/marketo.block.page.js
@@ -4,19 +4,34 @@ export default class Marketo {
   constructor(page) {
     this.page = page;
     this.marketo = this.page.locator('.marketo');
-    this.salutation = this.marketo.locator('#Salutation');
-    this.firstName = this.marketo.locator('#FirstName');
-    this.lastName = this.marketo.locator('#LastName');
-    this.email = this.marketo.locator('#Email');
-    this.phone = this.marketo.locator('#Phone');
-    this.company = this.marketo.locator('#mktoFormsCompany_ignore');
-    this.functionalArea = this.marketo.locator('#mktoFormsFunctionalArea');
-    this.country = this.marketo.locator('#Country');
-    this.state = this.marketo.locator('#State');
-    this.postalCode = this.marketo.locator('#PostalCode');
-    this.jobTitle = this.marketo.locator('#mktoFormsJobTitle');
-    this.primaryProductInterest = this.marketo.locator('#mktoFormsPrimaryProductInterest');
+    this.firstName = this.marketo.locator('input[name="FirstName"]');
+    this.lastName = this.marketo.locator('input[name="LastName"]');
+    this.email = this.marketo.locator('input[name="Email"]');
+    this.phone = this.marketo.locator('input[name="Phone"]');
+    // this.company = this.marketo.locator('#mktoFormsCompany_ignore');
+    this.company = this.marketo.locator('input[name="mktoFormsCompany"]');
+    this.functionalArea = this.marketo.locator('select[name="mktoFormsFunctionalArea"]');
+    this.country = this.marketo.locator('select[name="Country"]');
+    this.state = this.marketo.locator('select[name="State"]');
+    this.postalCode = this.marketo.locator('input[name="PostalCode"]');
+    this.jobTitle = this.marketo.locator('select[name="mktoFormsJobTitle"]');
+    this.primaryProductInterest = this.marketo.locator('select[name="mktoFormsPrimaryProductInterest"]');
     this.submitButton = this.marketo.locator('#mktoButton_new');
+
+    // this.marketo = this.page.locator('.marketo');
+    // this.firstName = this.marketo.locator('#FirstName');
+    // this.lastName = this.marketo.locator('#LastName');
+    // this.email = this.marketo.locator('#Email');
+    // this.phone = this.marketo.locator('#Phone');
+    // // this.company = this.marketo.locator('#mktoFormsCompany_ignore');
+    // this.company = this.marketo.locator('input[name="mktoFormsCompany"]');
+    // this.functionalArea = this.marketo.locator('#mktoFormsFunctionalArea');
+    // this.country = this.marketo.locator('#Country');
+    // this.state = this.marketo.locator('#State');
+    // this.postalCode = this.marketo.locator('#PostalCode');
+    // this.jobTitle = this.marketo.locator('#mktoFormsJobTitle');
+    // this.primaryProductInterest = this.marketo.locator('#mktoFormsPrimaryProductInterest');
+    // this.submitButton = this.marketo.locator('#mktoButton_new');
   }
 
   /**
@@ -36,8 +51,8 @@ export default class Marketo {
    * @description Form ID: MCZ Production (2277)
    */
   async submitProductionForm() {
-    await this.functionalArea.selectOption('Other');
     await this.country.selectOption('United States');
+    await this.functionalArea.selectOption('Other');
     await this.jobTitle.selectOption('Other');
     await this.primaryProductInterest.selectOption('Digital marketing');
     await this.state.selectOption('California');
@@ -51,8 +66,8 @@ export default class Marketo {
   }
 
   async submitRFITemplateForm() {
-    await this.functionalArea.selectOption('Other', { timeout: 10000 });
     await this.country.selectOption('United States');
+    await this.functionalArea.selectOption('Other', { timeout: 10000 });
     await this.jobTitle.selectOption('Other');
     await this.primaryProductInterest.selectOption('Digital marketing');
     await this.state.selectOption('California');
@@ -66,47 +81,47 @@ export default class Marketo {
   }
 
   async submitDiscoverTemplateForm() {
-    await this.company.fill('Adobe');
     await this.country.selectOption('United States', { timeout: 10000 });
     await this.email.fill('test@adobe.com');
+    await this.company.fill('Adobe');
     await this.submitButton.click();
   }
 
   async submitExploreTemplateForm() {
-    await this.company.fill('Adobe');
-    await this.functionalArea.selectOption('Other');
     await this.country.selectOption('United States');
+    await this.functionalArea.selectOption('Other');
     await this.jobTitle.selectOption('Other');
     await this.firstName.fill('TestFirstName');
     await this.lastName.fill('TestLastName');
     await this.email.fill('test@adobe.com');
+    await this.company.fill('Adobe');
     await this.submitButton.click();
   }
 
   async submitEvaluateTemplateForm() {
-    await this.company.fill('Adobe');
-    await this.functionalArea.selectOption('Other', { timeout: 10000 });
     await this.country.selectOption('United States');
+    await this.functionalArea.selectOption('Other', { timeout: 10000 });
     await this.jobTitle.selectOption('Other');
     await this.state.selectOption('California');
     await this.firstName.fill('TestFirstName');
     await this.lastName.fill('TestLastName');
     await this.email.fill('test@adobe.com');
+    await this.company.fill('Adobe');
     await this.phone.fill('415-111-2222');
     await this.postalCode.fill('94111');
     await this.submitButton.click();
   }
 
   async submitWebinarTemplateForm() {
-    await this.company.fill('Adobe');
-    await this.functionalArea.selectOption('Other', { timeout: 10000 });
     await this.country.selectOption('United States');
+    await this.functionalArea.selectOption('Other', { timeout: 10000 });
     await this.jobTitle.selectOption('Other');
     await this.state.selectOption('California');
     await this.firstName.fill('TestFirstName');
     await this.lastName.fill('TestLastName');
     await this.email.fill('test@adobe.com');
     await this.phone.fill('415-111-2222');
+    await this.company.fill('Adobe');
     await this.postalCode.fill('94111');
     await this.submitButton.click();
   }
@@ -116,11 +131,11 @@ export default class Marketo {
     await this.country.selectOption('United States');
     await this.jobTitle.selectOption('Other');
     await this.state.selectOption('California');
-    await this.company.fill('Adobe');
     await this.firstName.fill('TestFirstName');
     await this.lastName.fill('TestLastName');
     await this.email.fill('test@adobe.com');
     await this.phone.fill('415-111-2222');
+    await this.company.fill('Adobe');
     await this.postalCode.fill('94111');
     await this.submitButton.click();
   }
