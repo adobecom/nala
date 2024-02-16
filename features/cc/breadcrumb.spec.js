5ed6739069961e8106bcc59a5967675d54dc42a1
sigadamramesh <130433161+sigadamvenkata@users.noreply.github.com>
2024-02-16 17:34:35 +0530

    CC regression automated tests for media rounded corners, breadcrumb features (#269)
    * CC Merchtable block automation
    
    CC Merchtable block automation (JIra: MWPW-140978)
    
    * CC sticky promo bar, promo with close action automated test cases
    
    CC sticky promo bar, promo with close action automated test cases
    Jira : MWPW-140982, MWPW-142252
    
    * CC Regression automated cases for media rounded corners, breadcrumb features
    
    CC Regression automated cases for media rounded corners, breadcrumb features (MWPW-140976, MWPW-142543)
    
    * Update roundedcorners-mediablock.test.js

---
+++ features/cc/breadcrumb.spec.js
@@ -0,0 +1,26 @@
+module.exports = {
+  name: 'breadcrumb',
+    features: [
+      {
+        tcid: '0',
+        name: '@breadcrumb-display',
+        path: '/drafts/Automation-PW/breadcrumb?georouting=off',
+        tags: '@cc @cc-breadcrumb @cc-breadcrumbdisplay',
+      },
+      {
+        tcid: '1',
+        name: '@breadcrumb-displayfirstlevelcheck',
+        path: '/drafts/Automation-PW/breadcrumb?georouting=off',
+        tags: '@cc @cc-breadcrumb @cc-breadcrumbfirstlevellinkcheck',
+        url : 'https://www.adobe.com/',
+      },
+      {
+        tcid: '2',
+        name: '@breadcrumb-displaypageparentlink',
+        path: '/drafts/Automation-PW/breadcrumb?georouting=off',
+        tags: '@cc @cc-breadcrumb @cc-breadcrumbparentlinkcheck',
+        url : 'https://www.adobe.com/products/photoshop.html',
+      },
+    ],
+  };
+  
