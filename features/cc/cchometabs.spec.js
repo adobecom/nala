330153fc667614a986dda176e326f053c4f5b020
sigadamramesh <130433161+sigadamvenkata@users.noreply.github.com>
2024-03-02 21:13:31 +0530

    CC Home page tabs, Fragment reference , March Pod test automation cases (#279)
    * CC Merchtable block automation
    
    CC Merchtable block automation (JIra: MWPW-140978)
    
    * CC sticky promo bar, promo with close action automated test cases
    
    CC sticky promo bar, promo with close action automated test cases
    Jira : MWPW-140982, MWPW-142252
    
    * CC Regression automated cases for media rounded corners, breadcrumb features
    
    CC Regression automated cases for media rounded corners, breadcrumb features (MWPW-140976, MWPW-142543)
    
    * Update roundedcorners-mediablock.test.js
    
    * CC home page tabs, fragment reference , merch pod AU tests
    
    CC home page tabs, fragment reference , merch pod AU tests
    Jiras:
    https://jira.corp.adobe.com/browse/MWPW-143230
    https://jira.corp.adobe.com/browse/MWPW-142599
    https://jira.corp.adobe.com/browse/MWPW-143231

---
+++ features/cc/cchometabs.spec.js
@@ -0,0 +1,30 @@
+module.exports = {
+  name: 'cchometabs',
+    features: [
+      {
+        tcid: '0',
+        name: '@cchometabs-display',
+        path: '/drafts/Automation-PW/cchometest?georouting=off',
+        tags: '@cc @cc-hometabs @cc-homepagetabsdisplay',
+      },
+      {
+        tcid: '1',
+        name: '@cchometabs-defaulttabselected',
+        path: '/drafts/Automation-PW/cchometest?georouting=off',
+        tags: '@cc @cc-hometabs @cc-homepagetabsdefaultselect',
+      },
+      {
+        tcid: '2',
+        name: '@cchometabs-tabswitchfeature',
+        path: '/drafts/Automation-PW/cchometest?georouting=off',
+        tags: '@cc @cc-hometabs @cc-homepagetabsswitch',
+      },
+      {
+        tcid: '3',
+        name: '@cchometabs-displaytabcontent',
+        path: '/drafts/Automation-PW/cchometest?georouting=off',
+        tags: '@cc @cc-hometabs @cc-homepagetabcontent',
+      },
+    ],
+  };
+  
---
+++ features/cc/fragmentreference.spec.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: 'fragmentreference',
+    features: [
+      {
+        tcid: '0',
+        name: '@fragmentreference-onpage',
+        path: '/drafts/Automation-PW/pricefragments?georouting=off',
+        tags: '@cc @cc-fragmentreference @cc-fragmentreference',
+      },
+    ],
+  };
+  
