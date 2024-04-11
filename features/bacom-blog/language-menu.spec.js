e945299ed23ab93cf88b12c64784c6d35d6c5b1e
Dennis Li <denli@adobe.com>
2024-04-11 15:13:33 -0700

    Adding region selector tests for bacom blog.

---
+++ features/bacom-blog/language-menu.spec.js
@@ -0,0 +1,13 @@
+module.exports = {
+  FeatureName: 'Bacom Blog Language Menu',
+  features: [
+    {
+      tcid: '0',
+      name: 'Bacom Blog Language Menu',
+      path: [
+        '/',
+      ],
+      tags: '@bacom-blog @smoke @regression',
+    },
+  ],
+};
