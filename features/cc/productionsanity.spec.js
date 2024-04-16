f52321b4eee731b2b725800e6e04986a25feba22
sigadam venkata ramesh <venkatas@adobe.com>
2024-04-16 22:16:13 +0530

    CC , CCT product pages sanity checks automation for EN
    Key CC , CCT product pages sanity checks automation for EN
    https://jira.corp.adobe.com/browse/MWPW-146289

---
+++ features/cc/productionsanity.spec.js
@@ -0,0 +1,45 @@
+module.exports = {
+  name: 'productionsanity',
+  features: [
+    {
+      tcid: '0',
+      name: '@creativecloudhomepage',
+      path: '/creativecloud.html?georouting=off',
+      tags: '@cc @cc-prodsanity @cc-homepagesanity',
+      commerceurl: 'https://commerce.adobe.com/store/commitment?items%5B0%5D%5Bid%5D=81DE7D1C91A201AB690BC4741B32F0C0&cli=adobe_com&ctx=fp&co=US&lang=en',
+    },
+    {
+      tcid: '1',
+      name: '@aftereffectsproductpage',
+      path: '/products/aftereffects.html?georouting=off',
+      tags: '@cc @cc-prodsanity @cc-aftereffectssanity',
+      url: 'https://www.adobe.com/creativecloud/plans.html?filter=video-audio&plan=individual',
+    },
+    {
+      tcid: '2',
+      name: '@ccpricingpage',
+      path: '/creativecloud/campaign/pricing.html?georouting=off',
+      tags: '@cc @cc-prodsanity @cc-pricingpagesanity',
+    },
+    {
+      tcid: '3',
+      name: '@illustratorproductpage',
+      path: '/products/illustrator.html?georouting=off',
+      tags: '@cc @cc-prodsanity @cc-illustratorsanity',
+      url: 'https://www.adobe.com/products/illustrator.html?georouting=off#marqueectatwp',
+    },
+    {
+      tcid: '4',
+      name: '@CCTproductpage',
+      path: '/creativecloud/business/teams/resources/how-to/5-diy-marketing-strategies.html?georouting=off',
+      tags: '@cc @cc-prodsanity @cc-CCTpagesanity',
+      url: 'https://www.adobe.com/creativecloud/business-plans.html?plan=team&step=2',
+    },
+    {
+      tcid: '5',
+      name: '@CCmodelprices',
+      path: '/products/premiere.html#marqueectatwp',
+      tags: '@cc @cc-prodsanity @cc-CCpricemodelsanity',
+    },
+  ],
+};
