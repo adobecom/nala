45ad1e861358dccc5d02194ab594985f2d6a9462
Dennis Li <34138453+Dli3@users.noreply.github.com>
2024-04-18 14:40:55 -0700

    Added tests for the new marketo forms templates (#329)
    Co-authored-by: Dennis Li <denli@adobe.com>

--- tests/milo/marketo.block.test.js
+++ tests/milo/marketo.block.test.js
@@ -10,11 +10,11 @@ test.describe('Marketo block test suite', () => {
     marketoBlock = new MarketoBlock(page);
   });
 
-  test(`@marketo production form, ${features[0].tags}`, async ({ page, baseURL }) => {
+  test(`0: @marketo production form, ${features[0].tags}`, async ({ page, baseURL }) => {
     const testPage = `${baseURL}${features[0].path}`;
     console.info(`[Test Page]: ${testPage}`);
 
-    await test.step('step-1: Go to Marketo Block test page', async () => {
+    await test.step('step-1: Go to the Marketo block test page', async () => {
       await page.goto(testPage);
       await page.waitForLoadState('domcontentloaded');
       await expect(page).toHaveURL(testPage);
@@ -34,17 +34,17 @@ test.describe('Marketo block test suite', () => {
     });
   });
 
-  test(`@marketo rfi template, ${features[1].tags}`, async ({ page, baseURL }) => {
+  test(`1: @marketo rfi template, ${features[1].tags}`, async ({ page, baseURL }) => {
     const testPage = `${baseURL}${features[1].path}`;
     console.info(`[Test Page]: ${testPage}`);
 
-    await test.step('step-1: Go to Marketo Block RFI Template test page', async () => {
+    await test.step('step-1: Go to the Marketo block RFI Template test page', async () => {
       await page.goto(testPage);
       await page.waitForLoadState('domcontentloaded');
       await expect(page).toHaveURL(testPage);
     });
 
-    await test.step('step-2: Verify that the expected fields display', async () => {
+    await test.step('step-2: Submit the form with valid inputs', async () => {
       await page.waitForTimeout(WAIT_TIME);
       await marketoBlock.submitRFITemplateForm();
     });
@@ -54,17 +54,17 @@ test.describe('Marketo block test suite', () => {
     });
   });
 
-  test(`@marketo discover template, ${features[2].tags}`, async ({ page, baseURL }) => {
+  test(`2: @marketo discover template, ${features[2].tags}`, async ({ page, baseURL }) => {
     const testPage = `${baseURL}${features[2].path}`;
     console.info(`[Test Page]: ${testPage}`);
 
-    await test.step('step-1: Go to Marketo Block discover template test page', async () => {
+    await test.step('step-1: Go to the Marketo block discover template test page', async () => {
       await page.goto(testPage);
       await page.waitForLoadState('domcontentloaded');
       await expect(page).toHaveURL(testPage);
     });
 
-    await test.step('step-2: Verify that the expected fields display', async () => {
+    await test.step('step-2: Submit the form with valid inputs', async () => {
       await page.waitForTimeout(WAIT_TIME);
       await marketoBlock.submitDiscoverTemplateForm();
     });
@@ -74,17 +74,17 @@ test.describe('Marketo block test suite', () => {
     });
   });
 
-  test(`@marketo explore template, ${features[3].tags}`, async ({ page, baseURL }) => {
+  test(`3: @marketo explore template, ${features[3].tags}`, async ({ page, baseURL }) => {
     const testPage = `${baseURL}${features[3].path}`;
     console.info(`[Test Page]: ${testPage}`);
 
-    await test.step('step-1: Go to Marketo Block explore template test page', async () => {
+    await test.step('step-1: Go to the Marketo block explore template test page', async () => {
       await page.goto(testPage);
       await page.waitForLoadState('domcontentloaded');
       await expect(page).toHaveURL(testPage);
     });
 
-    await test.step('step-2: Verify that the expected fields display', async () => {
+    await test.step('step-2: Submit the form with valid inputs', async () => {
       await page.waitForTimeout(WAIT_TIME);
       await marketoBlock.submitExploreTemplateForm();
     });
@@ -94,17 +94,17 @@ test.describe('Marketo block test suite', () => {
     });
   });
 
-  test(`@marketo evaluate template, ${features[4].tags}`, async ({ page, baseURL }) => {
+  test(`4: @marketo evaluate template, ${features[4].tags}`, async ({ page, baseURL }) => {
     const testPage = `${baseURL}${features[4].path}`;
     console.info(`[Test Page]: ${testPage}`);
 
-    await test.step('step-1: Go to Marketo Block evaluate template test page', async () => {
+    await test.step('step-1: Go to the Marketo block evaluate template test page', async () => {
       await page.goto(testPage);
       await page.waitForLoadState('domcontentloaded');
       await expect(page).toHaveURL(testPage);
     });
 
-    await test.step('step-2: Verify that the expected fields display', async () => {
+    await test.step('step-2: Submit the form with valid inputs', async () => {
       await page.waitForTimeout(WAIT_TIME);
       await marketoBlock.submitEvaluateTemplateForm();
     });
@@ -114,17 +114,17 @@ test.describe('Marketo block test suite', () => {
     });
   });
 
-  test(`@marketo webinar template, ${features[5].tags}`, async ({ page, baseURL }) => {
+  test(`5: @marketo webinar template, ${features[5].tags}`, async ({ page, baseURL }) => {
     const testPage = `${baseURL}${features[5].path}`;
     console.info(`[Test Page]: ${testPage}`);
 
-    await test.step('step-1: Go to Marketo Block webinar template test page', async () => {
+    await test.step('step-1: Go to the Marketo block webinar template test page', async () => {
       await page.goto(testPage);
       await page.waitForLoadState('domcontentloaded');
       await expect(page).toHaveURL(testPage);
     });
 
-    await test.step('step-2: Verify that the expected fields display', async () => {
+    await test.step('step-2: Submit the form with valid inputs', async () => {
       await page.waitForTimeout(WAIT_TIME);
       await marketoBlock.submitWebinarTemplateForm();
     });
@@ -134,19 +134,79 @@ test.describe('Marketo block test suite', () => {
     });
   });
 
-  test(`@marketo trial template, ${features[6].tags}`, async ({ page, baseURL }) => {
+  test(`6: @marketo trial template, ${features[6].tags}`, async ({ page, baseURL }) => {
     const testPage = `${baseURL}${features[6].path}`;
     console.info(`[Test Page]: ${testPage}`);
 
-    await test.step('step-1: Go to Marketo Block content discover template test page', async () => {
+    await test.step('step-1: Go to the Marketo block content discover template test page', async () => {
       await page.goto(testPage);
       await page.waitForLoadState('domcontentloaded');
       await expect(page).toHaveURL(testPage);
     });
 
-    await test.step('step-2: Verify that the expected fields display', async () => {
+    await test.step('step-2: Submit the form with valid inputs', async () => {
       await page.waitForTimeout(WAIT_TIME);
       await marketoBlock.submitTrialTemplateForm();
+    });
+
+    await test.step('step-3: Verify the form submission redirect', async () => {
+      await expect(page).not.toHaveURL(testPage, { timeout: 15000 });
+    });
+  });
+
+  test(`7: @marketo full template, ${features[7].tags}`, async ({ page, baseURL }) => {
+    const testPage = `${baseURL}${features[7].path}`;
+    console.info(`[Test Page]: ${testPage}`);
+
+    await test.step('step-1: Go to the Marketo block full template test page', async () => {
+      await page.goto(testPage);
+      await page.waitForLoadState('domcontentloaded');
+      await expect(page).toHaveURL(testPage);
+    });
+
+    await test.step('step-2: Submit the form with valid inputs', async () => {
+      await page.waitForTimeout(WAIT_TIME);
+      await marketoBlock.submitFullTemplateForm();
+    });
+
+    await test.step('step-3: Verify the form submission redirect', async () => {
+      await expect(page).not.toHaveURL(testPage, { timeout: 15000 });
+    });
+  });
+
+  test(`8: @marketo expanded template, ${features[8].tags}`, async ({ page, baseURL }) => {
+    const testPage = `${baseURL}${features[8].path}`;
+    console.info(`[Test Page]: ${testPage}`);
+
+    await test.step('step-1: Go to the Marketo block expanded template test page', async () => {
+      await page.goto(testPage);
+      await page.waitForLoadState('domcontentloaded');
+      await expect(page).toHaveURL(testPage);
+    });
+
+    await test.step('step-2: Submit the form with valid inputs', async () => {
+      await page.waitForTimeout(WAIT_TIME);
+      await marketoBlock.submitExpandedTemplateForm();
+    });
+
+    await test.step('step-3: Verify the form submission redirect', async () => {
+      await expect(page).not.toHaveURL(testPage, { timeout: 15000 });
+    });
+  });
+
+  test(`9: @marketo essential template, ${features[9].tags}`, async ({ page, baseURL }) => {
+    const testPage = `${baseURL}${features[9].path}`;
+    console.info(`[Test Page]: ${testPage}`);
+
+    await test.step('step-1: Go to the Marketo block essential template test page', async () => {
+      await page.goto(testPage);
+      await page.waitForLoadState('domcontentloaded');
+      await expect(page).toHaveURL(testPage);
+    });
+
+    await test.step('step-2: Submit the form with valid inputs', async () => {
+      await page.waitForTimeout(WAIT_TIME);
+      await marketoBlock.submitEssentialTemplateForm();
     });
 
     await test.step('step-3: Verify the form submission redirect', async () => {
