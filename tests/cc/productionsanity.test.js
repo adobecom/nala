eccf70521ded4cd5d6e155a9628459523171e86d
sigadamramesh <130433161+sigadamvenkata@users.noreply.github.com>
2024-04-22 13:37:21 +0530

    Update productionsanity.test.js
    removed tailing spaces from given lines

--- tests/cc/productionsanity.test.js
+++ tests/cc/productionsanity.test.js
@@ -16,7 +16,7 @@ test.describe('verify Key product pages and features on Production pages', () =>
       await expect(page).toHaveURL(`${baseURL}${features[0].path}`);
     });
     await test.step('creativecloud page gnav, marquee cta, price pod checks', async () => {
-      await page.waitForLoadState();  
+      await page.waitForLoadState();
       expect(await prodsanity.Gnav).toBeTruthy();
       expect(await prodsanity.fedsNav).toBeTruthy();
       expect(await prodsanity.signIn).toBeTruthy();
@@ -42,7 +42,7 @@ test.describe('verify Key product pages and features on Production pages', () =>
       await expect(page).toHaveURL(`${baseURL}${features[1].path}`);
     });
     await test.step('After Effects product page sanity', async () => {
-      await page.waitForLoadState();  
+      await page.waitForLoadState();
       expect(await prodsanity.Gnav).toBeTruthy();
       expect(await prodsanity.localNav).toBeTruthy();
       expect(await prodsanity.magaMenuItems).toBeTruthy();
@@ -63,7 +63,7 @@ test.describe('verify Key product pages and features on Production pages', () =>
       await expect(page).toHaveURL(`${baseURL}${features[2].path}`);
     });
     await test.step('creative cloud pricing page elements checks sanity', async () => {
-      await page.waitForLoadState();  
+      await page.waitForLoadState();
       expect(await prodsanity.Gnav).toBeTruthy();
       expect(await prodsanity.universalNav).toBeTruthy();
       expect(await prodsanity.startFreeTrialCTA).toBeTruthy();
@@ -80,7 +80,7 @@ test.describe('verify Key product pages and features on Production pages', () =>
       await expect(page).toHaveURL(`${baseURL}${features[3].path}`);
     });
     await test.step('Illustrator page sanity checks', async () => {
-      await page.waitForLoadState();  
+      await page.waitForLoadState();
       expect(await prodsanity.Gnav).toBeTruthy();
       expect(await prodsanity.appSwitcher).toBeTruthy();
       expect(await prodsanity.breadCrumb).toBeTruthy();
@@ -99,7 +99,7 @@ test.describe('verify Key product pages and features on Production pages', () =>
       await expect(page).toHaveURL(`${baseURL}${features[4].path}`);
     });
     await test.step('CCT milo pages sanity checks', async () => {
-      await page.waitForLoadState();  
+      await page.waitForLoadState();
       expect(await prodsanity.Gnav).toBeTruthy();
       expect(await prodsanity.CCBusinessGnavLink).toBeTruthy();
       expect(await prodsanity.CCTBuynowCTA).toBeTruthy();
