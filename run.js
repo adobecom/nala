0e3cdf388963e47d06e0caa0028ba0108534fcf4
Jacky Sun <67350368+JackySun9@users.noreply.github.com>
2024-04-23 15:50:24 -0700

    MWPW-146964: [Nala] unified test run commands  (#335)
    * add test for quiz and result pages
    
    * add test for quiz and result pages
    
    * add test for quiz and result pages
    
    * add POC for analytics test
    
    * update analytics tests and envs
    
    * result envs
    
    * update analytics only on firefox
    
    * add dynamic tests
    
    * add dynamic test for quiz page
    
    * update quiz test
    
    * update test structure
    
    * add test for quiz and result pages
    
    * add POC for analytics test
    
    * update analytics tests and envs
    
    * result envs
    
    * update analytics only on firefox
    
    * add dynamic tests
    
    * add dynamic test for quiz page
    
    * update quiz test
    
    * update test structure
    
    * update uar tests to new format
    
    * update uar tests
    
    * move everything to uar folder
    
    * add dependency for js-yaml
    
    * update test according to latest comments
    
    * update test according to latest comments
    
    * update tests according to the latest comments
    
    * add uar config
    
    * add analytics for uar
    
    * update according to feedbacks
    
    * update tags
    
    * add @cc tag
    
    * update according to feedbacks
    
    * add dynamic tests for uar
    
    * update according to review comments
    
    * fix some typos
    
    * update according to review comments
    
    * merge console log info
    
    * add UI screenshots for UAR
    
    * move view point to test
    
    * update according to feedback
    
    * add analytics test for UAR
    
    * update validation and uar libs
    
    * update libs
    
    * update by feedback
    
    * update some config and tests
    
    * update some config and tests
    
    * update test content path to common path
    
    * update analytics tests
    
    * fix test failures caused by test code
    
    * fix test failures
    
    * fix test failures
    
    * add visual test for CAAS with two pages
    
    * update according to feedbacks
    
    * update report in config file
    
    * add screenshot for milo main live vs uar-integration live
    
    * add timestamp js for time stampe recording
    
    * add screenshot diff for uar stable and beta
    
    * move uar screenshots into visual compare folder
    
    * add UI screenshots for DX Quiz
    
    * build a function to get screenshots
    
    * update locator
    
    * udate uar to only run basic tests
    
    * update according to feedback
    
    * add screenshot for UAR in CC which will go live
    
    * update final path for UAR in CC
    
    * update the final CC path
    
    * update cc uar screenshots to hlx.live vs stage
    
    * update test
    
    * update screenshot test
    
    * update uar analytics to run on STAGE
    
    * update tests to run on different envs after go live
    
    * Fix test failures for MWPW-143444
    
    * update urls
    
    * MWPW-143444: setup automation for cc uar quiz
    
    * update for feedbacks
    
    * update for feedbacks
    
    * fix some validations
    
    * update uar automation for Stage testing
    
    * fix lint error
    
    * fix tests failures caused by timeout
    
    * make uar able to run on milo
    
    * limited dynamic test to 20 random
    
    * delete unuse tests to avoid noises
    
    * comment out soft compare to avoid failures
    
    * comment out soft compare to avoid failures
    
    * remove uar tests from cc folder
    
    * remove stage stuff
    
    * fix lint and test failures
    
    * remove unuse ulrs
    
    * get milo stage back for analytics
    
    * update id to be unique
    
    * update package.json
    
    * remove main cc hlx live links
    
    * remove cc tags for uar tests
    
    * MWPW-146964: unified test run commands
    
    * support '-' and '--'
    
    * add required for -c and -p
    
    * show help if no option provided
    
    * MWPW-146964: [Nala] unified test run commands, support run on all projects or some projects
    
    * MWPW-146964: [Nala] unified test run commands, support run on all projects or some projects
    
    ---------
    
    Co-authored-by: xiasun <xiasun@xiasuns-MacBook-Pro-2.local>
    Co-authored-by: Aaron Mauchley <mauchley@adobe.com>
    Co-authored-by: xiasun <xiasun@xiasuns-mbp-2.corp.adobe.com>

--- run.js
+++ run.js
@@ -6,6 +6,7 @@ Usage: node run.js [options]
 
 Options:
 -p, --project       Specify the project to run tests on, e.g., milo-live-chrome (required).
+                    Use 'all' for all projects, or comma-separated list of projects.
 -c, --config        Specify name of configuration file, e.g., milo (required).
 -g, --grep          Filter tests by grep pattern, e.g., '@milo'.
 -r, --reporter      Specify the reporter to use, e.g., 'html'.
@@ -96,7 +97,14 @@ function runPlaywrightTests() {
   }
 
   if (argv.project) {
-    options.push(`--project=${argv.project}`);
+    if (argv.project === 'all') {
+      console.log('Running tests for all projects');
+    } else {
+      const projectArray = argv.project.split(',');
+      projectArray.forEach((project) => {
+        options.push(`--project=${project}`);
+      });
+    }
   } else {
     console.error('Error: -p (project) is required');
     process.exit(1);
