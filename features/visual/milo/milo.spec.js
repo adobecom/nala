11cd06395b3ac1f8aa2a250553b34e36e6ac3a50
Jacky Sun <XIASUN@ADOBE.COM>
2024-04-30 10:08:13 -0700

    MWPW-147289: [Nala] Refine screenshot diff tool, first step, cleanup and build new APIs

---
+++ features/visual/milo/milo.spec.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name: 'Milo Visual Comparison',
+  features: [
+    {
+      tcid: '0',
+      name: '@columns-visual',
+      path: '/test/features/blocks/columns',
+      tags: '@columns-visual @visual @milo-screenshots',
+    },
+    {
+      tcid: '1',
+      name: '@marquee-visual',
+      path: '/test/features/blocks/marquee',
+      tags: '@marquee-visua  @visual @milo-screenshots',
+    },
+  ],
+};
