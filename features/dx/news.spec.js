21eaeabc1a44648c35739d48a4b72b8372ab8f2e
cod17828 <cod17828@adobe.com>
2024-05-23 14:47:22 +0200

    news page test

---
+++ features/dx/news.spec.js
@@ -0,0 +1,12 @@
+/* eslint-disable max-len */
+module.exports = {
+  FeatureName: 'DX News page',
+  features: [
+    {
+      tcid: 'MWPW-137796',
+      name: '@desc-public-app-gravity-card-collection',
+      path: '/solutionpartners/drafts/automation/regression/partner-news',
+      tags: '@dx-news @regression',
+    },
+  ],
+};
---
+++ tests/dx/news.test.js
@@ -0,0 +1,20 @@
+/* eslint-disable prefer-destructuring */
+import { test, expect } from '@playwright/test';
+
+const News = require('../../features/dx/news.spec.js');
+
+const { features } = News;
+
+test.describe('Validate news page for public users', () => {
+  // Test - MWPW-137796
+  test(`${features[0].name},${features[0].tags}`, async ({ page, baseURL }) => {
+    console.info(`${baseURL}${features[0].path}`);
+
+    // test step-1
+    await test.step('Go to News page', async () => {
+      await page.goto(`${baseURL}${features[0].path}`);
+      await expect(page).toHaveURL(`${baseURL}${features[0].path}`);
+      await page.waitForTimeout(2000);
+    });
+  });
+});
