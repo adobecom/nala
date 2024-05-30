95bf24dcd5e0cb2b4d4d29f462ef3e6cd828c5c9
cod17828 <124770688+cod17828@users.noreply.github.com>
2024-05-30 18:42:53 +0200

    Dx partners/news page (#351)
    * news page test
    
    * news page test url
    
    * update after comments
    
    * update after comments
    
    * update after comments
    
    * update after comments

---
+++ features/dx/news.spec.js
@@ -0,0 +1,11 @@
+module.exports = {
+  FeatureName: 'DX News page',
+  features: [
+    {
+      tcid: 'MWPW-137796',
+      name: '@desc-public-app-gravity-card-collection',
+      path: '/solutionpartners/drafts/automation/regression/partner-news',
+      tags: '@dx-news @regression',
+    },
+  ],
+};
