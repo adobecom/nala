97be71068c1d1167151af2b98c5e793b3cefc66e
nateekar <nateekar@adobe.com>
2024-05-31 12:29:08 -0700

    updating the workers

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,7 @@ const config = {
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 3 : 3,
+  workers: process.env.CI ? 4 : 3,
   /* Reporter to use.*/
   reporter: process.env.CI
     ? [['github'], ['list'], ['./utils/reporters/base-reporter.js']]
