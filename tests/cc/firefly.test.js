267bcaf74ea41f8a07c87d7aaee9968c7584583d
sigadam venkata ramesh <venkatas@adobe.com>
2024-06-03 23:22:29 +0530

    CC Firefly integration tests
    CC firefly product page UI & integration with FF web

---
+++ tests/cc/firefly.test.js
@@ -0,0 +1,62 @@
+import { expect, test } from '@playwright/test';
+import { features } from '../../features/cc/firefly.spec.js';
+import FireFly from '../../selectors/cc/firefly.page.js';
+
+let firefly;
+test.describe('firefly integration', () => {
+  test.beforeEach(async ({ page }) => {
+    firefly = new FireFly(page);
+  });
+  test(`${features[0].name},${features[0].tags}`, async ({ page, baseURL }) => {
+    console.info(`[Test Page]: ${baseURL}${features[0].path}`);
+    await test.step('Firefly interactive marquee search via IMS login', async () => {
+      await page.goto(`${baseURL}${features[0].path}`);
+      await page.waitForLoadState('domcontentloaded');
+      await expect(page).toHaveURL(`${baseURL}${features[0].path}`);
+    });
+    await test.step('search term take use to IMS and post login to FF product page', async () => {
+      await page.waitForLoadState();
+      const searchtext = features[0].term;
+      expect(await firefly.searchPromptBox).toBeTruthy();
+      expect(await firefly.generateCTA).toBeTruthy();
+      await firefly.searchPromptBox.fill(features[0].term);
+      // await page.waitForTimeout(2000);
+      await firefly.generateCTA.click();
+      await firefly.stage_login(process.env.IMS_EMAIL, process.env.IMS_PASS);
+      await expect(page).toHaveURL(/.*firefly/);
+      await firefly.promtbar_input(searchtext);
+    });
+  });
+
+  test(`${features[1].name},${features[1].tags}`, async ({ page, baseURL }) => {
+    console.info(`[Test Page]: ${baseURL}${features[1].path}`);
+    await test.step('click generate without search prompt', async () => {
+      await page.goto(`${baseURL}${features[1].path}`);
+      await page.waitForLoadState('domcontentloaded');
+      await expect(page).toHaveURL(`${baseURL}${features[1].path}`);
+    });
+    await test.step('user hits serach CTA with out search prompt', async () => {
+      await page.waitForLoadState();
+      expect(await firefly.searchPromptBox).toBeTruthy();
+      expect(await firefly.generateCTA).toBeTruthy();
+      await firefly.generateCTA.click();
+      await expect(page).toHaveURL(/.*firefly/);
+    });
+  });
+
+  test(`${features[2].name},${features[2].tags}`, async ({ page, baseURL }) => {
+    console.info(`[Test Page]: ${baseURL}${features[2].path}`);
+    await test.step('click generate without search prompt', async () => {
+      await page.goto(`${baseURL}${features[2].path}`);
+      await page.waitForLoadState('domcontentloaded');
+      await expect(page).toHaveURL(`${baseURL}${features[2].path}`);
+    });
+    await test.step('user hits serach CTA with out search prompt', async () => {
+      await page.waitForLoadState();
+      expect(await firefly.FFInteractiveCards).toBeTruthy();
+      expect(await firefly.FFEntisement).toBeTruthy();
+      expect(await firefly.searchPromptBox).toBeTruthy();
+      expect(await firefly.promptDefaultText).toBeTruthy();
+    });
+  });
+});
