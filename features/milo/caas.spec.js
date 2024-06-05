241c1b61447d4042e7790c2aa8995e8085cd836a
Gayane Mirijanyan <gmirijan@Gayanes-MBP.attlocal.net>
2024-06-05 01:07:01 -0700

    NALA tests for CAAS

---
+++ features/milo/caas.spec.js
@@ -0,0 +1,16 @@
+module.exports = {
+    FeatureName: 'CAAS Feature',
+    features: [
+        {
+        tcid: '0',
+        name: '@CAAS collection',
+        path: '/drafts/nala/blocks/caas/caascollection',
+        data: {
+            cardsPerPage: 4,
+            caasTitle: 'Consonant Card Collection Title',
+            paginator: '1 - 4 of 8 results',
+        },
+        tags: '@caas @smoke @regression @milo',
+        },
+    ],
+}
---
+++ tests/milo/caas.feature.test.js
@@ -0,0 +1,48 @@
+import { expect, test } from "@playwright/test";
+import { WebUtil } from "../../libs/webutil.js";
+import { features } from "../../features/milo/caas.spec.js";
+import Caas from "../../selectors/milo/caas.feature.page.js";
+
+let webUtil;
+let caas;
+let consoleErrors = [];
+
+const miloLibs = process.env.MILO_LIBS || '';
+
+test.describe("Milo CAAS Feature test suite", () => {
+  test.beforeEach(async ({ page }) => {
+    webUtil = new WebUtil(page);
+    caas = new Caas(page);
+
+    page.on("console", (exception) => {
+      if (exception.type() === "error") {
+        consoleErrors.push(exception.text());
+      }
+    });
+  });
+
+  test.afterEach(async () => {
+    consoleErrors = [];
+  });
+    
+      // Test 0 : CAAS collection
+      test(`${features[0].name},${features[0].tags}`, async ({ page, baseURL }) => {
+        console.info(`[Test Page]: ${baseURL}${features[0].path}${miloLibs}`);
+        const {data} = features[0];
+
+        await test.step('step-1: Go to CAAS collection test page', async () => {
+            await page.goto(`${baseURL}${features[0].path}${miloLibs}`);
+            await page.waitForLoadState('domcontentloaded');
+            await expect(page).toHaveURL('${baseURL}${features[0].path}');
+        });
+
+        await test.step('step-2: Verify CAAS collection content/specs', async () => {
+            await expect(await caas.caasCards).toBeVisible();
+            await expect(await caas.caasCards).toHaveCount(data.cardsPerPage);
+
+            // verify caas title and paginator
+            await expect(await caas.caasTitle).toContainText(data.caasTitle);
+            await expect(await caas.caasPaginator).toContainText(data.paginator);
+        });
+    });
+});
