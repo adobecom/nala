d32395d461873e0738c9b8bd11dd2c6169f3c951
Dennis Li <34138453+Dli3@users.noreply.github.com>
2024-06-26 09:06:06 -0700

    Removed old form template test. (#384)
    Co-authored-by: Dennis Li <denli@adobe.com>

--- features/milo/marketo.block.spec.js
+++ features/milo/marketo.block.spec.js
@@ -3,60 +3,22 @@ module.exports = {
   features: [
     {
       tcid: '0',
-      name: '@marketo production form',
-      path: '/drafts/nala/blocks/marketo/marketo-mcz-production-2277',
-      tags: '@marketo @marketoProductionForm @milo @smoke @regression',
+      name: '@marketo full template',
+      path: [
+        '/drafts/nala/blocks/marketo/full',
+      ],
+      tags: '@marketo @marketoFullTemplate @milo @smoke @regression',
     },
     {
       tcid: '1',
-      name: '@marketo rfi template',
-      path: '/drafts/nala/blocks/marketo/content-rfi-template',
-      tags: '@marketo @marketoRFITemplate @milo @smoke @regression',
+      name: '@marketo full template with company type',
+      path: [
+        '/drafts/nala/blocks/marketo/full-with-company-type',
+      ],
+      tags: '@marketo @marketoFullTemplate @milo @smoke @regression',
     },
     {
       tcid: '2',
-      name: '@marketo discover template',
-      path: '/drafts/nala/blocks/marketo/content-discover-template',
-      tags: '@marketo @marketoDiscoverTemplate @milo @smoke @regression',
-    },
-    {
-      tcid: '3',
-      name: '@marketo explore template',
-      path: '/drafts/nala/blocks/marketo/content-explore-template',
-      tags: '@marketo @marketoExploreTemplate @milo @smoke @regression',
-    },
-    {
-      tcid: '4',
-      name: '@marketo evaluate template',
-      path: '/drafts/nala/blocks/marketo/content-evaluate-template',
-      tags: '@marketo @marketoEvaluateTemplate @milo @smoke @regression',
-    },
-    {
-      tcid: '5',
-      name: '@marketo webinar template',
-      path: '/drafts/nala/blocks/marketo/content-webinar-template',
-      tags: '@marketo @marketoWebinarTemplate @milo @smoke @regression',
-    },
-    {
-      tcid: '6',
-      name: '@marketo trial template',
-      path: '/drafts/nala/blocks/marketo/content-trial-template',
-      tags: '@marketo @marketoTrialTemplate @milo @smoke @regression',
-    },
-    {
-      tcid: '7',
-      name: '@marketo full template',
-      path: '/drafts/nala/blocks/marketo/full',
-      tags: '@marketo @marketoFullTemplate @milo @smoke @regression',
-    },
-    {
-      tcid: '8',
-      name: '@marketo full template',
-      path: '/drafts/nala/blocks/marketo/full-with-company-type',
-      tags: '@marketo @marketoFullTemplate @milo @smoke @regression',
-    },
-    {
-      tcid: '9',
       name: '@marketo expanded template',
       path: [
         '/drafts/nala/blocks/marketo/expanded',
@@ -65,7 +27,7 @@ module.exports = {
       tags: '@marketo @marketoExpandedTemplate @milo @smoke @regression',
     },
     {
-      tcid: '10',
+      tcid: '3',
       name: '@marketo essential template',
       path: [
         '/drafts/nala/blocks/marketo/essential',
