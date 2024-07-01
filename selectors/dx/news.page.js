2be42a48b92d97a859d22db9f79e75aae3c03795
Kristijan Vizi <kristijanvizi@Kristijans-MacBook-Pro-2.local>
2024-07-01 11:34:11 +0200

    New: added login flow test cases

--- selectors/dx/news.page.js
+++ selectors/dx/news.page.js
@@ -2,12 +2,14 @@ export default class NewsPage {
   constructor(page) {
     this.page = page;
     this.resultNumber = page.locator('.partner-cards-cards-results').nth(0);
+    this.firstCardDate = page.locator('.card-footer .card-date').nth(0);
     this.firstCardTitle = page.locator('.card-title').nth(0);
+    this.lastCardTitle = page.locator('.card-title').last();
     this.searchField = page.locator('.input');
     this.clearSearchSelector = page.locator('[aria-label="Reset"]');
     this.clearAllSelector = page.locator('[aria-label="Clear all"]');
     this.sortBtn = page.locator('.sort-btn');
-    this.oldestOption = page.getByRole('button', { name: 'Date: oldest' });
+    this.oldestOption = page.getByRole('button', { name: 'oldest' });
     this.paginationText = page.locator('.pagination-total-results');
     this.loadMore = page.locator('[aria-label="Load more"]');
     this.firstCardDate = page.locator('.card-date').nth(0);
@@ -25,6 +27,8 @@ export default class NewsPage {
     this.logoutButton = page.locator('[daa-ll="Sign Out"]');
     this.cardCount = page.locator('.card-wrapper');
     this.readCard = page.locator('.card-btn');
+    this.dialogCloseButton = page.locator('.dialog-close');
+    this.signInButtonStageAdobe = page.locator('.profile-comp.secondary-button');
   }
 
   async expandFilterOptions(filterSection) {
