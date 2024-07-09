ca2f7a107cc9efb3c470cf74ef1d0ec550620d7d
Pavan Kumar <pavankumar@adobe.com>
2024-07-09 10:11:24 +0530

    Added iOS, Android, iPad methods and Test file

---
+++ tests/feds/mobileHomePageSanity.test.js
@@ -0,0 +1,35 @@
+import { test, beforeEach, afterEach, expect } from '@playwright/test';
+import { features } from '../../features/feds/prodSanity/mobileTesting/mobilehomePageSanity.spec.js';
+import HomePageSanity from '../../selectors/feds/feds.homepagesanity.page.js';
+
+test.describe('Test Suite for Home Page on Android & iOS & iPad Devices', () => {
+  let home;
+
+  beforeEach(async ({ page }) => {
+    home = new HomePageSanity(page);
+  });
+
+  afterEach(async ({ page }) => {
+    await page.close();
+  });
+  features.forEach((props) => {
+    test(`${props.name}, ${props.tags}, ${props.country}`, async ({ page, baseURL }) => {
+      console.info(`[FEDSInfo] Checking Page: ${props.country} = ${baseURL}${features[props.tcid].path}`);
+
+      const pageURL = `${baseURL}${features[props.tcid].path}`;
+      await page.goto(pageURL, { waitUntil: 'networkidle' });
+      await expect(page).toHaveURL(pageURL);
+
+      // Verifying the visibility of G-NAV Elements
+      await home.validatingUnav();
+      // Verifying the visibility of Hamburger Menu Elements
+      await home.validatingHamburgerMenu();
+      // Verifying the visibility of Creative Cloud Elements
+      await home.validatingCreativityAndDesign(props.country);
+      // Verifying the visibility of Experience Cloud Elements
+      await home.validatingMarketingAndCommerce();
+      // Verifying the visibility of Footer Elements
+      await home.validatingFooter(props.country);
+    });
+  });
+});
