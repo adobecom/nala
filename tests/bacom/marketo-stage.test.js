5e32a4b9b23f703e8510f280bf83daa1c851da73
Dennis Li <denli@adobe.com>
2024-07-18 15:55:39 -0700

    Seeing if this tagging method gets picked up.

--- tests/bacom/marketo-stage.test.js
+++ tests/bacom/marketo-stage.test.js
@@ -6,7 +6,7 @@ let marketoBlock;
 const WAIT_TIME = 10000;
 const miloLibs = process.env.MILO_LIBS || '';
 
-test.describe('Marketo block test suite for stage form', () => {
+test.describe('Marketo block test suite for stage form', { tag: '@stageForm' }, () => {
   // test.describe('Marketo block test suite for stage form', { tag: '@stageForm' }, () => {
   test.beforeEach(async ({ page }) => {
     marketoBlock = new MarketoBlock(page);
