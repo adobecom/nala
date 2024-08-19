da9ec4e5f40b5562618f01052fc39a630a5fd3e4
Dennis Li <34138453+Dli3@users.noreply.github.com>
2024-08-19 13:15:06 -0700

    Adding marketo messages tests (#420)
    Co-authored-by: Dennis Li <denli@adobe.com>

--- tests/milo/marketo.block.test.js
+++ tests/milo/marketo.block.test.js
@@ -6,22 +6,25 @@ let marketoBlock;
 const miloLibs = process.env.MILO_LIBS || '';
 
 test.describe('Marketo block test suite', () => {
-  test.beforeEach(async ({ page, browserName }) => {
-    if (browserName === 'chromium') test.skip('TODO: debug why this is failing on github actions');
+  test.beforeAll(async ({ browserName }) => {
+    if (browserName === 'chromium' && process.env.CI) test.skip('TODO: debug why this is failing on github actions');
 
+    if (process.env.CI) test.setTimeout(1000 * 60 * 3); // 3 minutes
+  });
+
+  test.beforeEach(async ({ page }) => {
     marketoBlock = new MarketoBlock(page);
-    await test.setTimeout(1000 * 60 * 3);
   });
 
   features[0].path.forEach((path) => {
-    test(`0: @marketo full template, ${features[0].tags}, path: ${path}`, async ({ page, baseURL }) => {
+    test(`0: @marketo full template (redirect), ${features[0].tags}, path: ${path}`, async ({ page, baseURL }) => {
       const testPage = `${baseURL}${path}${miloLibs}`;
       console.info(`[Test Page]: ${testPage}`);
 
       await test.step('step-1: Go to the Marketo block full template test page', async () => {
         await page.goto(testPage);
         await page.waitForLoadState('domcontentloaded');
-        await expect(page).toHaveURL(testPage);
+        await expect(page.url()).toBe(testPage);
         await expect(marketoBlock.email).toBeVisible({ timeout: 8000 });
       });
 
@@ -45,7 +48,7 @@ test.describe('Marketo block test suite', () => {
 
   features[1].path.forEach((path) => {
     test(
-      `1: @marketo full template with company type, ${features[1].tags}, path: ${path}`,
+      `1: @marketo full template (redirect) with company type, ${features[1].tags}, path: ${path}`,
       async ({ page, baseURL }) => {
         const testPage = `${baseURL}${path}${miloLibs}`;
         console.info(`[Test Page]: ${testPage}`);
@@ -53,7 +56,7 @@ test.describe('Marketo block test suite', () => {
         await test.step('step-1: Go to the Marketo block full template test page', async () => {
           await page.goto(testPage);
           await page.waitForLoadState('domcontentloaded');
-          await expect(page).toHaveURL(testPage);
+          await expect(page.url()).toBe(testPage);
           await expect(marketoBlock.email).toBeVisible({ timeout: 8000 });
         });
 
@@ -77,14 +80,14 @@ test.describe('Marketo block test suite', () => {
   });
 
   features[2].path.forEach((path) => {
-    test(`2: @marketo expanded template, ${features[2].tags}}, path: ${path}`, async ({ page, baseURL }) => {
+    test(`2: @marketo expanded template (redirect), ${features[2].tags}}, path: ${path}`, async ({ page, baseURL }) => {
       const testPage = `${baseURL}${path}${miloLibs}`;
       console.info(`[Test Page]: ${testPage}`);
 
       await test.step('step-1: Go to the Marketo block expanded template test page', async () => {
         await page.goto(testPage);
         await page.waitForLoadState('domcontentloaded');
-        await expect(page).toHaveURL(testPage);
+        await expect(page.url()).toBe(testPage);
         await expect(marketoBlock.email).toBeVisible({ timeout: 8000 });
       });
 
@@ -107,14 +110,14 @@ test.describe('Marketo block test suite', () => {
   });
 
   features[3].path.forEach((path) => {
-    test(`3: @marketo essential template, ${features[3].tags}, path: ${path}`, async ({ page, baseURL }) => {
+    test(`3: @marketo essential template (redirect), ${features[3].tags}, path: ${path}`, async ({ page, baseURL }) => {
       const testPage = `${baseURL}${path}${miloLibs}`;
       console.info(`[Test Page]: ${testPage}`);
 
       await test.step('step-1: Go to the Marketo block essential template test page', async () => {
         await page.goto(testPage);
         await page.waitForLoadState('domcontentloaded');
-        await expect(page).toHaveURL(testPage);
+        await expect(page.url()).toBe(testPage);
         await expect(marketoBlock.email).toBeVisible({ timeout: 8000 });
       });
 
@@ -131,6 +134,125 @@ test.describe('Marketo block test suite', () => {
           await marketoBlock.submitButton.waitFor({ state: 'detached' });
           const redirectedUrl = await page.url();
           await expect(redirectedUrl).toContain('?submissionid');
+        }).toPass();
+      });
+    });
+  });
+
+  features[4].path.forEach((path) => {
+    test(`4: @marketo full template (message), ${features[4].tags}, path: ${path}`, async ({ page, baseURL }) => {
+      const testPage = `${baseURL}${path}${miloLibs}`;
+      console.info(`[Test Page]: ${testPage}`);
+
+      await test.step('step-1: Go to the Marketo block full template test page', async () => {
+        await page.goto(testPage);
+        await page.waitForLoadState('domcontentloaded');
+        await expect(page.url()).toBe(testPage);
+        await expect(marketoBlock.email).toBeVisible({ timeout: 8000 });
+      });
+
+      await test.step('step-2: check the input field placeholders', async () => {
+        await marketoBlock.checkInputPlaceholders();
+      });
+
+      await test.step('step-3: Submit the form with valid inputs', async () => {
+        await marketoBlock.submitFullTemplateForm();
+      });
+
+      await test.step('step-4: Verify the form submission redirect', async () => {
+        await expect(async () => {
+          await expect(marketoBlock.message).toBeAttached();
+          await expect(page.url()).toBe(testPage);
+        }).toPass();
+      });
+    });
+  });
+
+  features[5].path.forEach((path) => {
+    test(
+      `5: @marketo full template (message) with company type, ${features[5].tags}, path: ${path}`,
+      async ({ page, baseURL }) => {
+        const testPage = `${baseURL}${path}${miloLibs}`;
+        console.info(`[Test Page]: ${testPage}`);
+
+        await test.step('step-1: Go to the Marketo block full template test page', async () => {
+          await page.goto(testPage);
+          await page.waitForLoadState('domcontentloaded');
+          await expect(page.url()).toBe(testPage);
+          await expect(marketoBlock.email).toBeVisible({ timeout: 8000 });
+        });
+
+        await test.step('step-2: check the input field placeholders', async () => {
+          await marketoBlock.checkInputPlaceholders();
+        });
+
+        await test.step('step-3: Submit the form with valid inputs', async () => {
+          await marketoBlock.submitFullTemplateForm('Digital commerce');
+        });
+
+        await test.step('step-4: Verify the form submission redirect', async () => {
+          await expect(async () => {
+            await expect(marketoBlock.message).toBeAttached();
+            await expect(page.url()).toBe(testPage);
+          }).toPass();
+        });
+      },
+    );
+  });
+
+  features[6].path.forEach((path) => {
+    test(`6: @marketo expanded (message) template, ${features[6].tags}}, path: ${path}`, async ({ page, baseURL }) => {
+      const testPage = `${baseURL}${path}${miloLibs}`;
+      console.info(`[Test Page]: ${testPage}`);
+
+      await test.step('step-1: Go to the Marketo block expanded template test page', async () => {
+        await page.goto(testPage);
+        await page.waitForLoadState('domcontentloaded');
+        await expect(page.url()).toBe(testPage);
+        await expect(marketoBlock.email).toBeVisible({ timeout: 8000 });
+      });
+
+      await test.step('step-2: check the input field placeholders', async () => {
+        await marketoBlock.checkInputPlaceholders();
+      });
+
+      await test.step('step-3: Submit the form with valid inputs', async () => {
+        await marketoBlock.submitExpandedTemplateForm();
+      });
+
+      await test.step('step-4: Verify the form submission redirect', async () => {
+        await expect(async () => {
+          await expect(marketoBlock.message).toBeAttached();
+          await expect(page.url()).toBe(testPage);
+        }).toPass();
+      });
+    });
+  });
+
+  features[7].path.forEach((path) => {
+    test(`7: @marketo essential (message) template, ${features[7].tags}, path: ${path}`, async ({ page, baseURL }) => {
+      const testPage = `${baseURL}${path}${miloLibs}`;
+      console.info(`[Test Page]: ${testPage}`);
+
+      await test.step('step-1: Go to the Marketo block essential template test page', async () => {
+        await page.goto(testPage);
+        await page.waitForLoadState('domcontentloaded');
+        await expect(page.url()).toBe(testPage);
+        await expect(marketoBlock.email).toBeVisible({ timeout: 8000 });
+      });
+
+      await test.step('step-2: check the input field placeholders', async () => {
+        await marketoBlock.checkInputPlaceholders();
+      });
+
+      await test.step('step-3: Submit the form with valid inputs', async () => {
+        await marketoBlock.submitEssentialTemplateForm();
+      });
+
+      await test.step('step-4: Verify the form submission redirect', async () => {
+        await expect(async () => {
+          await expect(marketoBlock.message).toBeAttached();
+          await expect(page.url()).toBe(testPage);
         }).toPass();
       });
     });
