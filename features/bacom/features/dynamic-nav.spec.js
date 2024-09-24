f741827e43e17b99d2bbc7babdf21719dab0ad5a
Dennis Li <denli@adobe.com>
2024-09-24 11:12:04 -0700

    Updating tags

--- features/bacom/features/dynamic-nav.spec.js
+++ features/bacom/features/dynamic-nav.spec.js
@@ -6,14 +6,14 @@ module.exports = {
       name: '@dynamic-nav on',
       entry: '/drafts/nala/features/dynamic-navigation/entry',
       on: '/drafts/nala/features/dynamic-navigation/on',
-      tags: '@dynamic-nav @bacom @smoke @regression',
+      tags: '@dynamic-nav @bacom @smoke @regression @bacomSmoke',
     },
     {
       tcid: '1',
       name: '@dynamic-nav off',
       entry: '/drafts/nala/features/dynamic-navigation/entry',
       off: '/drafts/nala/features/dynamic-navigation/off',
-      tags: '@dynamic-nav @bacom @smoke @regression',
+      tags: '@dynamic-nav @bacom @smoke @regression @bacomSmoke',
     },
   ],
 };
