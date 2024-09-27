6d2d17eeba8077e22ea6ae0ee56dea49d4cba764
Dennis Li <denli@adobe.com>
2024-09-27 16:38:17 -0700

    Adding visual comparison tests for bacom.

---
+++ features/visual/bacom/sot.spec.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'BACOM SOT Visual Comparison',
+  features: [
+    {
+      tcid: '0',
+      name: 'BACOM Visual Comparison',
+      stable: '@bacom_prod',
+      beta: '@bacom_stage',
+      tags: '@bacom-visual @visual @bacom-screenshots',
+      data: 'data/bacom/visual/sot.yml',
+    },
+  ],
+};
