a3e462627b795223281aa45cb6c8c1ce8e456ce9
Jacky Sun <XIASUN@ADOBE.COM>
2024-10-01 16:18:51 -0700

    add tests for graybox-bacom

---
+++ features/visual/graybox/sot.bacom.spec.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'Graybox Bacom SOT Visual Comparison',
+  features: [
+    {
+      tcid: '0',
+      name: '@graybox-bacom-visual',
+      stable: '@bacom_stage',
+      beta: '@graybox_bacom',
+      tags: '@graybox-bacom-visual @visual @graybox-bacom-screenshots',
+      data: 'data/graybox/sot-bacom.yml',
+    },
+  ],
+};
