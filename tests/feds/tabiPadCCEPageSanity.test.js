8e47c846ba4b90c0f69f4a70e546a5add99fa055
pavankumar <pavankumar@adobe.com>
2024-10-05 09:51:42 +0530

    Illustrator, CCE & Home Page Script updation.

---
+++ tests/feds/tabiPadCCEPageSanity.test.js
@@ -0,0 +1,36 @@
+import { expect, test } from '@playwright/test';
+import { features } from '../../features/feds/prodSanity/tabiPadTesting/tabiPadCCE.spec.js';
+import CreativeCloudEnterpriseSanity from '../../selectors/feds/feds.ccepagesanity.page.js';
+
+test.describe('Test Suite for Creative Cloud Business Enterprise Page Components', () => {
+  let enterprise;
+
+  test.beforeEach(async ({ page }) => {
+    enterprise = new CreativeCloudEnterpriseSanity(page);
+  });
+
+  test.afterEach(async ({ page }) => {
+    await page.close();
+  });
+
+  features.forEach((props) => {
+    test(`${props.name}, ${props.tags}, ${props.country}`, async ({ page, baseURL }) => {
+      console.info(`[FEDSInfo] Checking Page: ${baseURL}${features[props.tcid].path}`);
+
+      const pageURL = `${baseURL}${features[props.tcid].path}`;
+      await page.goto(pageURL, { waitUntil: 'networkidle' });
+      await expect(page).toHaveURL(pageURL);
+
+      // Verifying the visibility of U-NAV Elements
+      await enterprise.validatingUnavElements(props.country);
+      // Verifying the visibility of Creativity & Design Elements
+      await enterprise.validatingCreativityAndDesignElementsiPadTab(props.country);
+      // Verifying the visibility of Product Elements
+      await enterprise.validatingProductElements();
+      // Verifying the visibility of Resource Elements
+      await enterprise.validatingResourceElements();
+      // Verifying the visibility of Footer Elements
+      await enterprise.validatingFooterElements();
+    });
+  });
+});
