074bc2d2537552f464632bfedf774c8a994244ed
Jacky Sun <XIASUN@ADOBE.COM>
2024-11-20 11:26:20 -0800

    add tests for graybox cc and homepage

---
+++ features/graybox/sot.homepage.404.spec.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'Graybox Homepage SOT 404 Check',
+  features: [
+    {
+      tcid: '0',
+      name: '@graybox-homepage-404-check',
+      stable: '@adobe_stage',
+      beta: '@graybox_homepage',
+      tags: '@graybox-homepage-404-check',
+      data: 'data/graybox/sot-homepage.yml',
+    },
+  ],
+};
