39c19f5a0526a00c9d40071611d7b61d9dcf1d0a
Dennis Li <34138453+Dli3@users.noreply.github.com>
2024-12-09 11:44:14 -0800

    Updated lang nav tests. Added header nav tests (#467)
    Co-authored-by: Dennis Li <denli@adobe.com>

---
+++ features/bacom-blog/nav.spec.js
@@ -0,0 +1,13 @@
+module.exports = {
+  FeatureName: 'Bacom Blog Header Nav',
+  features: [
+    {
+      tcid: '0',
+      name: 'Bacom Blog Header Nav',
+      path: [
+        '/',
+      ],
+      tags: '@bacom-blog @smoke @regression @nav',
+    },
+  ],
+};
