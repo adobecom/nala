ea17c85df84c121a48421b47b616edf5e2ed5019
Haris Bijli <all46558@adobe.com>
2025-03-16 20:36:01 -0700

    ax-marquee block

---
+++ features/express/ax-marquee.spec.js
@@ -0,0 +1,20 @@
+module.exports = {
+  name: 'Express ax-marquee card block',
+  features: [
+    {
+      tcid: '0',
+      name: '@ax-marquee with button',
+      path: ['/express/create', '/express/business', '/express/business/teams',
+        '/express/entitled', '/express/learn/students', '/express/nonprofits',
+      ],
+      tags: '@express @smoke @regression @ax-marquee',
+    },
+
+    {
+      tcid: '1',
+      name: '@ax-marquee with animation',
+      path: ['/express/create', '/express/learn/blog'],
+      tags: '@express @smoke @regression @ax-marquee',
+    },
+  ],
+};
---
+++ tests/express/ax-marquee.test.js
@@ -0,0 +1,91 @@
+/* eslint-disable no-plusplus */
+import { expect, test } from '@playwright/test';
+import { features } from '../../features/express/ax-marquee.spec.js';
+import AxMarquee from '../../selectors/express/ax-marquee.page.js';
+
+let axMarquee;
+
+test.describe('ax-marquee test suite', () => {
+  test.beforeEach(async ({ page }) => {
+    axMarquee = new AxMarquee(page);
+  });
+
+  features[0].path.forEach((path) => {
+    test(`${features[0].name}, path: ${path}, test marquee with button`, async ({ baseURL, page }) => {
+      const testPage = `${baseURL}${path}`;
+      await axMarquee.gotoURL(testPage);
+
+      await test.step('validate elements in block', async () => {
+        await page.waitForLoadState('domcontentloaded');
+        await expect(axMarquee.axmarquee).toBeVisible();
+        await expect(axMarquee.mainHeading).toBeVisible();
+        const heading = await axMarquee.mainHeading.innerText();
+        expect(heading.length).toBeTruthy();
+        const paragraphCount = axMarquee.text.count();
+        for (let i = 0; i < paragraphCount; i++) {
+          await expect(axMarquee.text).nth(i).toBeVisible();
+          const text = await axMarquee.text.nth(i).innerText();
+          expect(text.length).toBeTruthy();
+        }
+        await expect(axMarquee.expressLogo).toBeVisible();
+        await expect(axMarquee.ctaButton).toBeVisible();
+      });
+
+      await test.step('test button click', async () => {
+        // Nonprofit page button points to external website.
+        if (path !== '/express/nonprofits') {
+          const target = {
+            '/express/business/teams': 'https://commerce-stg.adobe.com/**',
+            '/express/business/': `${baseURL}/express/business#sales-contact-form-1`,
+            '/express/create': 'https://new.express.adobe.com/**',
+            '/express/entitled': 'https://new.express.adobe.com/**',
+            '/express/learn/students': `${baseURL}/express/learn/students#susi-light-student`,
+          };
+
+          await axMarquee.ctaButton.click();
+          await page.waitForURL(target[`${path}`], 6000);
+          const url = page.url();
+          expect(url).not.toBe(testPage);
+        }
+      });
+    });
+  });
+
+  features[1].path.forEach((path) => {
+    test(`${features[1].name}, path: ${path}, test marquee with animation`, async ({ baseURL, page, browserName }) => {
+      const testPage = `${baseURL}${path}`;
+      await axMarquee.gotoURL(testPage);
+      await page.waitForSelector('.ax-marquee .marquee-foreground');
+
+      await test.step('validate elements in block ', async () => {
+        await page.waitForLoadState('domcontentloaded');
+        await expect(axMarquee.axmarquee).toBeVisible();
+        await expect(axMarquee.mainHeading).toBeVisible();
+        const heading = await axMarquee.mainHeading.innerText();
+        expect(heading.length).toBeTruthy();
+        const paragraphCount = axMarquee.text.count();
+        for (let i = 0; i < paragraphCount; i++) {
+          await expect(axMarquee.text).nth(i).toBeVisible();
+          const text = await axMarquee.text.nth(i).innerText();
+          expect(text.length).toBeTruthy();
+        }
+        await expect(axMarquee.expressLogo).toBeVisible();
+        await expect(axMarquee.video).toBeVisible();
+      });
+
+      await test.step('validate elements in block ', async () => {
+        // Animation not loading in Chrome for test script.
+        if (browserName !== 'chromium') {
+          await axMarquee.reduceMotionWrapper.waitFor(3000);
+          await expect(axMarquee.reduceMotionPlayVideoBtn).not.toBeVisible();
+          await expect(axMarquee.reduceMotionPauseVideoBtn).toBeVisible();
+          await axMarquee.reduceMotionPauseVideoBtn.hover();
+          await expect(axMarquee.reduceMotionPauseVideoTxt).toBeVisible();
+          await axMarquee.reduceMotionPauseVideoBtn.click();
+          await page.waitForLoadState();
+          await expect(axMarquee.reduceMotionPlayVideoBtn).toBeVisible();
+        }
+      });
+    });
+  });
+});
