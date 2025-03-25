3bd1ca5e497d88648f00a750ef63463b63efa2e7
Jacky Sun <XIASUN@ADOBE.COM>
2025-03-25 11:50:12 -0700

    fix eslint errors

--- tests/uar/quiz.ai.ui.test.js
+++ tests/uar/quiz.ai.ui.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-await-in-loop */
 /* eslint-disable no-loop-func */
 /* eslint-disable no-restricted-syntax */
-import { expect, test } from '@playwright/test';
+import { test } from '@playwright/test';
 import Quiz from '../../selectors/uar/quiz.page.js';
 
 const QuizSpec = require('../../features/uar/quiz.ai.ui.spec.js');
