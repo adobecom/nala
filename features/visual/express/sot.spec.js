97d0ba9d7f05f4983f44ba6c1917ba87d0393317
Jacky Sun <67350368+JackySun9@users.noreply.github.com>
2025-03-25 17:01:10 -0700

    add screenshots test for express pages (#491)
    * add tests for graybox-bacom
    
    * just use chrome
    
    * add more waiting time
    
    * add merch card
    
    * add 404 checker for graxybox-bacom
    
    * update gitignore and file name
    
    * add screenshots for graybox-dc
    
    * add 404 checking for graybox dc
    
    * adjust screenshot tests
    
    * update screenshot tests to public s3 and git action to run them
    
    * fix git action
    
    * update git action
    
    * install dependencies
    
    * only able to run hlx.live pages not stage pages
    
    * add tests for graybox cc and homepage
    
    * fix cc graybox screenshots
    
    * add visual tests for bacom-blog,cc and homepage
    
    * update blog urls
    
    * update testing urls for bacom
    
    * update quiz ai test for new bc-uar block
    
    * fix eslint errors
    
    * update run-lint
    
    * fix eslint errors
    
    * add screenshots for express pages

---
+++ features/visual/express/sot.spec.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'Express SOT Visual Comparison',
+  features: [
+    {
+      tcid: '0',
+      name: '@express-visual',
+      stable: '@adobe_prod',
+      beta: '@adobe_stage',
+      tags: '@express-visual @visual @express-screenshots',
+      data: 'data/express/sot-basic.yml',
+    },
+  ],
+};
