64595d0ca2150dd0a94c22454d8210422075c42a
Haris Bijli <all46558@adobe.com>
2025-03-28 12:43:04 -0700

    grid marquee block

---
+++ features/express/grid-marquee.spec.js
@@ -0,0 +1,25 @@
+module.exports = {
+  name: 'Express grid-marquee block',
+  features: [
+    {
+      tcid: '0',
+      name: '@grid-marquee headline',
+      path: ['/'],
+      tags: '@express @smoke @regression @grid-marquee @headline',
+    },
+
+    {
+      tcid: '1',
+      name: '@grid-marquee cards',
+      path: ['/express/'],
+      tags: '@express @smoke @regression @grid-marquee @marquee-cards',
+    },
+
+    {
+      tcid: '2',
+      name: '@ax-marquee ratings',
+      path: ['/express/'],
+      tags: '@express @smoke @regression @grid-marquee @ratings',
+    },
+  ],
+};
