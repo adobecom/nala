b182e479e31b180d315bc4f3f00ba031bd903f84
Jacky Sun <XIASUN@ADOBE.COM>
2025-03-27 22:45:46 -0700

    make screenshot diff to next level, support base and current version

--- tests/visual/express/sot.base.test.js
+++ tests/visual/express/sot.base.test.js
@@ -1,15 +1,15 @@
 import { test } from '@playwright/test';
-import { features } from '../../../features/visual/bacom-blog/sot.spec.js';
-import { takeTwo } from '../../../libs/screenshot/take.js';
+import { features } from '../../../features/visual/express/sot.base.spec.js';
+import { takeOne } from '../../../libs/screenshot/take.js';
 import { writeResultsToFile } from '../../../libs/screenshot/utils.js';
 
 const { WebUtil } = require('../../../libs/webutil.js');
 
-const folderPath = 'screenshots/bacom-blog';
+const folderPath = 'screenshots/express';
 const results = {};
-const MILO_LIBS = '?milolibs=stage';
+const MILO_LIBS = '';
 
-test.describe('BACOM Blog SOT visual comparison test suite', () => {
+test.describe('Express SOT visual comparison test suite', () => {
   // reset timeout because we use this to run all test data
   test.setTimeout(10 * 60 * 1000);
 
@@ -18,16 +18,12 @@ test.describe('BACOM Blog SOT visual comparison test suite', () => {
       const testdata = await WebUtil.loadTestData(`${feature.data}`);
 
       for (const key of Object.keys(testdata)) {
-        const stableURL = testdata[key];
-        console.info(stableURL);
         const betaURL = testdata[key] + MILO_LIBS;
         console.info(betaURL);
 
         const name = `${feature.name}-${key}-${testInfo.project.name}`;
-        const result = await takeTwo(
+        const result = await takeOne(
           page,
-          stableURL,
-          async () => { await page.waitForSelector('.feds-footer-privacyLink'); },
           betaURL,
           async () => { await page.waitForSelector('.feds-footer-privacyLink'); },
           folderPath,
