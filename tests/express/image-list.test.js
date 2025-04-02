a545523ca53deae9021210533893fd07679aa39a
Haris Bijli <all46558@adobe.com>
2025-04-02 16:30:49 -0700

    script improvements

--- tests/express/image-list.test.js
+++ tests/express/image-list.test.js
@@ -4,7 +4,6 @@ import { features } from '../../features/express/image-list.spec.js';
 import ImageList from '../../selectors/express/image-list.page.js';
 
 let imageList;
-const stageURL = 'https://www.stage.adobe.com/express/';
 
 test.describe('Image List Block Test Suite', () => {
   // before each test block
@@ -14,106 +13,82 @@ test.describe('Image List Block Test Suite', () => {
 
   test(`${features[0].name},${features[0].tags}`, async ({ page, baseURL }) => {
     console.info(`${baseURL}${features[0].path}`);
-
-    await test.step('Go to Image List block test page', async () => {
-      await page.goto(`${baseURL}${features[0].path}`);
-      await page.waitForLoadState('domcontentloaded');
-      await expect(page).toHaveURL(`${baseURL}${features[0].path}`);
-    });
+    const testPage = `${baseURL}${features[0].path}`;
+    await page.goto(testPage);
+    await page.waitForSelector('.global-footer');
 
     await test.step('Verify block displayed ', async () => {
-      await page.waitForLoadState();
       await expect(imageList.imageListXSmall).toBeVisible();
     });
 
-    await test.step('On click, goes to homepage ', async () => {
-      await page.waitForLoadState();
+    await test.step('Test image click ', async () => {
       await imageList.imageListXSmall.click();
-      await expect(page).toHaveURL(`${stageURL}`);
+      expect(page.url).not.toBe(testPage);
     });
   });
 
   test(`${features[1].name},${features[1].tags}`, async ({ page, baseURL }) => {
     console.info(`${baseURL}${features[1].path}`);
-
-    await test.step('Go to Image List block test page', async () => {
-      await page.goto(`${baseURL}${features[1].path}`);
-      await page.waitForLoadState('domcontentloaded');
-      await expect(page).toHaveURL(`${baseURL}${features[1].path}`);
-    });
+    const testPage = `${baseURL}${features[1].path}`;
+    await page.goto(testPage);
+    await page.waitForSelector('.global-footer');
 
     await test.step('Verify block displayed ', async () => {
-      await page.waitForLoadState();
       await expect(imageList.imageListSmall).toBeVisible();
     });
 
-    await test.step('On click, goes to homepage ', async () => {
+    await test.step('Test image click ', async () => {
       await page.waitForLoadState();
       await imageList.imageListSmall.click();
-      await expect(page).toHaveURL(`${stageURL}`);
+      expect(page.url).not.toBe(testPage);
     });
   });
 
   test(`${features[2].name},${features[2].tags}`, async ({ page, baseURL }) => {
     console.info(`${baseURL}${features[2].path}`);
-
-    await test.step('Go to Image List block test page', async () => {
-      await page.goto(`${baseURL}${features[2].path}`);
-      await page.waitForLoadState('domcontentloaded');
-      await expect(page).toHaveURL(`${baseURL}${features[2].path}`);
-    });
+    const testPage = `${baseURL}${features[2].path}`;
+    await page.goto(testPage);
+    await page.waitForSelector('.global-footer');
 
     await test.step('Verify block displayed ', async () => {
-      await page.waitForLoadState();
       await expect(imageList.imageList).toBeVisible();
     });
 
-    await test.step('On click, goes to homepage ', async () => {
-      await page.waitForLoadState();
+    await test.step('Test image click ', async () => {
       await imageList.imageList.click();
-      await expect(page).toHaveURL(`${stageURL}`);
+      expect(page.url).not.toBe(testPage);
     });
   });
 
   test(`${features[3].name},${features[3].tags}`, async ({ page, baseURL }) => {
     console.info(`${baseURL}${features[3].path}`);
-
-    await test.step('Go to Image List block test page', async () => {
-      await page.goto(`${baseURL}${features[3].path}`);
-      await page.waitForLoadState('domcontentloaded');
-      await expect(page).toHaveURL(`${baseURL}${features[3].path}`);
-    });
+    const testPage = `${baseURL}${features[3].path}`;
+    await page.goto(testPage);
+    await page.waitForSelector('.global-footer');
 
     await test.step('Verify block displayed ', async () => {
-      await page.waitForLoadState();
       await expect(imageList.imageListLarge).toBeVisible();
     });
 
-    await test.step('On click, goes to homepage ', async () => {
-      await page.waitForLoadState();
+    await test.step('Test image click ', async () => {
       await imageList.imageListLarge.click();
-      await expect(page).toHaveURL(`${stageURL}`);
+      expect(page.url).not.toBe(testPage);
     });
   });
 
   test(`${features[4].name},${features[4].tags}`, async ({ page, baseURL }) => {
     console.info(`${baseURL}${features[4].path}`);
-
-    await test.step('Go to Image List block test page', async () => {
-      await page.goto(`${baseURL}${features[4].path}`);
-      await page.waitForLoadState('domcontentloaded');
-      await expect(page).toHaveURL(`${baseURL}${features[4].path}`);
-    });
+    const testPage = `${baseURL}${features[4].path}`;
+    await page.goto(testPage);
+    await page.waitForSelector('.global-footer');
 
     await test.step('Verify block displayed ', async () => {
-      await page.waitForLoadState();
       await expect(imageList.imageListXLarge).toBeVisible();
     });
 
-    await test.step('On click, goes to homepage ', async () => {
-      await page.waitForLoadState();
+    await test.step('Test image click ', async () => {
       await imageList.imageListXLarge.click();
-      await expect(page).toHaveURL(`${stageURL}`);
+      expect(page.url).not.toBe(testPage);
     });
   });
 });
