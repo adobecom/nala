d7060075b85f6f8869681f2cff5cb148e20f08ea
Haris Bijli <all46558@adobe.com>
2025-04-28 17:12:57 -0700

    how-to-steps-carousel block

---
+++ selectors/express/how-to-steps-carousel.page.js
@@ -0,0 +1,37 @@
+export default class HowToStepsCarousel {
+  constructor(page) {
+    this.page = page;
+    this.howToStepsCarouselContainer = page.locator('.how-to-steps-carousel-container');
+    this.image = this.howToStepsCarouselContainer.locator('> picture');
+    this.heading = this.howToStepsCarouselContainer.locator('h2');
+    this.howToStepsCarousel = this.howToStepsCarouselContainer.locator('.how-to-steps-carousel');
+    this.tipNumbers = this.howToStepsCarousel.locator('.tip-numbers');
+    this.tipNumber = this.howToStepsCarousel.locator('.tip-number');
+    this.tips = this.howToStepsCarousel.locator('.tips');
+    this.tip = this.tips.locator('.tip');
+    this.tipTitle = this.tip.locator('h3');
+    this.tipText = this.tip.locator('div');
+    this.button = this.howToStepsCarouselContainer.locator('.button');
+    this.logoMakerPage = page.locator('data-testid="logo-maker"');
+    this.globalFooter = page.locator('.global-footer');
+  }
+
+  async gotoURL(url) {
+    await this.page.goto(url);
+    await this.globalFooter.waitFor();
+  }
+
+  async scrollToHowToStepsCarousel() {
+    await this.page.waitForLoadState('domcontentloaded');
+    if (this.howToStepsCarouselContainer) {
+      await this.howToStepsCarouselContainer.scrollIntoViewIfNeeded(2000);
+    }
+  }
+
+  async clickButton() {
+    await this.page.waitForLoadState('domcontentloaded');
+    if (this.button) {
+      await this.button.click();
+    }
+  }
+}
