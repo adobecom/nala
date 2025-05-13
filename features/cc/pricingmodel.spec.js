e05d6e397b63b088b887e34133bc8b51be6bf69c
sigadamramesh <130433161+sigadamvenkata@users.noreply.github.com>
2025-05-14 00:16:23 +0530

    price model, inline video, Jarvis presence AU test  (#505)
    * CC Merchtable block automation
    
    CC Merchtable block automation (JIra: MWPW-140978)
    
    * CC sticky promo bar, promo with close action automated test cases
    
    CC sticky promo bar, promo with close action automated test cases
    Jira : MWPW-140982, MWPW-142252
    
    * CC Regression automated cases for media rounded corners, breadcrumb features
    
    CC Regression automated cases for media rounded corners, breadcrumb features (MWPW-140976, MWPW-142543)
    
    * Update roundedcorners-mediablock.test.js
    
    * CC home page tabs, fragment reference , merch pod AU tests
    
    CC home page tabs, fragment reference , merch pod AU tests
    Jiras:
    https://jira.corp.adobe.com/browse/MWPW-143230
    https://jira.corp.adobe.com/browse/MWPW-142599
    https://jira.corp.adobe.com/browse/MWPW-143231
    
    * CC , CCT product pages sanity checks automation for EN
    
    Key CC , CCT product pages sanity checks automation for EN
    https://jira.corp.adobe.com/browse/MWPW-146289
    
    * CC Tier1 locales production sanity tests
    
    CC Tier1 locales production sanity tests
    https://jira.corp.adobe.com/browse/MWPW-146289
    
    * Update productionsanity.spec.js
    
    removed the lengthy URL
    
    * Update productionsanity.page.js
    
    removed the space
    
    * Update productionsanity.test.js
    
    removed tailing spaces from given lines
    
    * CC page Accordion features regression automated cases
    
    CC page Accordion features regression automated cases
    https://jira.corp.adobe.com/browse/MWPW-147151
    https://jira.corp.adobe.com/browse/MWPW-147152
    https://jira.corp.adobe.com/browse/MWPW-147153
    https://jira.corp.adobe.com/browse/MWPW-147154
    
    * Update accordion.test.js
    
    corrected the typo
    
    * Update accordion.test.js
    
    fixed indentations issue at line 14,15,16
    
    Also removed wait from line number 81
    
    * CC Firefly integration tests
    
    CC firefly product page UI & integration with FF web
    
    * Update firefly.test.js
    
    removed unwanted wait
    
    * Update firefly.test.js
    
    removed commented line
    
    * Update firefly.test.js
    
    removed wait for load state line
    
    * Update firefly.test.js
    
    typo fixed
    
    * OST price fixes
    
    OST price are changed in few locales and now corrected the script as per OST changes
    
    * Doccloud pages gnav sanity test
    
    Doccloud pages gnav sanity test
    
    * adding missed stage cc domain
    
    adding missed stage cc domain
    
    * Excluding production sanity & stage webtier tests from stage run
    
    Excluding production sanity & stage webtier tests from stage run
    
    * CC minor DOM / path changes
    
    CC minor DOM / path changes
    
    * CC model price, inline video, jarvis automated test cases
    
    CC model price, inline video, jarvis automated test cases
    
    * Update pricingmodel.page.js
    
    ---------
    
    Co-authored-by: Aaron Mauchley <mauchley@adobe.com>

---
+++ features/cc/pricingmodel.spec.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name: 'pricingmodel',
+  features: [
+    {
+      tcid: '0',
+      name: '@pricingmodel-ui',
+      path: '/creativecloud?georouting=off',
+      tags: '@cc @cc-pricingmodel @cc-pricingmodel-ui',
+    },
+    {
+      tcid: '1',
+      name: '@pricingmodel-tabandpicksubscription',
+      path: '/creativecloud?georouting=off',
+      tags: '@cc @cc-pricingmodel @cc-pricingmodel-tabandpicksubscription',
+    },
+  ],
+};
