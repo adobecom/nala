9e20a0983ad7fe58b81bb4ca6969d045dfe5672d
Gayane Mirijanyan <gmirijan@Gayanes-MacBook-Pro.local>
2025-06-16 02:01:06 -0700

    MWPW-175082: Automation test in NALA is failing

--- selectors/caas/caas.feature.page.js
+++ selectors/caas/caas.feature.page.js
@@ -6,6 +6,6 @@ export default class Caas {
     this.caasFirstCard = this.page.locator('.consonant-Card').nth(0);
     this.caasCards = this.page.locator('.consonant-Card');
     this.caasTitle = this.page.locator('.consonant-FiltersInfo-title');
-    this.caasPaginator = this.page.locator('.consonant-Pagination-summary');
+    this.caasPaginator = this.page.locator('.consonant-Pagination-btn--prev');
   }
 }
