b7fc4951a43614daf9569fa405c0a0b65c745b03
syt <144407787+Snehayt@users.noreply.github.com>
2025-09-02 11:00:37 +0530

    MWPW-177523 : photoshop sanity automation (#529)
    * photoshop sanity
    
    * Android Automation for Photoshop :Feds
    
    ---------
    
    Co-authored-by: ADOBENET\syt <syt@adobe.com>

---
+++ tests/feds/mobilePhotoshopPageSanity.test.js
@@ -0,0 +1,44 @@
+import { test, expect } from '@playwright/test';
+import { features } from '../../features/feds/prodSanity/mobileTesting/mobilePhotoshopSanity.spec.js';
+import PhotoshopPageSanity from '../../selectors/feds/feds.photoshopsanity.page.js';
+import CreativeCloudEnterpriseSanity from '../../selectors/feds/feds.ccepagesanity.page.js';
+import HomePageSanity from '../../selectors/feds/feds.homepagesanity.page.js';
+
+test.describe('Test Suite for mobile Photoshop Page Components', () => {
+  let photoshop;
+  let home;
+  let enterprise;
+
+  test.beforeEach(async ({ page }) => {
+    photoshop = new PhotoshopPageSanity(page);
+    home = new HomePageSanity(page);
+    enterprise = new CreativeCloudEnterpriseSanity(page);
+  });
+
+  test.afterEach(async ({ page }) => {
+    await page.close();
+  });
+
+  features.forEach((props) => {
+    test(`${props.name}, ${props.tags}, ${props.country}`, async ({ page, baseURL }) => {
+      console.info(`[FEDSInfo] Checking Page: ${baseURL}${features[props.tcid].path}`);
+
+      const pageURL = `${baseURL}${features[props.tcid].path}`;
+      await page.goto(pageURL, { waitUntil: 'networkidle' });
+      await expect(page).toHaveURL(pageURL);
+
+      // Verifying the visibility of U-NAV Elements
+      await photoshop.validatingUnavMobile(test);
+      // Verifying the visibility of L-NAV Elements
+      await photoshop.validatingLnavElements();
+      // Verifying the visibility of Hamburger Elements
+      await photoshop.validatingHamburgerMenu(props.country);
+      // Verifying the visibility of Footer Elements
+      await enterprise.validatingFooterMob(test, props.country);
+      // Verifying Cookie Preference
+      await home.validatingCookiePreference(props.country);
+      // Verifying Change Region
+      await photoshop.validatingChangeRegion(props.country);
+    });
+  });
+});
---
+++ tests/feds/photoshop.sanity.test.js
@@ -0,0 +1,33 @@
+import { expect, test } from '@playwright/test';
+import { features } from '../../features/feds/photoshop.spec.js';
+import PhotoshopPageSanity from '../../selectors/feds/feds.photoshopsanity.page.js';
+
+test.describe('Test Suite for Photoshop Page Components', () => {
+  let photoshop;
+
+  test.beforeEach(async ({ page }) => {
+    photoshop = new PhotoshopPageSanity(page);
+  });
+
+  test.afterEach(async ({ page }) => {
+    await page.close();
+  });
+
+  features.forEach((props) => {
+    test(`${props.name}, ${props.tags}, ${props.country}`, async ({ page, baseURL }) => {
+      console.info(`[FEDSInfo] Checking Page: ${baseURL}${props.path}`);
+
+      const pageURL = `${baseURL}${props.path}`;
+      await page.goto(pageURL, { waitUntil: 'networkidle' });
+      await expect(page).toHaveURL(pageURL);
+      await photoshop.validatingcloseDialogModel(props.country);
+
+      // Verifying the visibility of U-NAV Elements
+      await photoshop.validatingUnavElements(props.country);
+      // Verifying the Visibility of Creativity & Design Elements
+      await photoshop.validatingCreativityAndDesignElements(props.country);
+      // Verifying the visibility of Footer Elements
+      await photoshop.validatingFooterElements(props.country);
+    });
+  });
+});
