ee301dbe5f9454ef92551adb845b5efab65cc7ba
Jacky Sun <67350368+JackySun9@users.noreply.github.com>
2025-10-09 15:33:11 -0700

    add test page for bc-uar prod (#533)
    * add tests for graybox-bacom
    
    * just use chrome
    
    * add more waiting time
    
    * add merch card
    
    * add 404 checker for graxybox-bacom
    
    * update gitignore and file name
    
    * add screenshots for graybox-dc
    
    * add 404 checking for graybox dc
    
    * adjust screenshot tests
    
    * update screenshot tests to public s3 and git action to run them
    
    * fix git action
    
    * update git action
    
    * install dependencies
    
    * only able to run hlx.live pages not stage pages
    
    * add tests for graybox cc and homepage
    
    * fix cc graybox screenshots
    
    * add visual tests for bacom-blog,cc and homepage
    
    * update blog urls
    
    * update testing urls for bacom
    
    * update quiz ai test for new bc-uar block
    
    * fix eslint errors
    
    * update run-lint
    
    * fix eslint errors
    
    * add screenshots for express pages
    
    * fix security concers
    
    * make screenshot diff to next level, support base and current version
    
    * add visual page object, update shell input checking
    
    * update express visual tests
    
    * create tests for brand concierge uar
    
    * update testing pages on prod

--- features/visual/uar-ai/quiz.ai.spec.js
+++ features/visual/uar-ai/quiz.ai.spec.js
@@ -5,7 +5,7 @@ module.exports = {
       tcid: '0',
       name: '@quiz-ai-visual',
       path: '/cc-shared/fragments/uar/brand-concierge/brand-concierge',
-      stable: '@adobe_stage',
+      stable: '@adobe_prod',
       beta: '@adobe_stage',
       tags: '@quiz-ai-visual @visual @uar-ai-screenshots',
       data: 'data/uar/quiz/ai/quiz-ai-basic.yml',
